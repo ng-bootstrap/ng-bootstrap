589d255d0611d6dec0eb6e0d9c2e7ac1355cb01e
Foxandxss <foxandxss@gmail.com>
2015-09-30 15:01:27 +0200

    chore: add clang formatting to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,59 +6,42 @@ var webpack = require('webpack');
 var gulpFormat = require('gulp-clang-format');
 var clangFormat = require('clang-format');
 
-gulp.task('clean', function (cb) {
-  del('dist/', cb);
-});
+gulp.task('clean', function(cb) { del('dist/', cb); });
 
-gulp.task('es5', function () {
-  var tsProject = ts.createProject('tsconfig.json', {
-    module: 'commonjs',
-    target: 'es5'
-  });
+gulp.task('es5', function() {
+  var tsProject = ts.createProject('tsconfig.json', {module: 'commonjs', target: 'es5'});
 
-  var tsResult = gulp.src([
-      'node_modules/typescript/lib/lib.es6.d.ts',
-      'typings/tsd.d.ts',
-      'src/**/*.ts'
-    ])
-    .pipe(ts(tsProject));
+  var tsResult =
+      gulp.src(['node_modules/typescript/lib/lib.es6.d.ts', 'typings/tsd.d.ts', 'src/**/*.ts']).pipe(ts(tsProject));
 
   return tsResult.js.pipe(gulp.dest('dist/cjs'));
 });
 
-gulp.task('es6', function () {
+gulp.task('es6', function() {
   var tsProject = ts.createProject('tsconfig.json');
 
-  var tsResult = gulp.src([
-      'typings/tsd.d.ts',
-      'src/**/*.ts'
-    ])
-    .pipe(ts(tsProject));
+  var tsResult = gulp.src(['typings/tsd.d.ts', 'src/**/*.ts']).pipe(ts(tsProject));
 
   return tsResult.js.pipe(gulp.dest('dist/es6'));
 });
 
-gulp.task('umd', ['es5', 'es6'], function (cb) {
-  webpack({
-    entry: './dist/cjs/core.js',
-    output: {
-      filename: 'dist/global/ng-bootstrap.js',
-      libraryTarget: 'umd'
-    },
-    externals: {
-      'angular2/angular2': 'angular2/angular2'
-    }
-  }, function (err, stats) {
-    if (err) throw new gutil.PluginError('webpack', err);
-    gutil.log("[webpack]", stats.toString());
-    cb();
-  })
+gulp.task('umd', ['es5', 'es6'], function(cb) {
+  webpack(
+      {
+        entry: './dist/cjs/core.js',
+        output: {filename: 'dist/global/ng-bootstrap.js', libraryTarget: 'umd'},
+        externals: {'angular2/angular2': 'angular2/angular2'}
+      },
+      function(err, stats) {
+        if (err) throw new gutil.PluginError('webpack', err);
+        gutil.log("[webpack]", stats.toString());
+        cb();
+      })
 });
 
 gulp.task('check-format', function() {
-  return doCheckFormat().on('warning', function(e) {
-    console.log("NOTE: this will be promoted to an ERROR in the continuous build");
-  });
+  return doCheckFormat().on(
+      'warning', function(e) { console.log("NOTE: this will be promoted to an ERROR in the continuous build"); });
 });
 
 gulp.task('enforce-format', function() {
@@ -72,8 +55,7 @@ gulp.task('enforce-format', function() {
 gulp.task('build', ['clean', 'umd', 'check-format']);
 gulp.task('default', ['build']);
 
-//formatting
+// formatting
 function doCheckFormat() {
-  return gulp.src(['src/**/*.ts'])
-    .pipe(gulpFormat.checkFormat('file', clangFormat));
+  return gulp.src(['gulpfile.js', 'src/**/*.ts']).pipe(gulpFormat.checkFormat('file', clangFormat));
 }
