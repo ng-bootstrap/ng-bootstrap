fe88f8fd11dee3cb7e85d7ba49fd20a20cb27c22
Foxandxss <foxandxss@gmail.com>
2015-10-15 10:29:50 +0200

    chore(dropdown): update to .44
    Closes #51

--- src/components/dropdown.ts
+++ src/components/dropdown.ts
@@ -1,16 +1,15 @@
-import {Component, View} from 'angular2/angular2';
+import {Component, Input} from 'angular2/angular2';
 
-@Component({selector: 'ngb-dropdown', properties: ['open']})
-@View({
+@Component({
+  selector: 'ngb-dropdown',
   template: `<div class="dropdown" [class.open]="open">
     <ng-content></ng-content>
   </div>`
 })
 export class NgbDropdown {
-  private open: boolean;
+  @Input() private open: boolean;
 }
 
-@Component({selector: 'ngb-dropdown-menu', host: {class: 'dropdown-menu'}})
-@View({template: `<ng-content></ng-content>`})
+@Component({selector: 'ngb-dropdown-menu', host: {'class': 'dropdown-menu'}, template: `<ng-content></ng-content>`})
 export class NgbDropdownMenu {
 }
