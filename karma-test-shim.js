fb2550424dc84997a84993954b56b61d5b8d0422
Foxandxss <foxandxss@gmail.com>
2015-10-24 21:32:51 +0200

    chore: testing tooling
    Closes #56

---
+++ karma-test-shim.js
@@ -0,0 +1,64 @@
+// Tun on full stack traces in errors to help debugging
+Error.stackTraceLimit=Infinity;
+
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 1000;
+
+// // Cancel Karma's synchronous start,
+// // we will call `__karma__.start()` later, once all the specs are loaded.
+__karma__.loaded = function() {};
+
+
+System.config({
+  packages: {
+    'base/temp/components': {
+      defaultExtension: false,
+      format: 'register',
+      map: Object.keys(window.__karma__.files).
+            filter(onlyAppFiles).
+            reduce(function createPathRecords(pathsMapping, appPath) {
+              // creates local module name mapping to global path with karma's fingerprint in path, e.g.:
+              // './hero.service': '/base/temp/components/hero.service.js?f4523daf879cfb7310ef6242682ccf10b2041b3e'
+              var moduleName = appPath.replace(/^\/base\/temp\/components\//, './').replace(/\.js$/, '');
+              pathsMapping[moduleName] = appPath + '?' + window.__karma__.files[appPath]
+              return pathsMapping;
+            }, {})
+
+      }
+    }
+});
+
+System.import('angular2/src/core/dom/browser_adapter').then(function(browser_adapter) {
+  browser_adapter.BrowserDomAdapter.makeCurrent();
+}).then(function() {
+  return Promise.all(
+    Object.keys(window.__karma__.files) // All files served by Karma.
+    .filter(onlySpecFiles)
+    // .map(filePath2moduleName)        // Normalize paths to module names.
+    .map(function(moduleName) {
+      // loads all spec files via their global module names (e.g. 'base/temp/test/hero.service_test')
+      return System.import(moduleName);
+    }));
+})
+.then(function() {
+  __karma__.start();
+}, function(error) {
+  __karma__.error(error.stack || error);
+});
+
+
+function filePath2moduleName(filePath) {
+  return filePath.
+           replace(/^\//, '').              // remove / prefix
+           replace(/\.\w+$/, '');           // remove suffix
+}
+
+
+function onlyAppFiles(filePath) {
+  return /^\/base\/temp\/components\/.*\.js$/.test(filePath)
+}
+
+
+function onlySpecFiles(path) {
+  return /_test\.js$/.test(path);
+}
---
+++ karma.conf.js
@@ -0,0 +1,31 @@
+module.exports = function(config) {
+  config.set({
+
+    basePath: '',
+
+    frameworks: ['jasmine'],
+
+    files: [
+      // paths loaded by Karma
+      {pattern: 'node_modules/systemjs/dist/system.src.js', included: true, watched: true},
+      {pattern: 'node_modules/angular2/bundles/angular2.js', included: true, watched: true},
+      {pattern: 'node_modules/angular2/bundles/testing.js', included: true, watched: true},
+      {pattern: 'karma-test-shim.js', included: true, watched: true},
+
+      // paths loaded via module imports
+      {pattern: 'temp/**/*.js', included: false, watched: true},
+
+      // paths to support debugging with source maps in dev tools
+      {pattern: 'src/**/*.ts', included: false, watched: false},
+      {pattern: 'temp/**/*.js.map', included: false, watched: false}
+    ],
+
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['Chrome'],
+    singleRun: false
+  })
+}
