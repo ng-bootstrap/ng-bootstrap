6f10352843564da17cf2cd156946e8fab14395d4
Foxandxss <foxandxss@gmail.com>
2015-10-27 18:40:57 +0100

    chore(rating): add a first batch of tests
    Closes #57

--- src/test/sanity_test.ts
+++
@@ -1,9 +0,0 @@
-describe('universal truths', () => {
-  it('should do math', () => {
-    expect(1 + 1).toEqual(2);
-
-    expect(5).toBeGreaterThan(4);
-  });
-
-  xit('should skip this', () => { expect(4).toEqual(40); });
-});
