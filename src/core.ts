e071e48e66581b817376e00e311390e6a2a5324c
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2015-11-02 14:17:16 +0100

    feat(all): make it easy to import all directives

--- src/core.ts
+++ src/core.ts
@@ -1,3 +1,9 @@
+import {NgbAccordion, NgbAccordionGroup} from './components/accordion';
+import {NgbDropdown, NgbDropdownMenu} from './components/dropdown';
+import {NgbRating} from './components/rating';
+
 export {NgbAccordion, NgbAccordionGroup} from './components/accordion';
 export {NgbDropdown, NgbDropdownMenu} from './components/dropdown';
 export {NgbRating} from './components/rating';
+
+export const NGB_DIRECTIVES = [NgbAccordion, NgbAccordionGroup, NgbDropdown, NgbDropdownMenu, NgbRating];
