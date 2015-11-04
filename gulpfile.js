fdc722dee5c4c81de4a2cfaf9d90a9f043402af1
Foxandxss <foxandxss@gmail.com>
2015-11-04 16:17:56 +0100

    chore(tooling): move everything to gulp
    Closes #78

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,30 @@
 var gulp = require('gulp');
+var ts = require('gulp-typescript');
 var gutil = require('gulp-util');
-var webpack = require('webpack');
-var gulpFormat = require('gulp-clang-format');
+var sourcemaps = require('gulp-sourcemaps');
+var ddescribeIit = require('gulp-ddescribe-iit');
+var del = require('del');
+var merge = require('merge2');
 var clangFormat = require('clang-format');
+var gulpFormat = require('gulp-clang-format');
+var runSequence = require('run-sequence');
+
+var PATHS = {src: 'src/**/*.ts', specs: 'src/**/*.spec.ts'};
+
+// Transpiling & Building
+
+var buildProject = ts.createProject('tsconfig.json', {declaration: true});
+
+gulp.task('clean:build', function() { return del('dist/'); });
+
+gulp.task('cjs', function() {
+  var tsResult = gulp.src([PATHS.src, '!' + PATHS.specs]).pipe(ts(buildProject));
+
+  return merge([tsResult.dts.pipe(gulp.dest('dist/cjs')), tsResult.js.pipe(gulp.dest('dist/cjs'))]);
+});
 
 gulp.task('umd', function(cb) {
+  var webpack = require('webpack');
   webpack(
       {
         entry: './dist/cjs/core.js',
@@ -18,8 +38,39 @@ gulp.task('umd', function(cb) {
         if (err) throw new gutil.PluginError('webpack', err);
         gutil.log("[webpack]", stats.toString());
         cb();
-      })
+      });
 });
+
+// Testing
+
+var testProject = ts.createProject('tsconfig.json');
+
+gulp.task('build-tests', function() {
+  var tsResult = gulp.src(PATHS.src).pipe(sourcemaps.init()).pipe(ts(testProject));
+
+  return tsResult.js.pipe(sourcemaps.write('.')).pipe(gulp.dest('temp'));
+});
+
+gulp.task(
+    'ddescribe-iit', function() { return gulp.src(PATHS.specs).pipe(ddescribeIit({allowDisabledTests: false})); });
+
+gulp.task('test', function(done) {
+  var karmaServer = require('karma').Server;
+  var travis = process.env.TRAVIS;
+
+  var config = {configFile: __dirname + '/karma.conf.js', singleRun: true, autoWatch: false};
+
+  if (travis) {
+    config['reporters'] = ['dots'];
+    config['browsers'] = ['Firefox'];
+  }
+
+  new karmaServer(config, done).start();
+});
+
+gulp.task('watch', ['build-tests'], function() { gulp.watch(PATHS.src, ['build-tests']); });
+
+// Formatting
 
 gulp.task('check-format', function() {
   return doCheckFormat().on(
@@ -34,11 +85,14 @@ gulp.task('enforce-format', function() {
   });
 });
 
-gulp.task('build', ['umd', 'check-format']);
-gulp.task('default', ['build']);
-
-// formatting
 function doCheckFormat() {
-  return gulp.src(['gulpfile.js', 'karma-test-shim.js', 'src/**/*.ts'])
-      .pipe(gulpFormat.checkFormat('file', clangFormat));
+  return gulp.src(['gulpfile.js', 'karma-test-shim.js', PATHS.src]).pipe(gulpFormat.checkFormat('file', clangFormat));
 }
+
+// Public Tasks
+
+gulp.task('build', function(done) {
+  runSequence('enforce-format', 'ddescribe-iit', 'build-tests', 'test', 'clean:build', 'cjs', 'umd', done);
+});
+
+gulp.task('default', function(done) { runSequence('enforce-format', 'ddescribe-iit', 'build-tests', 'test', done); });
