3a4b7df486ba9eed17f59850dced408161a9b36e
Wesley Cho <wesley.cho@gmail.com>
2015-11-07 21:34:43 -0800

    feat(dropdown): change API to be more natural
    - Remove unnecessary `dropdownMenu` component
    - Change dropdown to directive
    - Add tests for toggle behavior

--- src/core.ts
+++ src/core.ts
@@ -1,9 +1,9 @@
 import {NgbAccordion, NgbAccordionGroup} from './accordion/accordion';
-import {NgbDropdown, NgbDropdownMenu} from './dropdown/dropdown';
+import {NgbDropdown} from './dropdown/dropdown';
 import {NgbRating} from './rating/rating';
 
 export {NgbAccordion, NgbAccordionGroup} from './accordion/accordion';
-export {NgbDropdown, NgbDropdownMenu} from './dropdown/dropdown';
+export {NgbDropdown} from './dropdown/dropdown';
 export {NgbRating} from './rating/rating';
 
-export const NGB_DIRECTIVES = [NgbAccordion, NgbAccordionGroup, NgbDropdown, NgbDropdownMenu, NgbRating];
+export const NGB_DIRECTIVES = [NgbAccordion, NgbAccordionGroup, NgbDropdown, NgbRating];
--- src/dropdown/dropdown.ts
+++ src/dropdown/dropdown.ts
@@ -1,15 +1,6 @@
-import {Component, Directive, Input} from 'angular2/angular2';
+import {Directive, Input} from 'angular2/angular2';
 
-@Component({
-  selector: 'ngb-dropdown',
-  template: `<div class="dropdown" [class.open]="open">
-    <ng-content></ng-content>
-  </div>`
-})
+@Directive({selector: 'ngb-dropdown', inputs: ['open'], host: {'class': 'dropdown', '[class.open]': 'open'}})
 export class NgbDropdown {
   @Input() private open: boolean;
-}
-
-@Directive({selector: 'ngb-dropdown-menu', host: {'class': 'dropdown-menu'}})
-export class NgbDropdownMenu {
 }
