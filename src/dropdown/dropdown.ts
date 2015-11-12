6d099d51cafd0795dfd1dae598c7f9e9fc503a96
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2015-11-12 16:46:02 +0100

    fix(dropdown): don't declare inputs twice

--- src/dropdown/dropdown.ts
+++ src/dropdown/dropdown.ts
@@ -1,6 +1,6 @@
 import {Directive, Input} from 'angular2/angular2';
 
-@Directive({selector: 'ngb-dropdown', inputs: ['open'], host: {'class': 'dropdown', '[class.open]': 'open'}})
+@Directive({selector: 'ngb-dropdown', host: {'class': 'dropdown', '[class.open]': 'open'}})
 export class NgbDropdown {
   @Input() private open: boolean;
 }
