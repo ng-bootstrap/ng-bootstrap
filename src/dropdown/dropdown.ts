e7bb8c2a8f1d731bf240f46ee97f16fc05862da0
Foxandxss <foxandxss@gmail.com>
2015-11-12 16:58:12 +0100

    refactor(dropdown): update to latest conventions

--- src/dropdown/dropdown.ts
+++ src/dropdown/dropdown.ts
@@ -2,5 +2,5 @@ import {Directive, Input} from 'angular2/angular2';
 
 @Directive({selector: 'ngb-dropdown', host: {'class': 'dropdown', '[class.open]': 'open'}})
 export class NgbDropdown {
-  @Input() private open: boolean;
+  @Input() open: boolean;
 }
