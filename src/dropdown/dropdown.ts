8b53aeec282307470eb2e5964df4779523422812
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2015-11-17 11:36:12 +0100

    feat(dropdown): support dropdown and dropups
    Closes #98

--- src/dropdown/dropdown.ts
+++ src/dropdown/dropdown.ts
@@ -1,6 +1,6 @@
 import {Directive, Input} from 'angular2/angular2';
 
-@Directive({selector: 'ngb-dropdown', exportAs: 'ngbDropdown', host: {'class': 'dropdown', '[class.open]': 'open'}})
+@Directive({selector: 'ngb-dropdown', exportAs: 'ngbDropdown', host: {'[class.open]': 'open'}})
 export class NgbDropdown {
   @Input() open: boolean;
 }
