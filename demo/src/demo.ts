f1c5717e3d06a3e0b366b2bf2500f8791d06da44
Foxandxss <foxandxss@gmail.com>
2015-12-08 14:27:50 +0100

    chore: add tooling for demo

---
+++ demo/src/demo.ts
@@ -0,0 +1,12 @@
+import {Component, bootstrap} from 'angular2/angular2';
+
+@Component({
+  selector: 'demo',
+  template: `
+    <div>Hello World</div>
+  `
+})
+class Demo {
+}
+
+bootstrap(Demo);
