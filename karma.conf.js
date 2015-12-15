0a0a8e9084f6a332f39f07e1ff4bbd5842cbe821
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2015-12-15 15:37:11 +0100

    chore: update angular2 to 2.0.0-alpha.54
    Closes #139

--- karma.conf.js
+++ karma.conf.js
@@ -9,9 +9,11 @@ module.exports = function(config) {
       // For travis
       'node_modules/es6-shim/es6-shim.js',
       // paths loaded by Karma
-      {pattern: 'node_modules/systemjs/dist/system.src.js', included: true, watched: true},
+      {pattern: 'node_modules/systemjs/dist/system.src.js', included: true, watched: false},
+      {pattern: 'node_modules/angular2/bundles/angular2-polyfills.js', included: true, watched: false},
+      {pattern: 'node_modules/rxjs/bundles/Rx.js', included: true, watched: false},
       {pattern: 'node_modules/angular2/bundles/angular2.js', included: true, watched: true},
-      {pattern: 'node_modules/angular2/bundles/testing.js', included: true, watched: true},
+      {pattern: 'node_modules/angular2/bundles/testing.dev.js', included: true, watched: true},
       {pattern: 'karma-test-shim.js', included: true, watched: true},
 
       // paths loaded via module imports
