0cc38a86cf404d57fb7340b4080f87364c0516c2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2016-03-04 17:14:02 +0100

    refactor(accordion): use reusable panels

---
+++ src/accordion/panel.spec.ts
@@ -0,0 +1,136 @@
+import {
+  iit,
+  it,
+  ddescribe,
+  describe,
+  expect,
+  inject,
+  injectAsync,
+  TestComponentBuilder,
+  beforeEachProviders
+} from 'angular2/testing';
+
+import {Component} from 'angular2/core';
+
+import {NgbPanel} from './accordion';
+
+function expectToBeClosed(panelEl) {
+  expect(panelEl.querySelector('.panel')).not.toHaveCssClass('panel-open');
+  expect(panelEl.querySelector('.panel .panel-collapse')).not.toHaveCssClass('in');
+  expect(panelEl.querySelector('.panel .panel-collapse').getAttribute('aria-expanded')).toEqual('false');
+}
+
+function expectToBeOpen(panelEl) {
+  expect(panelEl.querySelector('.panel')).toHaveCssClass('panel-open');
+  expect(panelEl.querySelector('.panel .panel-collapse')).toHaveCssClass('in');
+  expect(panelEl.querySelector('.panel .panel-collapse').getAttribute('aria-expanded')).toEqual('true');
+}
+
+function expectToBeDisabled(panelEl) {
+  expect(panelEl.querySelector('.panel-title span')).toHaveCssClass('text-muted');
+}
+
+function expectToBeEnabled(panelEl) {
+  expect(panelEl.querySelector('.panel-title span')).not.toHaveCssClass('text-muted');
+}
+
+describe('ngb-panel', () => {
+
+  it('should render as a closed, enabled panel by default', injectAsync([TestComponentBuilder], (tcb) => {
+       const panelHtml = `
+      <ngb-panel title="Test title">Test content</ngb-panel>
+    `;
+
+       return tcb.overrideTemplate(TestComponent, panelHtml).createAsync(TestComponent).then((fixture) => {
+         const panelEl = fixture.nativeElement;
+
+         fixture.detectChanges();
+         expectToBeClosed(panelEl);
+         expectToBeEnabled(panelEl);
+       });
+     }));
+
+  it('should render an open pane when requested', injectAsync([TestComponentBuilder], (tcb) => {
+       const panelHtml = `
+      <ngb-panel title="Test title" [open]="true">Test content</ngb-panel>
+    `;
+
+       return tcb.overrideTemplate(TestComponent, panelHtml).createAsync(TestComponent).then((fixture) => {
+         const panelEl = fixture.nativeElement;
+
+         fixture.detectChanges();
+         expectToBeOpen(panelEl);
+       });
+     }));
+
+  it('should render a disabled pane when requested', injectAsync([TestComponentBuilder], (tcb) => {
+       const panelHtml = `
+      <ngb-panel title="Test title" [disabled]="true">Test content</ngb-panel>
+    `;
+
+       return tcb.overrideTemplate(TestComponent, panelHtml).createAsync(TestComponent).then((fixture) => {
+         const panelEl = fixture.nativeElement;
+
+         fixture.detectChanges();
+         expectToBeClosed(panelEl);
+         expectToBeDisabled(panelEl);
+       });
+     }));
+
+  it('should render an open and disabled pane when requested', injectAsync([TestComponentBuilder], (tcb) => {
+       const panelHtml = `
+      <ngb-panel title="Test title" [open]="true" [disabled]="true">Test content</ngb-panel>
+    `;
+
+       return tcb.overrideTemplate(TestComponent, panelHtml).createAsync(TestComponent).then((fixture) => {
+         const panelEl = fixture.nativeElement;
+
+         fixture.detectChanges();
+         expectToBeOpen(panelEl);
+         expectToBeDisabled(panelEl);
+       });
+     }));
+
+  it('should toggle open on header click', injectAsync([TestComponentBuilder], (tcb) => {
+       const panelHtml = `
+      <ngb-panel title="Test title">Test content</ngb-panel>
+    `;
+
+       return tcb.overrideTemplate(TestComponent, panelHtml).createAsync(TestComponent).then((fixture) => {
+         const panelEl = fixture.nativeElement;
+
+         fixture.nativeElement.querySelector('.panel a').click();
+         fixture.detectChanges();
+         expectToBeOpen(panelEl);
+
+         fixture.nativeElement.querySelector('.panel a').click();
+         fixture.detectChanges();
+         expectToBeClosed(panelEl);
+       });
+     }));
+
+  it('should not toggle open on header click if disabled', injectAsync([TestComponentBuilder], (tcb) => {
+       const panelHtml = `
+      <ngb-panel title="Test title" [disabled]="true">Test content</ngb-panel>
+    `;
+
+       return tcb.overrideTemplate(TestComponent, panelHtml).createAsync(TestComponent).then((fixture) => {
+         const panelEl = fixture.nativeElement;
+
+         fixture.detectChanges();
+         expectToBeClosed(panelEl);
+
+         fixture.nativeElement.querySelector('.panel a').click();
+         fixture.detectChanges();
+         expectToBeClosed(panelEl);
+       });
+     }));
+
+});
+
+@Component({selector: 'test-cmp', directives: [NgbPanel], template: ''})
+class TestComponent {
+  open = false;
+  disabled = false;
+  title = 'Test title';
+}
--- src/collapse/collapse.ts
+++ src/collapse/collapse.ts
@@ -3,7 +3,7 @@ import {Directive, Input} from 'angular2/core';
 @Directive({
   selector: '[ngbCollapse]',
   exportAs: 'ngbCollapse',
-  host: {'class': 'collapse', '[class.in]': '!collapsed', '[attr.aria-expanded]': '!collapsed'}
+  host: {'[class.collapse]': 'true', '[class.in]': '!collapsed', '[attr.aria-expanded]': '!collapsed'}
 })
 export class NgbCollapse {
   @Input('ngbCollapse') collapsed: boolean;
