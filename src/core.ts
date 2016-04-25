d181e0eaaa17f7a55738b94df78f13a9bbb4ac0d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2016-04-25 12:19:06 +0200

    feat(tabset): add tabset component
    Closes #7
    
    Closes #181

--- src/core.ts
+++ src/core.ts
@@ -5,6 +5,7 @@ import {NgbDropdown, NgbDropdownToggle} from './dropdown/dropdown';
 import {NgbPagination} from './pagination/pagination';
 import {NgbProgressbar} from './progressbar/progressbar';
 import {NgbRating} from './rating/rating';
+import {NgbTabset, NgbTab, NgbTabContent, NgbTabTitle} from './tabset/tabset';
 
 export {NgbAccordion, NgbPanel} from './accordion/accordion';
 export {NgbAlert} from './alert/alert';
@@ -13,8 +14,9 @@ export {NgbDropdown, NgbDropdownToggle} from './dropdown/dropdown';
 export {NgbPagination} from './pagination/pagination';
 export {NgbProgressbar} from './progressbar/progressbar';
 export {NgbRating} from './rating/rating';
+export {NgbTabset, NgbTab, NgbTabContent, NgbTabTitle} from './tabset/tabset';
 
 export const NGB_DIRECTIVES = [
   NgbAccordion, NgbAlert, NgbCollapse, NgbPanel, NgbDropdown, NgbDropdownToggle, NgbPagination, NgbProgressbar,
-  NgbRating
+  NgbRating, NgbTabset, NgbTab, NgbTabContent, NgbTabTitle
 ];
---
+++ src/tabset/tabset.ts
@@ -0,0 +1,58 @@
+import {
+  Component,
+  Input,
+  ContentChildren,
+  QueryList,
+  Directive,
+  TemplateRef,
+  ContentChild,
+  AfterContentChecked
+} from 'angular2/core';
+
+@Directive({selector: 'template[ngb-tab-title]'})
+export class NgbTabTitle {
+  constructor(public templateRef: TemplateRef) {}
+}
+
+@Directive({selector: 'template[ngb-tab-content]'})
+export class NgbTabContent {
+  constructor(public templateRef: TemplateRef) {}
+}
+
+@Directive({selector: 'ngb-tab'})
+export class NgbTab {
+  @Input() title: string;
+
+  @ContentChild(NgbTabContent) contentTpl: NgbTabContent;
+  @ContentChild(NgbTabTitle) titleTpl: NgbTabTitle;
+}
+
+@Component({
+  selector: 'ngb-tabset',
+  template: `
+    <ul class="nav nav-tabs" role="tablist">
+      <li class="nav-item" *ngFor="#tab of tabs; #i=index">
+        <a class="nav-link" [class.active]="i === activeIdx" (click)="select(i)">
+          {{tab.title}}<template [ngTemplateOutlet]="tab.titleTpl?.templateRef"></template>
+        </a>
+      </li>
+    </ul>
+    <div class="tab-content">
+      <div *ngFor="#tab of tabs; #i=index" class="tab-pane" [class.active]="i === activeIdx" role="tabpanel">
+        <template [ngTemplateOutlet]="tab.contentTpl.templateRef"></template>
+      </div>
+    </div>
+  `
+})
+export class NgbTabset implements AfterContentChecked {
+  @ContentChildren(NgbTab) tabs: QueryList<NgbTab>;
+
+  @Input() activeIdx = 0;
+
+  select(tabIdx: number) { this.activeIdx = tabIdx; }
+
+  ngAfterContentChecked() {
+    // auto-correct _activeIdx that might have been set incorrectly as input
+    this.activeIdx = Math.min(Math.max(0, this.activeIdx), this.tabs.length - 1);
+  }
+}
