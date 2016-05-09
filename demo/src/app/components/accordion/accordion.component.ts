18100f59297b0d0701b41a58471c5099ceff3293
Foxandxss <foxandxss@gmail.com>
2016-05-10 00:05:48 +0200

    docs: accordion demo

--- demo/src/app/components/accordion/accordion.component.ts
+++ demo/src/app/components/accordion/accordion.component.ts
@@ -1,24 +1,20 @@
 import { Component } from '@angular/core';
 
-import 'prismjs/themes/prism-okaidia.css';
+import { NgbTabContent, NgbTab, NgbTabset } from '../../../../../src/tabset/tabset';
+import { InstructionsItemComponent } from '../shared';
+import { AccordionBasicComponent, basicHtmlContent, basicTsContent } from './demos';
 
-// import { AccordionDemoComponent } from 'core/accordion/demo/demo.ts';
-
-// import { NgbAccordion } from '../../../../../core/src/accordion/accordion';
-
-import { NgbAccordion, NgbPanel } from '../../../../../src/accordion/accordion';
-
-// const demoMarkdown = require('./demo/demo.md');
-// const tsContent = require('!!prismjs?lang=typescript!./demo/demo.ts');
-// const htmlContent = require('!!prismjs?lang=markup!./demo/demo.html');
+const accordionDocs = require('./demos/ngb-accordion.json');
+const panelDocs = require('./demos/ngb-panel.json');
 
 @Component({
   selector: 'ngbd-accordion',
   template: require('./accordion.component.html'),
-  directives: [NgbAccordion, NgbPanel]
+  directives: [InstructionsItemComponent, NgbTab, NgbTabset, NgbTabContent, AccordionBasicComponent]
 })
 export class AccordionComponent {
-  // instructions = demoMarkdown;
-  // tsCode = tsContent;
-  // htmlCode = htmlContent;
+  accordionDocs = accordionDocs;
+  panelDocs = panelDocs;
+  basicHtmlContent = basicHtmlContent;
+  basicTsContent = basicTsContent;
 }
