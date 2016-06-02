c5b3080f7450c8283f4f2ab14b14ca1ed345291a
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2016-06-02 14:03:51 +0200

    feat(popover): introduce popover window

---
+++ src/popover/popover.ts
@@ -0,0 +1,16 @@
+import {Component, Input, ChangeDetectionStrategy} from '@angular/core';
+
+@Component({
+  selector: 'ngb-popover-window',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  host: {'[class]': '"popover " + placement', 'role': 'tooltip'},
+  template: `
+    <div class="popover-arrow"></div>
+    <h3 class="popover-title">{{title}}</h3>
+    <div class="popover-content"><ng-content></ng-content></div>
+    `
+})
+export class NgbPopoverWindow {
+  @Input() placement = 'top';
+  @Input() title: string;
+}
