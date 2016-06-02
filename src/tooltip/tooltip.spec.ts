fe70ac0ffb9932da8ceb5d30efbdeced2950acf5
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2016-06-02 14:03:32 +0200

    feat(tooltip): introduce tooltip window

---
+++ src/tooltip/tooltip.spec.ts
@@ -0,0 +1,25 @@
+import {iit, it, ddescribe, describe, expect, inject, async} from '@angular/core/testing';
+import {TestComponentBuilder} from '@angular/compiler/testing';
+
+import {NgbTooltipWindow} from './tooltip';
+
+describe('ngb-tooltip-window', () => {
+
+  it('should render tooltip on top by default', async(inject([TestComponentBuilder], (tcb: TestComponentBuilder) => {
+       tcb.createAsync(NgbTooltipWindow).then((fixture) => {
+         fixture.detectChanges();
+
+         expect(fixture.nativeElement).toHaveCssClass('tooltip');
+         expect(fixture.nativeElement).toHaveCssClass('top');
+         expect(fixture.nativeElement.getAttribute('role')).toBe('tooltip');
+       });
+     })));
+
+  it('should position tooltips as requested', async(inject([TestComponentBuilder], (tcb: TestComponentBuilder) => {
+       tcb.createAsync(NgbTooltipWindow).then((fixture) => {
+         fixture.componentInstance.placement = 'left';
+         fixture.detectChanges();
+         expect(fixture.nativeElement).toHaveCssClass('left');
+       });
+     })));
+});
