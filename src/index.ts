6708e37d27c91ef6d5b9117c78644447399eed03
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2016-06-03 17:12:07 +0200

    feat(tooltip): initial version of the tooltip directive
    Closes #8

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,8 @@ import {NgbPagination} from './pagination/pagination';
 import {NgbProgressbar} from './progressbar/progressbar';
 import {NgbRating} from './rating/rating';
 import {NgbTabset, NgbTab, NgbTabContent, NgbTabTitle} from './tabset/tabset';
-import {NgbRadioGroup, NgbRadioLabel, NgbRadio, NGB_RADIO_DIRECTIVES} from './buttons/radio';
+import {NGB_TOOLTIP_DIRECTIVES} from './tooltip/tooltip';
+import {NGB_RADIO_DIRECTIVES} from './buttons/radio';
 
 export {NgbAccordion, NgbPanel} from './accordion/accordion';
 export {NgbAlert} from './alert/alert';
@@ -18,10 +19,10 @@ export {NgbPagination} from './pagination/pagination';
 export {NgbProgressbar} from './progressbar/progressbar';
 export {NgbRating} from './rating/rating';
 export {NgbTabset, NgbTab, NgbTabContent, NgbTabTitle} from './tabset/tabset';
+export {NgbTooltip, NgbTooltipWindow, NGB_TOOLTIP_DIRECTIVES} from './tooltip/tooltip';
 export {NgbRadioGroup, NgbRadioLabel, NgbRadio, NGB_RADIO_DIRECTIVES} from './buttons/radio';
 
 export const NGB_DIRECTIVES = [
   NgbAccordion, NgbAlert, NGB_CAROUSEL_DIRECTIVES, NgbCollapse, NgbPanel, NgbDropdown, NgbDropdownToggle, NgbPagination,
-  NgbProgressbar, NgbRating, NgbTabset, NgbTab, NgbTabContent, NgbTabTitle, NgbRadioGroup, NgbRadio, NgbRadioLabel,
-  NGB_RADIO_DIRECTIVES
+  NgbProgressbar, NgbRating, NgbTabset, NgbTab, NgbTabContent, NgbTabTitle, NGB_TOOLTIP_DIRECTIVES, NGB_RADIO_DIRECTIVES
 ];
