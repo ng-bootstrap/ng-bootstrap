e1da62e1aff4567dd87344701e333539176fbe4e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2016-06-23 14:21:58 +0200

    docs(alert): add API docs
    Closes #254

--- src/alert/alert.ts
+++ src/alert/alert.ts
@@ -1,5 +1,8 @@
 import {Component, Input, Output, EventEmitter, ChangeDetectionStrategy} from '@angular/core';
 
+/**
+ * Alerts can be used to provide feedback messages.
+ */
 @Component({
   selector: 'ngb-alert',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -13,8 +16,18 @@ import {Component, Input, Output, EventEmitter, ChangeDetectionStrategy} from '@
     `
 })
 export class NgbAlert {
+  /**
+   * A flag indicating if a given alert can be dismissed (closed) by a user. If this flag is set, a close button (in a
+   * form of a cross) will be displayed.
+   */
   @Input() dismissible: boolean = true;
+  /**
+   * Alert type (CSS class). Bootstrap 4 recognizes the following types: "success", "info", "warning" and "danger".
+  */
   @Input() type: string = 'warning';
+  /**
+   * An event emitted when the close button is clicked. This event has no payload. Only relevant for dismissible alerts.
+   */
   @Output() close = new EventEmitter();
 
   closeHandler() { this.close.emit(null); }
