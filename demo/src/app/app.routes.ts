84abcb4fd63a37bee8d7aebcf61136d1055c867d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2016-06-24 14:58:53 +0200

    docs(demo): add API docs for buttons (#260)

--- demo/src/app/app.routes.ts
+++ demo/src/app/app.routes.ts
@@ -1,13 +1,14 @@
 import { provideRouter, RouterConfig } from '@angular/router';
 
 import { HomeComponent } from './home';
-import { NgbdAccordion, NgbdAlert } from './components';
+import { NgbdAccordion, NgbdAlert, NgbdButtons } from './components';
 
 const routes: RouterConfig = [
   { path: '', redirectTo: 'home' },
   { path: 'home', component: HomeComponent },
   { path: 'components/accordion', component: NgbdAccordion },
-  { path: 'components/alert', component: NgbdAlert }
+  { path: 'components/alert', component: NgbdAlert },
+  { path: 'components/buttons', component: NgbdButtons }
 ];
 
 export const APP_ROUTER_PROVIDERS = [
