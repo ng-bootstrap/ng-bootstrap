a620ab8660f68718d56c65de873bae32b3f031fa
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2016-06-24 15:18:16 +0200

    fix(tabset): use user-supplied tab indexes
    Fixes #215
    Fixes #230
    Closes #262

--- src/tabset/tabset.ts
+++ src/tabset/tabset.ts
@@ -9,6 +9,8 @@ import {
   AfterContentChecked
 } from '@angular/core';
 
+let nextId = 0;
+
 @Directive({selector: 'template[ngbTabTitle]'})
 export class NgbTabTitle {
   constructor(public templateRef: TemplateRef<any>) {}
@@ -19,27 +21,43 @@ export class NgbTabContent {
   constructor(public templateRef: TemplateRef<any>) {}
 }
 
+/**
+ * A directive representing an individual tab.
+ */
 @Directive({selector: 'ngb-tab'})
 export class NgbTab {
+  /**
+   * Unique tab identifier. Must be unique for the entire document for proper accessibility support.
+   */
+  @Input() id: string = `ngb-tab-${nextId++}`;
+  /**
+   * Simple (string only) title. Use the "NgbTabTitle" directive for more complex use-cases.
+   */
   @Input() title: string;
+  /**
+   * Allows toggling disabled state of a given state. Disabled tabs can't be selected.
+   */
   @Input() disabled: boolean = false;
 
   @ContentChild(NgbTabContent) contentTpl: NgbTabContent;
   @ContentChild(NgbTabTitle) titleTpl: NgbTabTitle;
 }
 
+/**
+ * A component that makes it easy to create tabbed interface.
+ */
 @Component({
   selector: 'ngb-tabset',
   template: `
     <ul class="nav nav-tabs" role="tablist">
-      <li class="nav-item" *ngFor="let tab of tabs; let i=index">
-        <a class="nav-link" [class.active]="i === activeIdx" [class.disabled]="tab.disabled" (click)="select(i)">
+      <li class="nav-item" *ngFor="let tab of tabs">
+        <a [id]="tab.id" class="nav-link" [class.active]="tab.id === activeId" [class.disabled]="tab.disabled" (click)="select(tab.id)">
           {{tab.title}}<template [ngTemplateOutlet]="tab.titleTpl?.templateRef"></template>
         </a>
       </li>
     </ul>
     <div class="tab-content">
-      <div *ngFor="let tab of tabs; let i=index" class="tab-pane" [class.active]="i === activeIdx" role="tabpanel">
+      <div *ngFor="let tab of tabs" class="tab-pane" [class.active]="tab.id === activeId" role="tabpanel" [attr.aria-labelledby]="tab.id">
         <template [ngTemplateOutlet]="tab.contentTpl.templateRef"></template>
       </div>
     </div>
@@ -48,18 +66,27 @@ export class NgbTab {
 export class NgbTabset implements AfterContentChecked {
   @ContentChildren(NgbTab) tabs: QueryList<NgbTab>;
 
-  @Input() activeIdx: number = 0;
+  /**
+   * An identifier of a tab that should be selected (active).
+   */
+  @Input() activeId: string;
 
-  select(tabIdx: number) {
-    if (!this.tabs.toArray()[tabIdx].disabled) {
-      this.activeIdx = tabIdx;
+  select(tabIdx: string) {
+    let selectedTab = this._getTabById(tabIdx);
+    if (selectedTab && !selectedTab.disabled) {
+      this.activeId = selectedTab.id;
     }
   }
 
   ngAfterContentChecked() {
-    // auto-correct _activeIdx that might have been set incorrectly as input
-    const activeIdx = Math.min(Math.max(0, this.activeIdx), this.tabs.length - 1);
-    this.activeIdx = this.tabs.toArray()[activeIdx] ? activeIdx : 0;
+    // auto-correct activeId that might have been set incorrectly as input
+    let activeTab = this._getTabById(this.activeId);
+    this.activeId = activeTab ? activeTab.id : (this.tabs.length ? this.tabs.first.id : null);
+  }
+
+  private _getTabById(id: string): NgbTab {
+    let tabsWithId: NgbTab[] = this.tabs.filter(tab => tab.id === id);
+    return tabsWithId.length ? tabsWithId[0] : null;
   }
 }
 
