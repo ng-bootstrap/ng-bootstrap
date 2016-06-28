592a57805360ebe0b244b79d06c79f934079cd04
robjacobs <rob.jacobs@msn.com>
2016-06-28 15:17:28 -0400

    feat(positioning): add positioning service
    Closes #289

---
+++ src/util/positioning.spec.ts
@@ -0,0 +1,263 @@
+import {it, describe, expect, beforeEach, afterEach} from '@angular/core/testing';
+
+import {Positioning} from './positioning';
+
+describe('Positioning', () => {
+  const positioning = new Positioning();
+  const documentMargin = document.documentElement.style.margin;
+  const bodyMargin = document.body.style.margin;
+  const bodyHeight = document.body.style.height;
+  const bodyWidth = document.body.style.width;
+
+  function createElement(height: number, width: number, marginTop: number, marginLeft: number): HTMLElement {
+    let element = document.createElement('div');
+    element.style.display = 'inline-block';
+    element.style.height = height + 'px';
+    element.style.width = width + 'px';
+    element.style.marginTop = marginTop + 'px';
+    element.style.marginLeft = marginLeft + 'px';
+
+    return element;
+  }
+
+  let cleanUp = false;
+  let element = createElement(200, 300, 100, 150);
+
+  document.documentElement.style.margin = '0';
+  document.body.style.margin = '0';
+  document.body.style.height = '2000px';
+  document.body.style.width = '2000px';
+
+  beforeEach(() => {
+    if (!cleanUp) {
+      document.body.appendChild(element);
+    }
+  });
+
+  afterEach(() => {
+    if (!cleanUp) {
+      document.body.removeChild(element);
+    }
+  });
+
+  it('should calculate the element offset', () => {
+    let position = positioning.offset(element);
+
+    expect(position.height).toBe(200);
+    expect(position.width).toBe(300);
+    expect(position.top).toBe(100);
+    expect(position.bottom).toBe(300);
+    expect(position.left).toBe(150);
+    expect(position.right).toBe(450);
+  });
+
+  it('should calculate the element offset when scrolled', () => {
+    document.documentElement.scrollTop = 1000;
+    document.documentElement.scrollLeft = 1000;
+
+    let position = positioning.offset(element);
+
+    expect(position.top).toBe(100);
+    expect(position.bottom).toBe(300);
+    expect(position.left).toBe(150);
+    expect(position.right).toBe(450);
+
+    document.documentElement.scrollTop = 0;
+    document.documentElement.scrollLeft = 0;
+  });
+
+  it('should calculate the element position', () => {
+    let position = positioning.position(element);
+
+    expect(position.height).toBe(200);
+    expect(position.width).toBe(300);
+    expect(position.top).toBe(100);
+    expect(position.bottom).toBe(300);
+    expect(position.left).toBe(150);
+    expect(position.right).toBe(450);
+  });
+
+  it('should calculate the element position when scrolled', () => {
+    document.documentElement.scrollTop = 1000;
+    document.documentElement.scrollLeft = 1000;
+
+    let position = positioning.position(element);
+
+    expect(position.top).toBe(100);
+    expect(position.bottom).toBe(300);
+    expect(position.left).toBe(150);
+    expect(position.right).toBe(450);
+
+    document.documentElement.scrollTop = 0;
+    document.documentElement.scrollLeft = 0;
+  });
+
+  it('should calculate the element position on positioned ancestor', () => {
+    let childElement = createElement(100, 150, 50, 75);
+
+    element.style.position = 'relative';
+    element.appendChild(childElement);
+
+    let position = positioning.position(childElement);
+
+    expect(position.top).toBe(50);
+    expect(position.bottom).toBe(150);
+    expect(position.left).toBe(75);
+    expect(position.right).toBe(225);
+
+    element.style.position = '';
+    element.removeChild(childElement);
+  });
+
+  it('should position the element top-left', () => {
+    let targetElement = createElement(50, 100, 10, 20);
+    document.body.appendChild(targetElement);
+
+    let position = positioning.positionElements(element, targetElement, 'top-left');
+
+    expect(position.top).toBe(50);
+    expect(position.left).toBe(150);
+
+    document.body.removeChild(targetElement);
+  });
+
+  it('should position the element top-center', () => {
+    let targetElement = createElement(50, 100, 10, 20);
+    document.body.appendChild(targetElement);
+
+    let position = positioning.positionElements(element, targetElement, 'top');
+
+    expect(position.top).toBe(50);
+    expect(position.left).toBe(250);
+
+    document.body.removeChild(targetElement);
+  });
+
+  it('should position the element top-right', () => {
+    let targetElement = createElement(50, 100, 10, 20);
+    document.body.appendChild(targetElement);
+
+    let position = positioning.positionElements(element, targetElement, 'top-right');
+
+    expect(position.top).toBe(50);
+    expect(position.left).toBe(450);
+
+    document.body.removeChild(targetElement);
+  });
+
+  it('should position the element bottom-left', () => {
+    let targetElement = createElement(50, 100, 10, 20);
+    document.body.appendChild(targetElement);
+
+    let position = positioning.positionElements(element, targetElement, 'bottom-left');
+
+    expect(position.top).toBe(300);
+    expect(position.left).toBe(150);
+
+    document.body.removeChild(targetElement);
+  });
+
+  it('should position the element bottom-center', () => {
+    let targetElement = createElement(50, 100, 10, 20);
+    document.body.appendChild(targetElement);
+
+    let position = positioning.positionElements(element, targetElement, 'bottom');
+
+    expect(position.top).toBe(300);
+    expect(position.left).toBe(250);
+
+    document.body.removeChild(targetElement);
+  });
+
+  it('should position the element bottom-right', () => {
+    let targetElement = createElement(50, 100, 10, 20);
+    document.body.appendChild(targetElement);
+
+    let position = positioning.positionElements(element, targetElement, 'bottom-right');
+
+    expect(position.top).toBe(300);
+    expect(position.left).toBe(450);
+
+    document.body.removeChild(targetElement);
+  });
+
+  it('should position the element left-top', () => {
+    let targetElement = createElement(50, 100, 10, 20);
+    document.body.appendChild(targetElement);
+
+    let position = positioning.positionElements(element, targetElement, 'left-top');
+
+    expect(position.top).toBe(100);
+    expect(position.left).toBe(50);
+
+    document.body.removeChild(targetElement);
+  });
+
+  it('should position the element left-center', () => {
+    let targetElement = createElement(50, 100, 10, 20);
+    document.body.appendChild(targetElement);
+
+    let position = positioning.positionElements(element, targetElement, 'left');
+
+    expect(position.top).toBe(175);
+    expect(position.left).toBe(50);
+
+    document.body.removeChild(targetElement);
+  });
+
+  it('should position the element left-bottom', () => {
+    let targetElement = createElement(50, 100, 10, 20);
+    document.body.appendChild(targetElement);
+
+    let position = positioning.positionElements(element, targetElement, 'left-bottom');
+
+    expect(position.top).toBe(300);
+    expect(position.left).toBe(50);
+
+    document.body.removeChild(targetElement);
+  });
+
+  it('should position the element right-top', () => {
+    let targetElement = createElement(50, 100, 10, 20);
+    document.body.appendChild(targetElement);
+
+    let position = positioning.positionElements(element, targetElement, 'right-top');
+
+    expect(position.top).toBe(100);
+    expect(position.left).toBe(450);
+
+    document.body.removeChild(targetElement);
+  });
+
+  it('should position the element right-center', () => {
+    let targetElement = createElement(50, 100, 10, 20);
+    document.body.appendChild(targetElement);
+
+    let position = positioning.positionElements(element, targetElement, 'right');
+
+    expect(position.top).toBe(175);
+    expect(position.left).toBe(450);
+
+    document.body.removeChild(targetElement);
+  });
+
+  it('should position the element right-bottom', () => {
+    let targetElement = createElement(50, 100, 10, 20);
+    document.body.appendChild(targetElement);
+
+    let position = positioning.positionElements(element, targetElement, 'right-bottom');
+
+    expect(position.top).toBe(300);
+    expect(position.left).toBe(450);
+
+    document.body.removeChild(targetElement);
+  });
+
+  it('cleanUp', () => {
+    cleanUp = true;
+    document.documentElement.style.margin = documentMargin;
+    document.body.style.margin = bodyMargin;
+    document.body.style.height = bodyHeight;
+    document.body.style.width = bodyWidth;
+  });
+});
