d0918216d5b0b8995e7a53bffba2a13a3c5a4952
Wesley Cho <wesley.cho@gmail.com>
2016-06-28 21:18:35 -0700

    feat(progresbar): add animation support
    - Add support for animation via `.progress-animated` class

--- src/progressbar/progressbar.ts
+++ src/progressbar/progressbar.ts
@@ -5,7 +5,8 @@ import {getValueInRange, toBoolean} from '../util/util';
   selector: 'ngb-progressbar',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `
-    <progress class="progress {{isStriped() && 'progress-striped'}} {{type && 'progress-' + type}}" [value]="getValue()" [max]="max">
+    <progress class="progress {{isAnimated() && 'progress-animated'}} {{isStriped() && 'progress-striped'}} {{type && 'progress-' + type}}"
+      [value]="getValue()" [max]="max">
       <div class="progress">
         <span class="progress-bar" [style.width.%]="getPercentValue()"><ng-content></ng-content></span>
       </div>
@@ -14,9 +15,12 @@ import {getValueInRange, toBoolean} from '../util/util';
 })
 export class NgbProgressbar {
   @Input() max: number = 100;
+  @Input() animated: boolean | string;
   @Input() striped: boolean | string;
   @Input() type: string;
   @Input() value: number;
+
+  isAnimated(): boolean { return toBoolean(this.animated); }
 
   isStriped(): boolean { return toBoolean(this.striped); }
 
