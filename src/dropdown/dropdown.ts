222d98f8288bbc86a64bb0b0f9be05a9420f746f
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2016-06-29 16:36:29 +0200

    feat(dropdown): add autoClose support
    Fixes #169
    
    Closes #302

--- src/dropdown/dropdown.ts
+++ src/dropdown/dropdown.ts
@@ -1,23 +1,56 @@
 import {Directive, Input, HostListener} from '@angular/core';
 
-@Directive(
-    {selector: '[ngbDropdown]', exportAs: 'ngbDropdown', host: {'[class.open]': 'open', '(keyup.esc)': 'close()'}})
+/**
+ * Transforms a node into a dropdown.
+ */
+@Directive({
+  selector: '[ngbDropdown]',
+  exportAs: 'ngbDropdown',
+  host: {'[class.open]': '_open', '(keyup.esc)': '_closeFromOutside()', '(document:click)': '_closeFromOutside()'}
+})
 export class NgbDropdown {
-  @Input() open: boolean = false;
+  /**
+   * Indicates that dropdown should be closed when selecting one of dropdown items (click) or pressing ESC.
+   */
+  @Input() autoClose = true;
 
-  close() { this.open = false; }
+  /**
+   *  Defines whether or not the dropdown-menu is open initially.
+   */
+  @Input('open') private _open = false;
+
+  isOpen() { return this._open; }
+  open() { this._open = true; }
+  close() { this._open = false; }
+  toggle() {
+    if (this.isOpen()) {
+      this.close();
+    } else {
+      this.open();
+    }
+  }
+
+  private _closeFromOutside() {
+    if (this.autoClose) {
+      this.close();
+    }
+  }
 }
 
+/**
+ * Allows the dropdown to be toggled via click. This directive is optional.
+ */
 @Directive({
   selector: '[ngbDropdownToggle]',
-  host: {'class': 'dropdown-toggle', 'aria-haspopup': 'true', '[attr.aria-expanded]': '_dropdown.open'}
+  host: {'class': 'dropdown-toggle', 'aria-haspopup': 'true', '[attr.aria-expanded]': '_dropdown.isOpen()'}
 })
 export class NgbDropdownToggle {
   constructor(private _dropdown: NgbDropdown) {}
 
-  @HostListener('click')
-  toggleOpen() {
-    this._dropdown.open = !this._dropdown.open;
+  @HostListener('click', ['$event'])
+  toggleOpen($event) {
+    $event.stopPropagation();
+    this._dropdown.toggle();
   }
 }
 
