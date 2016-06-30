bb953f8bf3971723e4eb3a63f98d5d3514f621e7
Foxandxss <foxandxss@gmail.com>
2016-07-01 01:15:27 +0200

    chore: fix clang

--- src/util/matchers.ts
+++ src/util/matchers.ts
@@ -2,11 +2,11 @@ import {getDOM} from '@angular/platform-browser/src/dom/dom_adapter';
 
 beforeEach(() => {
   jasmine.addMatchers({
-    toHaveCssClass: function (util, customEqualityTests) {
-      return { compare: buildError(false), negativeCompare: buildError(true) };
+    toHaveCssClass: function(util, customEqualityTests) {
+      return {compare: buildError(false), negativeCompare: buildError(true)};
 
       function buildError(isNot: any /** TODO #???? */) {
-        return function (actual: any /** TODO #???? */, className: any /** TODO #???? */) {
+        return function(actual: any /** TODO #???? */, className: any /** TODO #???? */) {
           return {
             pass: getDOM().hasClass(actual, className) === !isNot,
             get message() {
