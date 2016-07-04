9155af43a0e4cffa7c999dd1e6dca6b6d9bf2971
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2016-07-04 17:43:11 +0200

    docs(demo): add tooltip basic demo
    Closes #348

---
+++ demo/src/app/components/tooltip/demos/index.ts
@@ -0,0 +1,9 @@
+import {NgbdTooltipBasic} from './basic/tooltip-basic';
+
+export const DEMO_DIRECTIVES = [NgbdTooltipBasic];
+
+export const DEMO_SNIPPETS = {
+  "basic": {
+    "code": require('!!prismjs?lang=typescript!./basic/tooltip-basic'),
+    "markup": require('!!prismjs?lang=markup!./basic/tooltip-basic.html')}
+};
