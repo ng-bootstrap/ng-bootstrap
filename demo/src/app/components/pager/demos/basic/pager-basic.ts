e9430464ab00538af93867cb929ee493958d9ed2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2016-07-05 13:26:17 +0200

    docs(demo): add demo for pager directive

---
+++ demo/src/app/components/pager/demos/basic/pager-basic.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+import {NGB_PAGER_DIRECTIVES} from '@ng-bootstrap/pager';
+
+@Component({
+  selector: 'ngbd-pager-basic',
+  template: require('./pager-basic.html'),
+  directives: [NGB_PAGER_DIRECTIVES]
+})
+export class NgbdPagerBasic {
+  currentPage = 0;
+}
