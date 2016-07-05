6a86f20b4da6b65323edcedf61e8bb0796bea60d
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2016-07-05 13:26:17 +0200

    docs(demo): add demo for pager directive
    Closes #357

---
+++ demo/src/app/components/pager/demos/align/pager-align.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+import {NGB_PAGER_DIRECTIVES} from '@ng-bootstrap/pager';
+
+@Component({
+  selector: 'ngbd-pager-align',
+  template: require('./pager-align.html'),
+  directives: [NGB_PAGER_DIRECTIVES]
+})
+export class NgbdPagerAlign {
+  currentPage = 0;
+}
