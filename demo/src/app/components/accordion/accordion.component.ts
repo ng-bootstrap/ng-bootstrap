073149dfc5c2cba632d823a6f9ed45b88c9226c9
Florent EXBRAYAT <florent.exbrayat@amadeus.com>
2016-07-06 11:49:12 +0200

    demo(accordion): add basic accordion demo, clean previous ex
    Closes #369

--- demo/src/app/components/accordion/accordion.component.ts
+++ demo/src/app/components/accordion/accordion.component.ts
@@ -2,14 +2,16 @@ import {Component} from '@angular/core';
 
 import {ContentWrapper} from '../../shared';
 import {ExampleBoxComponent, NgbdApiDocs} from '../shared';
-import {AccordionBasicComponent, basicHtmlContent, basicTsContent} from './demos';
+import {AccordionBasicComponent, basicHtmlContent, basicTsContent, AccordionStaticComponent, staticHtmlContent, staticTsContent} from './demos';
 
 @Component({
   selector: 'ngbd-accordion',
   template: require('./accordion.component.html'),
-  directives: [ContentWrapper, AccordionBasicComponent, NgbdApiDocs, ExampleBoxComponent]
+  directives: [ContentWrapper, AccordionBasicComponent, AccordionStaticComponent, NgbdApiDocs, ExampleBoxComponent]
 })
 export class NgbdAccordion {
   basicHtmlContent = basicHtmlContent;
   basicTsContent = basicTsContent;
+  staticHtmlContent = staticHtmlContent;
+  staticTsContent = staticTsContent;
 }
---
+++ demo/src/app/components/accordion/demos/static/index.ts
@@ -0,0 +1,3 @@
+export * from './accordion-static.ts';
+export const staticTsContent = require('!!prismjs?lang=typescript!./accordion-static.ts');
+export const staticHtmlContent = require('!!prismjs?lang=markup!./accordion-static.html');
