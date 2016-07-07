32c251e1aeb683f16a66ef2b1fce2f8bf23a6fce
robjacobs <rob.jacobs@msn.com>
2016-07-07 10:47:22 -0400

    feat(positioning): implement rounding
    Closes #388
    
    Closes #388

--- src/util/positioning.ts
+++ src/util/positioning.ts
@@ -7,7 +7,7 @@ export class Positioning {
     return (this.getStyle(element, 'position') || 'static') === 'static';
   }
 
-  private parentOffsetElement(element: HTMLElement): HTMLElement {
+  private offsetParent(element: HTMLElement): HTMLElement {
     let offsetParentEl = <HTMLElement>element.offsetParent || document.documentElement;
 
     while (offsetParentEl && offsetParentEl !== document.documentElement && this.isStaticPositioned(offsetParentEl)) {
@@ -17,44 +17,73 @@ export class Positioning {
     return offsetParentEl || document.documentElement;
   }
 
-  position(element: HTMLElement): ClientRect {
-    const elOffset = this.offset(element);
-    const offsetParentEl = this.parentOffsetElement(element);
-
+  position(element: HTMLElement, round = true): ClientRect {
+    let elPosition: ClientRect;
     let parentOffset: ClientRect = {width: 0, height: 0, top: 0, bottom: 0, left: 0, right: 0};
 
-    if (offsetParentEl !== document.documentElement) {
-      parentOffset = this.offset(offsetParentEl);
-      parentOffset.top += offsetParentEl.clientTop - offsetParentEl.scrollTop;
-      parentOffset.left += offsetParentEl.clientLeft - offsetParentEl.scrollLeft;
+    if (this.getStyle(element, 'position') === 'fixed') {
+      elPosition = element.getBoundingClientRect();
+    } else {
+      const offsetParentEl = this.offsetParent(element);
+
+      elPosition = this.offset(element, false);
+
+      if (offsetParentEl !== document.documentElement) {
+        parentOffset = this.offset(offsetParentEl, false);
+      }
+
+      parentOffset.top += offsetParentEl.clientTop;
+      parentOffset.left += offsetParentEl.clientLeft;
     }
 
-    return {
-      height: elOffset.height,
-      width: elOffset.width,
-      top: elOffset.top - parentOffset.top,
-      bottom: elOffset.bottom - parentOffset.top,
-      left: elOffset.left - parentOffset.left,
-      right: elOffset.right - parentOffset.left
-    };
+    elPosition.top -= parentOffset.top;
+    elPosition.bottom -= parentOffset.top;
+    elPosition.left -= parentOffset.left;
+    elPosition.right -= parentOffset.left;
+
+    if (round) {
+      elPosition.height = Math.round(elPosition.height);
+      elPosition.width = Math.round(elPosition.width);
+      elPosition.top = Math.round(elPosition.top);
+      elPosition.bottom = Math.round(elPosition.bottom);
+      elPosition.left = Math.round(elPosition.left);
+      elPosition.right = Math.round(elPosition.right);
+    }
+
+    return elPosition;
   }
 
-  offset(element: HTMLElement): ClientRect {
+  offset(element: HTMLElement, round = true): ClientRect {
     const elBcr = element.getBoundingClientRect();
+    const viewportOffset = {
+      top: window.pageYOffset - document.documentElement.clientTop,
+      left: window.pageXOffset - document.documentElement.clientLeft
+    };
 
-    return {
+    let elOffset = {
       height: elBcr.height || element.offsetHeight,
       width: elBcr.width || element.offsetWidth,
-      top: elBcr.top + (window.pageYOffset || document.documentElement.scrollTop),
-      bottom: elBcr.bottom + (window.pageYOffset || document.documentElement.scrollTop),
-      left: elBcr.left + (window.pageXOffset || document.documentElement.scrollLeft),
-      right: elBcr.right + (window.pageXOffset || document.documentElement.scrollLeft)
+      top: elBcr.top + viewportOffset.top,
+      bottom: elBcr.bottom + viewportOffset.top,
+      left: elBcr.left + viewportOffset.left,
+      right: elBcr.right + viewportOffset.left
     };
+
+    if (round) {
+      elOffset.height = Math.round(elOffset.height);
+      elOffset.width = Math.round(elOffset.width);
+      elOffset.top = Math.round(elOffset.top);
+      elOffset.bottom = Math.round(elOffset.bottom);
+      elOffset.left = Math.round(elOffset.left);
+      elOffset.right = Math.round(elOffset.right);
+    }
+
+    return elOffset;
   }
 
   positionElements(hostElement: HTMLElement, targetElement: HTMLElement, placement: string, appendToBody?: boolean):
       ClientRect {
-    const hostElPosition = appendToBody ? this.offset(hostElement) : this.position(hostElement);
+    const hostElPosition = appendToBody ? this.offset(hostElement, false) : this.position(hostElement, false);
     const shiftWidth: any = {
       left: hostElPosition.left,
       center: hostElPosition.left + hostElPosition.width / 2 - targetElement.offsetWidth / 2,
@@ -104,6 +133,13 @@ export class Positioning {
         targetElPosition.right += shiftWidth[placementPrimary];
         break;
     }
+
+    targetElPosition.height = Math.round(targetElPosition.height);
+    targetElPosition.width = Math.round(targetElPosition.width);
+    targetElPosition.top = Math.round(targetElPosition.top);
+    targetElPosition.bottom = Math.round(targetElPosition.bottom);
+    targetElPosition.left = Math.round(targetElPosition.left);
+    targetElPosition.right = Math.round(targetElPosition.right);
 
     return targetElPosition;
   }
