f1b07c96324b2691ce9598771eaeb2a6aae4de36
Max Okorokov <max.okorokov@gmail.com>
2016-07-12 10:37:24 +0200

    demo(accordion): more accordion demos

--- demo/src/app/components/accordion/demos/index.ts
+++ demo/src/app/components/accordion/demos/index.ts
@@ -1,13 +1,26 @@
 import {NgbdAccordionBasic} from './basic/accordion-basic';
+import {NgbdAccordionPreventChange} from './preventChange/accordion-preventChange';
 import {NgbdAccordionStatic} from './static/accordion-static';
+import {NgbdAccordionToggle} from './toggle/accordion-toggle';
 
-export const DEMO_DIRECTIVES = [NgbdAccordionBasic, NgbdAccordionStatic];
+export const DEMO_DIRECTIVES =
+    [NgbdAccordionBasic, NgbdAccordionPreventChange, NgbdAccordionStatic, NgbdAccordionToggle];
 
 export const DEMO_SNIPPETS = {
   "basic": {
     "code": require('!!prismjs?lang=typescript!./basic/accordion-basic'),
-    "markup": require('!!prismjs?lang=markup!./basic/accordion-basic.html')},
+    "markup": require('!!prismjs?lang=markup!./basic/accordion-basic.html')
+  },
+  "preventChange": {
+    "code": require('!!prismjs?lang=typescript!./preventChange/accordion-preventChange'),
+    "markup": require('!!prismjs?lang=markup!./preventChange/accordion-preventChange.html')
+  },
   "static": {
     "code": require('!!prismjs?lang=typescript!./static/accordion-static'),
-    "markup": require('!!prismjs?lang=markup!./static/accordion-static.html')}
+    "markup": require('!!prismjs?lang=markup!./static/accordion-static.html')
+  },
+  "toggle": {
+    "code": require('!!prismjs?lang=typescript!./toggle/accordion-toggle'),
+    "markup": require('!!prismjs?lang=markup!./toggle/accordion-toggle.html')
+  }
 };
