ddc53e84eb4f0f50a9dff188a295b384417a3812
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2016-07-22 14:35:04 +0200

    feat(typeahead): add support for custom templates for search results

--- src/typeahead/typeahead-window.ts
+++ src/typeahead/typeahead-window.ts
@@ -1,27 +1,35 @@
-import {Component, Input, Output, EventEmitter} from '@angular/core';
+import {Component, Input, Output, EventEmitter, TemplateRef} from '@angular/core';
 import {NgbHighlight} from './highlight';
+
+function toStringFormatter(result) {
+  return `${result}`;
+}
+
+class ResultTplCtx {
+  constructor(public result, public term: string, public formatter: (result) => string) {}
+}
 
 @Component({
   selector: 'ngb-typeahead-window',
   exportAs: 'ngbTypeaheadWindow',
   template: `
+    <template #rt let-result="result" let-term="term" let-formatter="formatter">
+      <ngb-highlight [result]="formatter(result)" [term]="term"></ngb-highlight>
+    </template>
     <div class="dropdown-menu" aria-labelledby="dropdownMenu1">
       <template ngFor [ngForOf]="results" let-result let-idx="index">
-        <a class="dropdown-item" [class.active]="idx === activeIdx" 
+        <a class="dropdown-item" [class.active]="idx === _activeIdx" 
           (mouseenter)="markActive(idx)" 
-          (click)="select(result)"><ngb-highlight [result]="formatter(result)" [term]="term"></ngb-highlight></a>
+          (click)="select(result)">
+            <template [ngTemplateOutlet]="resultTemplate || rt" [ngOutletContext]="_prepareTplCtx(result)"></template>
+        </a>
       </template>
     </div>
   `,
   directives: [NgbHighlight]
 })
 export class NgbTypeaheadWindow {
-  private activeIdx = 0;
-
-  /**
-   * Event raised when users selects a particular result row.
-   */
-  @Output('select') selectEvent = new EventEmitter();
+  private _activeIdx = 0;
 
   /**
    * Typeahead match results to be displayed
@@ -37,14 +45,25 @@ export class NgbTypeaheadWindow {
    * A function used to format a given result before display. This function should return a formated string without any
    * HTML markup.
    */
-  @Input() formatter = (result) => { return `${result}`; };
+  @Input() formatter = toStringFormatter;
 
+  /**
+   * A template to display a matching result.
+   */
+  @Input() resultTemplate: TemplateRef<ResultTplCtx>;
 
-  markActive(activeIdx: number) { this.activeIdx = activeIdx; }
+  /**
+   * Event raised when users selects a particular result row.
+   */
+  @Output('select') selectEvent = new EventEmitter();
 
-  next() { this.activeIdx = (this.activeIdx + 1) % this.results.length; }
+  markActive(_activeIdx: number) { this._activeIdx = _activeIdx; }
 
-  prev() { this.activeIdx = (this.activeIdx === 0 ? this.results.length - 1 : this.activeIdx - 1); }
+  next() { this._activeIdx = (this._activeIdx + 1) % this.results.length; }
+
+  prev() { this._activeIdx = (this._activeIdx === 0 ? this.results.length - 1 : this._activeIdx - 1); }
 
   select(item) { this.selectEvent.emit(item); }
+
+  private _prepareTplCtx(result: any) { return new ResultTplCtx(result, this.term, this.formatter); }
 }
