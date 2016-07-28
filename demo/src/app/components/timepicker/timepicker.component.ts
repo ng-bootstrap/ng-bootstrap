27150c88d07c81996a00a3d02015531762018a04
Max Okorokov <max.okorokov@gmail.com>
2016-07-28 16:28:12 +0200

    demo(timepicker): custom validator demo
    Closes #498

--- demo/src/app/components/timepicker/timepicker.component.ts
+++ demo/src/app/components/timepicker/timepicker.component.ts
@@ -20,6 +20,9 @@ import {DEMO_DIRECTIVES, DEMO_SNIPPETS} from './demos';
       <ngbd-example-box demoTitle="Custom steps" [htmlSnippet]="snippets.steps.markup" [tsSnippet]="snippets.steps.code">
         <ngbd-timepicker-steps></ngbd-timepicker-steps>      
       </ngbd-example-box>
+      <ngbd-example-box demoTitle="Custom validation" [htmlSnippet]="snippets.validation.markup" [tsSnippet]="snippets.validation.code">
+        <ngbd-timepicker-validation></ngbd-timepicker-validation>      
+      </ngbd-example-box>      
     </ngbd-content-wrapper>
   `,
   directives: [ContentWrapper, NgbdApiDocs, DEMO_DIRECTIVES, ExampleBoxComponent]
