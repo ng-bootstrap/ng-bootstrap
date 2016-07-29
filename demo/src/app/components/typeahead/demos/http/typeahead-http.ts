2de82d3b340871381b415bf579f63f9ba1cfb2ba
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2016-07-29 16:24:53 +0200

    demo: fix typeahead wikipedia demo
    Closes #514

--- demo/src/app/components/typeahead/demos/http/typeahead-http.ts
+++ demo/src/app/components/typeahead/demos/http/typeahead-http.ts
@@ -15,7 +15,7 @@ export class WikipediaService {
   constructor(private _jsonp: Jsonp) {}
 
   search(term: string) {
-    let wikiUrl = 'http://en.wikipedia.org/w/api.php';
+    let wikiUrl = 'https://en.wikipedia.org/w/api.php';
     let params = new URLSearchParams();
     params.set('search', term);
     params.set('action', 'opensearch');
