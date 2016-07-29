2a0ec99285c8e4ffe177e5efa2fd5ab000da2690
Max Okorokov <max.okorokov@gmail.com>
2016-07-29 11:55:25 +0200

    fix(build): generate changelog without 'v' version prefix
    Closes #506

--- gulpfile.js
+++ gulpfile.js
@@ -104,7 +104,11 @@ gulp.task('npm', function() {
 gulp.task('changelog', function() {
   var conventionalChangelog = require('gulp-conventional-changelog');
   return gulp.src('CHANGELOG.md', {})
-      .pipe(conventionalChangelog({preset: 'angular', releaseCount: 1}))
+      .pipe(conventionalChangelog({preset: 'angular', releaseCount: 1}, {
+        // Override release version to avoid `v` prefix for git comparison
+        // See https://github.com/conventional-changelog/conventional-changelog-core/issues/10
+        currentTag: require('./package.json').version
+      }))
       .pipe(gulp.dest('./'));
 });
 
