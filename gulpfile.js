735f98eda4c7f776fcfa21598ef53aae661f1452
Jesús Rodríguez <Foxandxss@gmail.com>
2016-07-29 12:34:31 +0200

    chore: remove api-docs.ts from linting (#508)

--- gulpfile.js
+++ gulpfile.js
@@ -154,7 +154,7 @@ gulp.task('tdd', ['clean:build-tests'], function(done) {
 // Formatting
 
 gulp.task('lint', function() {
-  return gulp.src([PATHS.src, PATHS.demo])
+  return gulp.src([PATHS.src, PATHS.demo, '!demo/src/api-docs.ts'])
       .pipe(tslint({configuration: require('./tslint.json'), formatter: 'prose'}))
       .pipe(tslint.report({summarizeFailureOutput: true}));
 });
