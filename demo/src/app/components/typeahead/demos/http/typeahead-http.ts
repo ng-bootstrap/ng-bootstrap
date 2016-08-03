841d8cd18494b451aea3c04536b6db59bc10f6d1
Max Okorokov <max.okorokov@gmail.com>
2016-08-03 20:12:11 +0200

    demo(typeahead): add error recovery to http demo

--- demo/src/app/components/typeahead/demos/http/typeahead-http.ts
+++ demo/src/app/components/typeahead/demos/http/typeahead-http.ts
@@ -1,26 +1,32 @@
-import {Component, Input} from '@angular/core';
+import {Component} from '@angular/core';
 import {HTTP_PROVIDERS, JSONP_PROVIDERS} from '@angular/http';
 import {NGB_TYPEAHEAD_DIRECTIVES, NGB_PRECOMPILE} from '@ng-bootstrap/ng-bootstrap';
 
 import {Injectable} from '@angular/core';
 import {Jsonp, URLSearchParams} from '@angular/http';
 import {Observable} from 'rxjs/Rx';
+import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/retry';
 import 'rxjs/add/operator/switchMap';
 
 @Injectable()
 export class WikipediaService {
   constructor(private _jsonp: Jsonp) {}
 
-  search(term: string) {
+  search(term: string, failRequest: boolean) {
     let wikiUrl = 'https://en.wikipedia.org/w/api.php';
     let params = new URLSearchParams();
     params.set('search', term);
     params.set('action', 'opensearch');
     params.set('format', 'json');
     params.set('callback', 'JSONP_CALLBACK');
+
+    if (failRequest) {
+      wikiUrl = 'https://invalidurl';
+    }
 
     return this._jsonp
       .get(wikiUrl, {search: params})
@@ -38,6 +44,9 @@ export class WikipediaService {
 })
 export class NgbdTypeaheadHttp {
 
+  private _failRequests: boolean;
+
+  private _error: boolean;
   private _searching: boolean;
 
   constructor(private _service: WikipediaService) {}
@@ -46,7 +55,17 @@ export class NgbdTypeaheadHttp {
     text$
       .debounceTime(300)
       .distinctUntilChanged()
-      .do(term => { this._searching = term.length > 0; })
-      .switchMap(term => term === '' ? Observable.of([]) : this._service.search(term))
+      .do(term => {
+        this._searching = term.length > 0;
+        this._error = false;
+      })
+      .switchMap(term => term === '' ? Observable.of([]) :
+        this._service.search(term, this._failRequests)
+          .retry(2)
+          .catch(() => {
+            this._error = true;
+            return Observable.of([]);
+          })
+      )
       .do(() => { this._searching = false; });
 }
