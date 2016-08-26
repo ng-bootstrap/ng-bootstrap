b2cc7662b3f3ed5acb5aace8e3ab98a4ee1709d2
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2016-08-26 15:04:23 +0200

    chore: extract API docs from interfaces

---
+++ misc/api-doc-test-cases/interface-with-properties.ts
@@ -0,0 +1,20 @@
+/**
+ * Represent options available when opening new modal windows.
+ */
+export interface NgbModalOptions {
+  /**
+   * Weather a backdrop element should be created for a given modal (true by default).
+   * Alternatively, specify 'static' for a backdrop which doesn't close the modal on click.
+   */
+  backdrop?: boolean | 'static';
+
+  /**
+   * Weather to close the modal when escape key is pressed (true by default).
+   */
+  keyboard?: boolean;
+
+  /**
+   * Size of a new modal window.
+   */
+  size?: 'sm' | 'lg';
+}
