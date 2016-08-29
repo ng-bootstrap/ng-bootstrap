248fe9c6c8e39d98574eaa59aafd6cc7ca8c0905
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2016-08-29 13:54:21 +0200

    docs(modal): API docs
    Closes #609

--- demo/src/app/components/shared/index.ts
+++ demo/src/app/components/shared/index.ts
@@ -3,10 +3,11 @@ import {NgModule} from '@angular/core';
 import {NgbdSharedModule} from '../../shared';
 import {ExampleBoxComponent} from './example-box/example-box.component';
 import {NgbdApiDocs} from './api-docs/api-docs.component';
+import {NgbdApiDocsClass} from './api-docs/api-docs-class.component';
 
 @NgModule({
   imports: [NgbdSharedModule],
-  declarations: [ExampleBoxComponent, NgbdApiDocs],
-  exports: [ExampleBoxComponent, NgbdApiDocs]
+  declarations: [ExampleBoxComponent, NgbdApiDocs, NgbdApiDocsClass],
+  exports: [ExampleBoxComponent, NgbdApiDocs, NgbdApiDocsClass]
 })
 export class NgbdComponentsSharedModule {}
