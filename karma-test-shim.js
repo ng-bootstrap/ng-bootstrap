b63599735e25debecc74293cb281dd81624b850d
Jesús Rodríguez <Foxandxss@gmail.com>
2016-09-01 11:32:17 +0200

    chore: update to RC6

--- karma-test-shim.js
+++ karma-test-shim.js
@@ -10,17 +10,41 @@ __karma__.loaded = function() {};
 System.config({baseURL: '/base'});
 
 System.config({
-  map: {'rxjs': 'node_modules/rxjs', '@angular': 'node_modules/@angular', 'temp': 'temp'},
-  packages: {
-    'temp': {main: 'core.js', defaultExtension: 'js'},
-    '@angular/core': {main: 'index.js', defaultExtension: 'js'},
-    '@angular/compiler': {main: 'index.js', defaultExtension: 'js'},
-    '@angular/common': {main: 'index.js', defaultExtension: 'js'},
-    '@angular/forms': {main: 'index.js', defaultExtension: 'js'},
-    '@angular/platform-browser': {main: 'index.js', defaultExtension: 'js'},
-    '@angular/platform-browser-dynamic': {main: 'index.js', defaultExtension: 'js'},
-    'rxjs': {defaultExtension: 'js'}
-  }
+  paths: {
+    // paths serve as alias
+    'npm:': 'node_modules/'
+  },
+  // map tells the System loader where to look for things
+  map: {
+    // our temp folder
+    temp: 'temp',
+
+    // angular bundles
+    '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
+    '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
+    '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
+    '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+    '@angular/platform-browser-dynamic':
+        'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+    '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
+    '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
+    '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+
+    // angular testing umd bundles
+    '@angular/core/testing': 'npm:@angular/core/bundles/core-testing.umd.js',
+    '@angular/common/testing': 'npm:@angular/common/bundles/common-testing.umd.js',
+    '@angular/compiler/testing': 'npm:@angular/compiler/bundles/compiler-testing.umd.js',
+    '@angular/platform-browser/testing': 'npm:@angular/platform-browser/bundles/platform-browser-testing.umd.js',
+    '@angular/platform-browser-dynamic/testing':
+        'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic-testing.umd.js',
+    '@angular/http/testing': 'npm:@angular/http/bundles/http-testing.umd.js',
+    '@angular/router/testing': 'npm:@angular/router/bundles/router-testing.umd.js',
+    '@angular/forms/testing': 'npm:@angular/forms/bundles/forms-testing.umd.js',
+
+    // other libraries
+    'rxjs': 'npm:rxjs'
+  },
+  packages: {temp: {main: 'core.js', defaultExtension: 'js'}, rxjs: {defaultExtension: 'js'}}
 });
 
 System.import('@angular/core/testing')
