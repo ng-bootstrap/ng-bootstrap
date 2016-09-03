08a085cc1229f338adb1f713f8224ca2e56f067d
jnizet <jb@ninja-squad.com>
2016-09-03 08:47:15 +0200

    feat(timepicker): add config service to provide default values
    refs #518

--- demo/src/app/components/timepicker/demos/index.ts
+++ demo/src/app/components/timepicker/demos/index.ts
@@ -4,27 +4,40 @@ import {NgbdTimepickerSeconds} from './seconds/timepicker-seconds';
 import {NgbdTimepickerSteps} from './steps/timepicker-steps';
 import {NgbdTimepickerValidation} from './validation/timepicker-validation';
 import {NgbdTimepickerSpinners} from './spinners/timepicker-spinners';
+import {NgbdTimepickerConfig} from './config/timepicker-config';
 
-export const DEMO_DIRECTIVES = [NgbdTimepickerBasic, NgbdTimepickerMeridian, NgbdTimepickerSeconds,
-  NgbdTimepickerSpinners, NgbdTimepickerSteps, NgbdTimepickerValidation];
+export const DEMO_DIRECTIVES = [
+  NgbdTimepickerBasic, NgbdTimepickerMeridian, NgbdTimepickerSeconds, NgbdTimepickerSpinners, NgbdTimepickerSteps,
+  NgbdTimepickerValidation, NgbdTimepickerConfig
+];
 
 export const DEMO_SNIPPETS = {
   basic: {
     code: require('!!prismjs?lang=typescript!./basic/timepicker-basic'),
-    markup: require('!!prismjs?lang=markup!./basic/timepicker-basic.html')},
+    markup: require('!!prismjs?lang=markup!./basic/timepicker-basic.html')
+  },
   meridian: {
     code: require('!!prismjs?lang=typescript!./meridian/timepicker-meridian'),
-    markup: require('!!prismjs?lang=markup!./meridian/timepicker-meridian.html')},
+    markup: require('!!prismjs?lang=markup!./meridian/timepicker-meridian.html')
+  },
   seconds: {
     code: require('!!prismjs?lang=typescript!./seconds/timepicker-seconds'),
-    markup: require('!!prismjs?lang=markup!./seconds/timepicker-seconds.html')},
+    markup: require('!!prismjs?lang=markup!./seconds/timepicker-seconds.html')
+  },
   spinners: {
     code: require('!!prismjs?lang=typescript!./spinners/timepicker-spinners'),
-    markup: require('!!prismjs?lang=markup!./spinners/timepicker-spinners.html')},
+    markup: require('!!prismjs?lang=markup!./spinners/timepicker-spinners.html')
+  },
   steps: {
     code: require('!!prismjs?lang=typescript!./steps/timepicker-steps'),
-    markup: require('!!prismjs?lang=markup!./steps/timepicker-steps.html')},
+    markup: require('!!prismjs?lang=markup!./steps/timepicker-steps.html')
+  },
   validation: {
     code: require('!!prismjs?lang=typescript!./validation/timepicker-validation'),
-    markup: require('!!prismjs?lang=markup!./validation/timepicker-validation.html')}
+    markup: require('!!prismjs?lang=markup!./validation/timepicker-validation.html')
+  },
+  config: {
+    code: require('!!prismjs?lang=typescript!./config/timepicker-config'),
+    markup: require('!!prismjs?lang=markup!./config/timepicker-config.html')
+  }
 };
---
+++ src/timepicker/timepicker-config.ts
@@ -0,0 +1,18 @@
+import {Injectable} from '@angular/core';
+
+/**
+ * Configuration service for the NgbTimepicker component.
+ * You can inject this service, typically in your root component, and customize the values of its properties in
+ * order to provide default values for all the timepickers used in the application.
+ */
+@Injectable()
+export class NgbTimepickerConfig {
+  meridian = false;
+  spinners = true;
+  seconds = false;
+  hourStep = 1;
+  minuteStep = 1;
+  secondStep = 1;
+  disabled = false;
+  readonlyInputs = false;
+}
