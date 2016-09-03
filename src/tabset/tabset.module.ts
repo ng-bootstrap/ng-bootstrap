e57f54aebdcdb726d5125bf1e8332701022f35e3
jnizet <jb@ninja-squad.com>
2016-09-03 15:32:59 +0200

    feat(tabset): add config service to provide default values
    refs #518

--- src/tabset/tabset.module.ts
+++ src/tabset/tabset.module.ts
@@ -2,9 +2,16 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
 import {NGB_TABSET_DIRECTIVES} from './tabset';
+import {NgbTabsetConfig} from './tabset-config';
 
 export {NgbTabChangeEvent} from './tabset';
+export {NgbTabsetConfig} from './tabset-config';
 
-@NgModule({declarations: NGB_TABSET_DIRECTIVES, exports: NGB_TABSET_DIRECTIVES, imports: [CommonModule]})
+@NgModule({
+  declarations: NGB_TABSET_DIRECTIVES,
+  exports: NGB_TABSET_DIRECTIVES,
+  imports: [CommonModule],
+  providers: [NgbTabsetConfig]
+})
 export class NgbTabsetModule {
 }
