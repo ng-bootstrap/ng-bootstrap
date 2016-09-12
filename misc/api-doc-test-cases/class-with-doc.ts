d0c51fc3d965d43fc7e3f8a2bd86826f9047894d
jnizet <jb@ninja-squad.com>
2016-09-12 18:31:55 +0200

    chore: document interface methods and commented classes
    Documenting methods in interfaces is not needed yet but will probably be in the future.
    Documenting classes which don't have any decorator is useful for context classes, that could be used later, in accordion for example
    
    Closes #725

---
+++ misc/api-doc-test-cases/class-with-doc.ts
@@ -0,0 +1,16 @@
+//a class with doc to extract
+
+/**
+ * This is a documented foo
+ */
+class DocumentedFoo {
+  /**
+   * the bar
+   */
+  bar: string;
+
+  /**
+   * some method
+   */
+  someMethod(): void {}
+}
