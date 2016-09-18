5a66f0062cd1f5ac735a0847fecf0eb5c4c7e6e3
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2016-09-18 10:34:47 +0200

    demo(tabset): illustrate proper method of switching active tab
    Closes #762
    Closes #594

--- demo/src/app/components/tabset/demos/selectbyid/tabset-selectbyid.ts
+++ demo/src/app/components/tabset/demos/selectbyid/tabset-selectbyid.ts
@@ -5,10 +5,4 @@ import {Component} from '@angular/core';
   templateUrl: './tabset-selectbyid.html'
 })
 export class NgbdTabsetSelectbyid {
-  tabIds = ['foo', 'bar'];
-  selectedId = this.tabIds[0];
-
-  public toggleSelected() {
-    this.selectedId = this.tabIds[(this.tabIds.indexOf(this.selectedId) + 1) % 2];
-  }
 }
