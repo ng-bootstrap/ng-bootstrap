bd896a60b12bc88215af7dcbb49636264b7f17c6
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2016-09-23 17:53:36 +0200

    chore: remove unused reflect-metadata
    Reflect.* is part of core-js.
    
    Closes #792

--- demo/src/polyfills.ts
+++ demo/src/polyfills.ts
@@ -1,6 +1,5 @@
 import 'core-js/shim';
 import 'classlist-polyfill';
-import 'reflect-metadata';
 require('zone.js/dist/zone');
 
 if (process.env.ENV === 'build') {
--- karma.conf.js
+++ karma.conf.js
@@ -9,7 +9,6 @@ module.exports = function(config) {
       // For travis
       'node_modules/core-js/client/shim.js',
       'node_modules/ie-shim/index.js',
-      'node_modules/reflect-metadata/Reflect.js',
       // paths loaded by Karma
       'node_modules/systemjs/dist/system-polyfills.js',
       'node_modules/systemjs/dist/system.src.js',
