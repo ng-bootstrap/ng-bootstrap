cb0e038122842fb3a0381e2c1f557bb4f73f053c
Max Okorokov <max.okorokov@gmail.com>
2016-09-29 10:32:58 +0200

    demo(rating): shows enter/leave usage and readonly ratings
    Closes #814

---
+++ demo/src/app/components/rating/demos/events/index.ts
@@ -0,0 +1,3 @@
+export * from './events.component';
+export const basicTsContent = require('!!prismjs?lang=typescript!./events.component.ts');
+export const basicHtmlContent = require('!!prismjs?lang=markup!./events.component.html');
