ef0bd7c2df364697c3287df36049265f1f0a98a8
Colin Morelli <colin.morelli@gmail.com>
2016-10-04 11:26:42 -0400

    docs: adding docs for show/hide events on popover and tooltip

---
+++ demo/src/app/components/popover/demos/visibility/popover-visibility.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'ngbd-popover-visibility',
+  templateUrl: './popover-visibility.html'
+})
+export class NgbdPopoverVisibility {
+
+}
