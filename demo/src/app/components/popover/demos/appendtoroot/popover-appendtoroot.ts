df0282bc1313838da25f01296ca6f365bcc4bd8d
Gregory McGee <spongessuck@gmail.com>
2016-10-07 21:49:02 -0400

    style(popover): Fix tslint issues

--- demo/src/app/components/popover/demos/appendtoroot/popover-appendtoroot.ts
+++ demo/src/app/components/popover/demos/appendtoroot/popover-appendtoroot.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'ngbd-popover-appendtoroot',
   templateUrl: './popover-appendtoroot.html',
-	styles: ['.card { padding: 50px 0; text-align: center; overflow:hidden }']
+  styles: ['.card { padding: 50px 0; text-align: center; overflow:hidden }']
 })
 export class NgbdPopoverAppendtoroot {
 }
