ec16edcee9acd91592bf0f75834f2a3fa9ef2871
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2016-10-10 12:13:20 +0200

    demo: import missing RxJS operators
    Closes #864

--- demo/src/app/components/typeahead/demos/http/typeahead-http.ts
+++ demo/src/app/components/typeahead/demos/http/typeahead-http.ts
@@ -1,6 +1,8 @@
 import {Component, Injectable} from '@angular/core';
 import {Jsonp, URLSearchParams} from '@angular/http';
 import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/debounceTime';
 import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/do';
