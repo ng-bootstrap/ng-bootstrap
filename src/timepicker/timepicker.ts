6d70381609cea02827a3ab37daf4e5f6e551e825
Dustin Chabrowski <dustin.duske@gmail.com>
2016-10-19 12:10:52 +0200

    fix(timepicker): add webkit prefix for navigation chevrons
    closes #908

--- src/timepicker/timepicker.ts
+++ src/timepicker/timepicker.ts
@@ -26,6 +26,7 @@ const NGB_TIMEPICKER_VALUE_ACCESSOR = {
       left: 0.05em;
       position: relative;
       top: 0.15em;
+      -webkit-transform: rotate(-45deg);
       transform: rotate(-45deg);
       vertical-align: middle;
       width: 0.71em;
@@ -33,6 +34,7 @@ const NGB_TIMEPICKER_VALUE_ACCESSOR = {
     
     .chevron.bottom:before {
       top: -.3em;
+      -webkit-transform: rotate(135deg);
       transform: rotate(135deg);
     }
     
