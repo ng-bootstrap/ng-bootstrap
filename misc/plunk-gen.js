a6d9b7b538f1d9ed52966d189b9da99c5e8a812b
jnizet <jb@ninja-squad.com>
2016-10-23 11:55:29 +0200

    demo: pin versions of dependencies in plunkr
    Closes #949

--- misc/plunk-gen.js
+++ misc/plunk-gen.js
@@ -4,9 +4,9 @@ const capitalize = require('./demo-gen-utils').capitalize;
 
 const plnkrUrl = 'http://plnkr.co/edit/?p=preview';
 
-const contentIndexHtml = fs.readFileSync('misc/plunker-builder-templates/index.html').toString();
-const contentConfigJs = fs.readFileSync('misc/plunker-builder-templates/config.js').toString();
 const contentMainTs = fs.readFileSync('misc/plunker-builder-templates/main.ts').toString();
+const packageJson = JSON.parse(fs.readFileSync('package.json'));
+const versions = getVersions();
 
 const ENTRY_CMPTS = {
   modal: ['component']
@@ -77,16 +77,112 @@ function generatePlnkrContent(componentName, demoName) {
   <form id="mainForm" method="post" action="${plnkrUrl}">
     <input type="hidden" name="description" value="Example usage of the ${componentName} widget from https://ng-bootstrap.github.io">
 ${generateTags(['Angular', 'Bootstrap', 'ng-bootstrap', capitalize(componentName)])}  
-    <input type="hidden" name="files[index.html]" value="${he.encode(contentIndexHtml)}">
-    <input type="hidden" name="files[config.js]" value="${he.encode(contentConfigJs)}">
+    <input type="hidden" name="files[index.html]" value="${he.encode(generateIndexHtml())}">
+    <input type="hidden" name="files[config.js]" value="${he.encode(generateConfigJs())}">
     <input type="hidden" name="files[src/main.ts]" value="${he.encode(contentMainTs)}">
     <input type="hidden" name="files[src/app.ts]" value="${he.encode(generateAppTsContent(componentName, demoName))}">
     <input type="hidden" name="files[src/${fileName}.ts]" value="${he.encode(codeContent.replace(`./${demoTplPath}`, `src/${demoTplPath}`))}">
     <input type="hidden" name="files[src/${fileName}.html]" value="${he.encode(markupContent)}">
   </form>
   <script>document.getElementById("mainForm").submit();</script>
-/body>
+</body>
 </html>`;
+}
+
+function generateIndexHtml() {
+  return `<!DOCTYPE html>
+<html>
+
+  <head>
+  <base href="." />
+    <title>ng-bootstrap demo</title>
+    <link rel="stylesheet" href="http://v4-alpha.getbootstrap.com/dist/css/bootstrap.min.css" />
+    <script src="https://unpkg.com/zone.js@${versions.zoneJs}/dist/zone.js"></script>
+    <script src="https://unpkg.com/zone.js@${versions.zoneJs}/dist/long-stack-trace-zone.js"></script>
+    <script src="https://unpkg.com/reflect-metadata@${versions.reflectMetadata}/Reflect.js"></script>
+    <script src="https://unpkg.com/systemjs@${versions.systemjs}/dist/system.js"></script>
+    <script src="config.js"></script>
+    <script>
+    System.import('app').catch(console.error.bind(console));
+</script>
+  </head>
+
+  <body>
+  <my-app>loading...</my-app>
+  </body>
+
+</html>`;
+}
+
+function generateConfigJs() {
+  return `System.config({
+  //use typescript for compilation
+  transpiler: 'typescript',
+  //typescript compiler options
+  typescriptOptions: {
+    emitDecoratorMetadata: true
+  },
+  paths: {
+    'npm:': 'https://unpkg.com/'
+  },
+  map: {
+
+    'app': './src',
+
+    '@angular/core': 'npm:@angular/core@${versions.angular}/bundles/core.umd.js',
+    '@angular/common': 'npm:@angular/common@${versions.angular}/bundles/common.umd.js',
+    '@angular/compiler': 'npm:@angular/compiler@${versions.angular}/bundles/compiler.umd.js',
+    '@angular/platform-browser': 'npm:@angular/platform-browser@${versions.angular}/bundles/platform-browser.umd.js',
+    '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic@${versions.angular}/bundles/platform-browser-dynamic.umd.js',
+    '@angular/http': 'npm:@angular/http@${versions.angular}/bundles/http.umd.js',
+    '@angular/router': 'npm:@angular/router@${versions.angularRouter}/bundles/router.umd.js',
+    '@angular/forms': 'npm:@angular/forms@${versions.angular}/bundles/forms.umd.js',
+
+    '@angular/core/testing': 'npm:@angular/core@${versions.angular}/bundles/core-testing.umd.js',
+    '@angular/common/testing': 'npm:@angular/common@${versions.angular}/bundles/common-testing.umd.js',
+    '@angular/compiler/testing': 'npm:@angular/compiler@${versions.angular}/bundles/compiler-testing.umd.js',
+    '@angular/platform-browser/testing': 'npm:@angular/platform-browser@${versions.angular}/bundles/platform-browser-testing.umd.js',
+    '@angular/platform-browser-dynamic/testing': 'npm:@angular/platform-browser-dynamic@${versions.angular}/bundles/platform-browser-dynamic-testing.umd.js',
+    '@angular/http/testing': 'npm:@angular/http@${versions.angular}/bundles/http-testing.umd.js',
+    '@angular/router/testing': 'npm:@angular/router@${versions.angularRouter}/bundles/router-testing.umd.js',
+
+    'rxjs': 'npm:rxjs@${versions.rxjs}',
+    'typescript': 'npm:typescript@${versions.typescript}/lib/typescript.js',
+
+    '@ng-bootstrap/ng-bootstrap': 'npm:@ng-bootstrap/ng-bootstrap@${versions.ngBootstrap}/bundles/ng-bootstrap.js'
+  },
+  packages: {
+    app: {
+      main: './main.ts',
+      defaultExtension: 'ts'
+    },
+    rxjs: {
+      defaultExtension: 'js'
+    }
+  }
+});
+`;
+}
+
+function getVersions() {
+  return {
+    angular: getVersion('@angular/core'),
+    angularRouter: getVersion('@angular/router'),
+    typescript: getVersion('typescript'),
+    rxjs: getVersion('rxjs'),
+    ngBootstrap: packageJson.version,
+    zoneJs: getVersion('zone.js'),
+    systemjs: getVersion('systemjs'),
+    reflectMetadata: getVersion('reflect-metadata')
+  };
+}
+
+function getVersion(name) {
+  var value = packageJson.dependencies[name] || packageJson.devDependencies[name];
+  if (!value) {
+    throw `couldn't find version for ${name} in package.json`;
+  }
+  return value;
 }
 
 module.exports = generatePlnkrContent;
--- misc/plunker-builder-templates/main.ts
+++ misc/plunker-builder-templates/main.ts
@@ -1,4 +1,4 @@
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {AppModule} from './app';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
+platformBrowserDynamic().bootstrapModule(AppModule);
