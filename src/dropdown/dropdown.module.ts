fafca710208c39e926c3a17859ad43e5cbddc8f2
Wesley Cho <wesley.cho@gmail.com>
2016-11-09 06:50:21 -0800

    feat(dropdown): add support for appending to body
    - Add support for appending dropdown to body

--- src/dropdown/dropdown.module.ts
+++ src/dropdown/dropdown.module.ts
@@ -1,11 +1,11 @@
 import {NgModule, ModuleWithProviders} from '@angular/core';
-import {NgbDropdown, NgbDropdownToggle} from './dropdown';
+import {NgbDropdown, NgbDropdownMenu, NgbDropdownToggle} from './dropdown';
 import {NgbDropdownConfig} from './dropdown-config';
 
-export {NgbDropdown, NgbDropdownToggle} from './dropdown';
+export {NgbDropdown, NgbDropdownMenu, NgbDropdownToggle} from './dropdown';
 export {NgbDropdownConfig} from './dropdown-config';
 
-const NGB_DROPDOWN_DIRECTIVES = [NgbDropdownToggle, NgbDropdown];
+const NGB_DROPDOWN_DIRECTIVES = [NgbDropdownMenu, NgbDropdownToggle, NgbDropdown];
 
 @NgModule({declarations: NGB_DROPDOWN_DIRECTIVES, exports: NGB_DROPDOWN_DIRECTIVES})
 export class NgbDropdownModule {
