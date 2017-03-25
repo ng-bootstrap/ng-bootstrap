8422200e9ac0ce2c4a107293e748dbf0533aa80d
Giampiero Bono <giampiero.bono@hotmail.it>
2017-03-25 14:05:33 +0100

    feat(carousel): add event on carousel slide with direction info

--- src/carousel/carousel.module.ts
+++ src/carousel/carousel.module.ts
@@ -1,10 +1,10 @@
 import {NgModule, ModuleWithProviders} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
-import {NGB_CAROUSEL_DIRECTIVES} from './carousel';
+import {NGB_CAROUSEL_DIRECTIVES, NgbSlideCarouselEvent} from './carousel';
 import {NgbCarouselConfig} from './carousel-config';
 
-export {NgbCarousel, NgbSlide} from './carousel';
+export {NgbCarousel, NgbSlide, NgbSlideCarouselEvent} from './carousel';
 export {NgbCarouselConfig} from './carousel-config';
 
 @NgModule({declarations: NGB_CAROUSEL_DIRECTIVES, exports: NGB_CAROUSEL_DIRECTIVES, imports: [CommonModule]})
