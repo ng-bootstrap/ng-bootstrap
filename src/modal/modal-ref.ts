e8debd953c7a643977ea08b6fb5f4d090a6677c1
jyoungers <jyoungers@wrberkley.com>
2017-03-26 13:55:36 -0500

    refactor(modal): setting NgbModalRef as a generic type

--- src/modal/modal-ref.ts
+++ src/modal/modal-ref.ts
@@ -1,4 +1,4 @@
-import {Injectable, ComponentRef} from '@angular/core';
+﻿import {Injectable, ComponentRef} from '@angular/core';
 
 import {NgbModalBackdrop} from './modal-backdrop';
 import {NgbModalWindow} from './modal-window';
@@ -26,7 +26,7 @@ export class NgbActiveModal {
  * A reference to a newly opened modal.
  */
 @Injectable()
-export class NgbModalRef {
+export class NgbModalRef<T> {
   private _resolve: (result?: any) => void;
   private _reject: (reason?: any) => void;
 
@@ -34,14 +34,14 @@ export class NgbModalRef {
    * The instance of component used as modal's content.
    * Undefined when a TemplateRef is used as modal's content.
    */
-  get componentInstance(): any {
+  get componentInstance(): T {
     if (this._contentRef.componentRef) {
       return this._contentRef.componentRef.instance;
     }
   }
 
   // only needed to keep TS1.8 compatibility
-  set componentInstance(instance: any) {}
+  set componentInstance(instance: T) {}
 
   /**
    * A promise that is resolved when a modal is closed and rejected when a modal is dismissed.
