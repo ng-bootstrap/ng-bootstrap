1af74c0933a9e26a2d3a3746ad780707df0cbcfa
Williams Medina <williams.medinaa@gmail.com>
2017-03-27 22:31:30 -0400

    fix: add karma ie polyfills

--- demo/src/polyfills.ts
+++ demo/src/polyfills.ts
@@ -1,14 +1,30 @@
-import 'core-js/shim';
+/** IE9, IE10 and IE11 requires all of the following polyfills. **/
+import 'core-js/es6/symbol';
+import 'core-js/es6/object';
+import 'core-js/es6/function';
+import 'core-js/es6/parse-int';
+import 'core-js/es6/parse-float';
+import 'core-js/es6/number';
+import 'core-js/es6/math';
+import 'core-js/es6/string';
+import 'core-js/es6/date';
+import 'core-js/es6/array';
+import 'core-js/es6/regexp';
+import 'core-js/es6/map';
+import 'core-js/es6/set';
 
-require('zone.js/dist/zone');
+/** Evergreen browsers require these. **/
+import 'core-js/es6/reflect';
+import 'core-js/es7/reflect';
+
+/** Zone JS is required by Angular itself. **/
+import 'zone.js/dist/zone';
 
 if (process.env.ENV === 'build') {
   // Production
 
 } else {
   // Development
-
   Error['stackTraceLimit'] = Infinity;
-
   require('zone.js/dist/long-stack-trace-zone');
 }
