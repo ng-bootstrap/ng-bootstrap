22a721086a99869cccad597b8b8c4b7ae2675e8f
AhmedMustafa <ahmedmustafa584@gmail.com>
2017-04-09 14:09:02 +0200

    feat(datepicker): add Islamic Umalqura calendar

--- demo/src/app/components/datepicker/demos/calendars/datepicker-calendars.ts
+++ demo/src/app/components/datepicker/demos/calendars/datepicker-calendars.ts
@@ -1,54 +1,67 @@
 import {Component, Injectable} from '@angular/core';
-import {NgbDateStruct, NgbCalendar, NgbCalendarIslamicCivil, NgbDatepickerI18n} from '@ng-bootstrap/ng-bootstrap';
+import {
+  NgbDateStruct, NgbCalendar, NgbCalendarIslamicCivil, NgbCalendarIslamicUmalqura,
+  NgbDatepickerI18n
+} from '@ng-bootstrap/ng-bootstrap';
 
-const WEEKDAYS_SHORT = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'];
-const MONTHS_SHORT = ['Muh.', 'Saf.', 'Rab. I', 'Rab. II', 'Jum. I', 'Jum. II', 'Raj.', 'Sha.', 'Ram.', 'Shaw.', 'Dhuʻl-Q.', 'Dhuʻl-H.'];
-const MONTHS_FULL = [
-  'Muharram', 'Safar', 'Rabiʻ I', 'Rabiʻ II', 'Jumada I', 'Jumada II', 'Rajab', 'Shaʻban', 'Ramadan', 'Shawwal', 'Dhuʻl-Qiʻdah',
-  'Dhuʻl-Hijjah'
-];
+const I18N_VALUES = {
+  weekdays: ['ن', 'ث', 'ر', 'خ', 'ج', 'س', 'ح'],
+  months: ['محرم', 'صفر', 'ربيع الأول', 'ربيع الآخر', 'جمادى الأولى', 'جمادى الآخرة', 'رجب', 'شعبان', 'رمضان', 'شوال',
+    'ذو القعدة', 'ذو الحجة']
+};
 
 @Injectable()
-export class IslamicCivilI18n extends NgbDatepickerI18n {
-
+export class IslamicI18n extends NgbDatepickerI18n {
 
   getWeekdayShortName(weekday: number) {
-    return WEEKDAYS_SHORT[weekday - 1];
+    return I18N_VALUES.weekdays[weekday - 1];
   }
 
   getMonthShortName(month: number) {
-    return MONTHS_SHORT[month - 1];
+    return I18N_VALUES.months[month - 1];
   }
 
   getMonthFullName(month: number) {
-    return MONTHS_FULL[month - 1];
+    return this.getMonthShortName(month);
   }
 }
 
 @Component({
   selector: 'ngbd-datepicker-calendars',
-  templateUrl: './datepicker-calendars.html',
-  providers: [
-    {provide: NgbCalendar, useClass: NgbCalendarIslamicCivil},
-    {provide: NgbDatepickerI18n, useClass: IslamicCivilI18n}
-  ]
+  templateUrl: './datepicker-calendars.html'
 })
 export class NgbdDatepickerCalendars {
+}
 
-  private today: NgbDateStruct;
-
+@Component({
+  selector: 'ngbd-islamic-civil',
+  templateUrl: './islamic-calendars.html',
+  providers: [
+    {provide: NgbCalendar, useClass: NgbCalendarIslamicCivil},
+    {provide: NgbDatepickerI18n, useClass: IslamicI18n}
+  ]
+})
+export class NgbdIslamicCivil {
+  name = 'Civil';
   model: NgbDateStruct;
-  date: {year: number, month: number};
+  date: { year: number, month: number };
 
-  constructor(calendar: NgbCalendar) {
-    this.today = calendar.getToday();
+  constructor(private calendar: NgbCalendar) {
   }
 
   selectToday() {
-    this.model = {
-      year: this.today.year,
-      month: this.today.month,
-      day: this.today.day
-    };
+    this.model = this.calendar.getToday();
   }
+}
+
+@Component({
+  selector: 'ngbd-islamic-umalqura',
+  templateUrl: './islamic-calendars.html',
+  providers: [
+    {provide: NgbCalendar, useClass: NgbCalendarIslamicUmalqura},
+    {provide: NgbDatepickerI18n, useClass: IslamicI18n}
+  ]
+})
+export class NgbdIslamicUmalqura extends NgbdIslamicCivil {
+  name = 'Umalqura';
 }
