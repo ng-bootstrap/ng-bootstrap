ng-bootstrap/ng-bootstrap

commit df30bbd030fc7f4f20100490e65c986c7eb4be83

chore: clean-up typings leftovers (#1006)

misc/api-doc.js
@@ -39,11 +39,6 @@ class APIDocVisitor {
       throw new Error(`File doesn't exist: ${fileName}.`)
     }
 
-    // don't extract anything from typings
-    if (/typings/.test(fileName)) {
-      return [];
-    }
-
     return sourceFile.statements.reduce((directivesSoFar, statement) => {
       if (statement.kind === ts.SyntaxKind.ClassDeclaration) {
         return directivesSoFar.concat(this.visitClassDeclaration(fileName, statement));

.gitignore
@@ -1,5 +1,4 @@
 /node_modules/
-/typings/
 /dist/
 /temp/
 /demo/dist/

misc/api-doc.spec.js
@@ -5,9 +5,6 @@ describe('APIDocVisitor', function() {
   it('should return [] if there are no docs to extract',
      function() { expect(apiDoc(['./misc/api-doc-test-cases/no-docs.ts'])).toEqual({}); });
 
-  it('should not extract anything from typings',
-     function() { expect(apiDoc(['./typings/globals/node/index.d.ts'])).toEqual({}); });
-
   it('should extract basic info from directives and components', function() {
     var docs = apiDoc(['misc/api-doc-test-cases/directives-no-in-out.ts']);
 
@@ -113,8 +110,7 @@ describe('APIDocVisitor', function() {
   });
 
   it('should extract documentation from services', function() {
-    var serviceDocs =
-        apiDoc(['./misc/api-doc-test-cases/services-with-methods.ts', './typings/index.d.ts']).ModalService;
+    var serviceDocs = apiDoc(['./misc/api-doc-test-cases/services-with-methods.ts']).ModalService;
 
     expect(serviceDocs.fileName).toBe('./misc/api-doc-test-cases/services-with-methods.ts');
     expect(serviceDocs.className).toBe('ModalService');

misc/get-doc.js
@@ -4,7 +4,7 @@ var glob = require('glob');
 function getFileNames() {
   return glob.sync('src/**/*.ts', {
     ignore: ['src/**/*.spec.ts', 'src/util/**']
-  }).concat(glob.sync('typings/**/*d.ts'));
+  });
 }
 
 function getApiDocs() {

commit 3e8a768d98720a16ecfe8b089a58bfd6f96489e7

chore: fix tests for the docs extraction
We had a regression in the docs extraction introduced
by df30bbd030fc7f4f20100490e65c986c7eb4be83 but the
breakage went unnoticed since we don't run doc extraction
test on CI. This commit fixes extraction logic and adds
docs extraction logic to the CI run.

.travis.yml
@@ -25,7 +25,7 @@ before_script:
   - ./scripts/sauce_connect_setup.sh
 
 script:
-  - gulp ci
+  - gulp ci && npm run api-doc-test
   - ./scripts/sauce_connect_block.sh
   - gulp saucelabs
 

misc/api-doc.js
@@ -32,7 +32,7 @@ function isPrivateOrInternal(member) {
 
 class APIDocVisitor {
   constructor(fileNames) {
-    this.program = ts.createProgram(fileNames, {});
+    this.program = ts.createProgram(fileNames, {lib: ["lib.es6.d.ts"]});
     this.typeChecker = this.program.getTypeChecker(true);
   }
 