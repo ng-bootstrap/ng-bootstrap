5371e4f0af3003ae23f4a6c96150ba6c4b93765e
ktriek <triekhaled@gmail.com>
2017-06-04 22:02:50 -0400

    feat(tabset): add nav justification and orientation
    Closes #1472
    Closes #1239

---
+++ demo/src/app/components/tabset/demos/orientation/tabset-orientation.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'ngbd-tabset-orientation',
+  templateUrl: './tabset-orientation.html'
+})
+export class NgbdTabsetOrientation {
+  currentOrientation = 'horizontal';
+}
