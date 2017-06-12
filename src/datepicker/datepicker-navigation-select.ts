9a27911183eaa6868bb3037d326eee0ab23867bf
Florent EXBRAYAT <florent.exbrayat@amadeus.com>
2017-06-12 16:57:50 +0200

    feat(datepicker): simplify css handling of the navigation system
    Closes #1614

--- src/datepicker/datepicker-navigation-select.ts
+++ src/datepicker/datepicker-navigation-select.ts
@@ -11,29 +11,30 @@ import {NgbCalendar} from './ngb-calendar';
     select {
       /* to align with btn-sm */
       padding: 0.25rem 0.5rem;
-      font-size: 0.875rem;      
+      font-size: 0.875rem;
       line-height: 1.25;
       /* to cancel the custom height set by custom-select */
       height: inherit;
       width: 50%;
+      display: inline-block;
     }
   `],
   template: `
     <select
       [disabled]="disabled"
-      class="custom-select d-inline-block"
+      class="custom-select"
       [value]="date?.month"
       (change)="changeMonth($event.target.value)"
       tabindex="-1">
         <option *ngFor="let m of months" [value]="m">{{ i18n.getMonthShortName(m) }}</option>
     </select><select
       [disabled]="disabled"
-      class="custom-select d-inline-block"
+      class="custom-select"
       [value]="date?.year"
       (change)="changeYear($event.target.value)"
       tabindex="-1">
         <option *ngFor="let y of years" [value]="y">{{ y }}</option>
-    </select> 
+    </select>
   `
 })
 export class NgbDatepickerNavigationSelect implements OnChanges {
