3137bfcd8a67769a7ec245c3f179391c1297babb
Benoit Charbonnier <benoit.charbonnier@amadeus.com>
2017-06-30 11:55:26 +0200

    fix(demo): reset scrollposition when navigating between component

--- demo/src/app/shared/content-wrapper/content-wrapper.component.ts
+++ demo/src/app/shared/content-wrapper/content-wrapper.component.ts
@@ -1,10 +1,16 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input, OnChanges} from '@angular/core';
 
 @Component({
   selector: 'ngbd-content-wrapper',
   templateUrl: './content-wrapper.component.html'
 })
-export class ContentWrapper {
+export class ContentWrapper implements OnChanges {
   @Input()
   public component: string;
+
+  ngOnChanges(changes) {
+    if (changes.component) {
+      document.body.scrollIntoView();
+    }
+  }
 }
