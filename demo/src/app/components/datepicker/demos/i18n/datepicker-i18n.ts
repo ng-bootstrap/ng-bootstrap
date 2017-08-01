c77f24cad54bfa6b77e743739a50d0c3459acaaf
jnizet <jb@ninja-squad.com>
2017-08-02 00:33:21 +0200

    demo(datepicker): remove radios and just show French datepicker
    Fixes #1712
    Closes #1723

--- demo/src/app/components/datepicker/demos/i18n/datepicker-i18n.ts
+++ demo/src/app/components/datepicker/demos/i18n/datepicker-i18n.ts
@@ -2,20 +2,18 @@ import {Component, Injectable} from '@angular/core';
 import {NgbDatepickerI18n} from '@ng-bootstrap/ng-bootstrap';
 
 const I18N_VALUES = {
-  'en': {
-    weekdays: ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'],
-    months: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
-  },
   'fr': {
     weekdays: ['Lu', 'Ma', 'Me', 'Je', 'Ve', 'Sa', 'Di'],
     months: ['Jan', 'Fév', 'Mar', 'Avr', 'Mai', 'Juin', 'Juil', 'Aou', 'Sep', 'Oct', 'Nov', 'Déc'],
   }
+  // other languages you would support
 };
 
-// Define a service holding the language. You probably already have one if your app is i18ned.
+// Define a service holding the language. You probably already have one if your app is i18ned. Or you could also
+// use the Angular LOCALE_ID value
 @Injectable()
 export class I18n {
-  language = 'en';
+  language = 'fr';
 }
 
 // Define custom service providing the months and weekdays translations
@@ -43,16 +41,5 @@ export class CustomDatepickerI18n extends NgbDatepickerI18n {
   providers: [I18n, {provide: NgbDatepickerI18n, useClass: CustomDatepickerI18n}] // define custom NgbDatepickerI18n provider
 })
 export class NgbdDatepickerI18n {
-
   model;
-
-  constructor(private _i18n: I18n) {}
-
-  set language(language: string) {
-    this._i18n.language = language;
-  }
-
-  get language() {
-    return this._i18n.language;
-  }
 }
