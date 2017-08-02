6eabe8d9ba654187b594c4a3df9950207242f5dc
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2017-08-02 10:14:21 +0200

    demo(buttons): properly generate paths for plunkers
    Fixes #1714
    
    Closes #1724

--- demo/src/app/components/buttons/buttons.component.ts
+++ demo/src/app/components/buttons/buttons.component.ts
@@ -11,14 +11,14 @@ import {DEMO_SNIPPETS} from './demos';
       <ngbd-example-box demoTitle="Checkbox buttons" [snippets]="snippets" component="buttons" demo="checkbox">
         <ngbd-buttons-checkbox></ngbd-buttons-checkbox>
       </ngbd-example-box>
-      <ngbd-example-box demoTitle="Checkbox buttons (Reactive Forms)" [snippets]="snippets" component="buttons" demo="checkboxReactive">
-        <ngbd-buttons-checkbox-reactive></ngbd-buttons-checkbox-reactive>
+      <ngbd-example-box demoTitle="Checkbox buttons (Reactive Forms)" [snippets]="snippets" component="buttons" demo="checkboxreactive">
+        <ngbd-buttons-checkboxreactive></ngbd-buttons-checkboxreactive>
       </ngbd-example-box>
       <ngbd-example-box demoTitle="Radio buttons" [snippets]="snippets" component="buttons" demo="radio">
         <ngbd-buttons-radio></ngbd-buttons-radio>
       </ngbd-example-box>
-      <ngbd-example-box demoTitle="Radio buttons (Reactive Forms)" [snippets]="snippets" component="buttons" demo="radioReactive">
-        <ngbd-buttons-radio-reactive></ngbd-buttons-radio-reactive>
+      <ngbd-example-box demoTitle="Radio buttons (Reactive Forms)" [snippets]="snippets" component="buttons" demo="radioreactive">
+        <ngbd-buttons-radioreactive></ngbd-buttons-radioreactive>
       </ngbd-example-box>
     </ngbd-component-wrapper>
   `
