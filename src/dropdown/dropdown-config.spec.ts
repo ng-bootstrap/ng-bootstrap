bfeff31d2e4dc0b34f51bb62d3df4f477acd2a0b
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2017-08-16 17:39:27 +0200

    fix(dropdown): correct TS typings
    ref: https://stackoverflow.com/q/45716754/1418796

--- src/dropdown/dropdown-config.spec.ts
+++ src/dropdown/dropdown-config.spec.ts
@@ -7,4 +7,15 @@ describe('ngb-dropdown-config', () => {
     expect(config.up).toBe(false);
     expect(config.autoClose).toBe(true);
   });
+
+  it('should allow setting "inside" and "outside" value for autoClose', () => {
+    const config = new NgbDropdownConfig();
+
+    // This test looks like having trivial assertions but its goal
+    // is to prove that we've got TS typings right.
+    config.autoClose = 'outside';
+    expect(config.autoClose).toBe('outside');
+    config.autoClose = 'inside';
+    expect(config.autoClose).toBe('inside');
+  });
 });
