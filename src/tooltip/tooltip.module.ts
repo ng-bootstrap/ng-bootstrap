4f1e51805dc1136ed506d98d49bf545f7a1eab9c
Saqib Javed MUHAMMAD (contractor) <saqib-javed.muhammad@amadeus.com>
2017-08-24 11:42:43 +0200

    feat: support more positioning options for tooltips and popovers
    Closes #857
    Closes #1772

--- src/tooltip/tooltip.module.ts
+++ src/tooltip/tooltip.module.ts
@@ -2,9 +2,11 @@ import {NgModule, ModuleWithProviders} from '@angular/core';
 
 import {NgbTooltip, NgbTooltipWindow} from './tooltip';
 import {NgbTooltipConfig} from './tooltip-config';
+import {Placement} from '../util/positioning';
 
 export {NgbTooltipConfig} from './tooltip-config';
 export {NgbTooltip} from './tooltip';
+export {Placement} from '../util/positioning';
 
 @NgModule({declarations: [NgbTooltip, NgbTooltipWindow], exports: [NgbTooltip], entryComponents: [NgbTooltipWindow]})
 export class NgbTooltipModule {
