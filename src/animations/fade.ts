a84f6c2ba0bd4322e37f258b12aa3cb5f73fb7a2
Matt Lewis <matthew.lewis@socialsignin.co.uk>
2017-08-31 15:20:17 +0100

    refactor: make fade animation opacity configurable

--- src/animations/fade.ts
+++ src/animations/fade.ts
@@ -1,7 +1,9 @@
 import {animation, style, animate, AnimationReferenceMetadata} from '@angular/animations';
 
-export const fadeIn: AnimationReferenceMetadata =
-    animation([style({opacity: 0}), animate('.15s linear', style({opacity: 0.5}))]);
+export const fadeIn: AnimationReferenceMetadata = animation(
+    [style({opacity: 0}), animate('{{ duration }} {{ easing }}', style({opacity: '{{ opacity }}'}))],
+    {params: {duration: '.15s', easing: 'linear', opacity: '0.5'}});
 
-export const fadeOut: AnimationReferenceMetadata =
-    animation([style({opacity: 0.5}), animate('.15s linear', style({opacity: 0}))]);
+export const fadeOut: AnimationReferenceMetadata = animation(
+    [style({opacity: '{{ opacity }}'}), animate('{{ duration }} {{ easing }}', style({opacity: 0}))],
+    {params: {duration: '.15s', easing: 'linear', opacity: '0.5'}});
