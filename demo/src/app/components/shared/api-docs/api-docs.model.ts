9d717da97c8a1fbe8402a3ad0f7027ad95f5558a
Max Okorokov <max@okorokov.com>
2017-08-31 16:43:06 +0200

    demo: add colored badges for items in documentation
    Closes #1806

--- demo/src/app/components/shared/api-docs/api-docs.model.ts
+++ demo/src/app/components/shared/api-docs/api-docs.model.ts
@@ -1,4 +1,5 @@
 export interface ClassDesc {
+  type: string;
   fileName: string;
   className: string;
   description: string;
--- demo/src/app/components/shared/index.ts
+++ demo/src/app/components/shared/index.ts
@@ -3,13 +3,14 @@ import {NgModule} from '@angular/core';
 import {NgbdSharedModule} from '../../shared';
 import {ExampleBoxComponent} from './example-box/example-box.component';
 import {NgbdApiDocs} from './api-docs/api-docs.component';
+import {NgbdApiDocsBadge} from './api-docs/api-docs-badge.component';
 import {NgbdApiDocsClass} from './api-docs/api-docs-class.component';
 import {NgbdApiDocsConfig} from './api-docs/api-docs-config.component';
 import {NgbdFragment} from './fragment/fragment.directive';
 
 @NgModule({
   imports: [NgbdSharedModule],
-  declarations: [ExampleBoxComponent, NgbdApiDocs, NgbdApiDocsClass, NgbdApiDocsConfig, NgbdFragment],
-  exports: [ExampleBoxComponent, NgbdApiDocs, NgbdApiDocsClass, NgbdApiDocsConfig, NgbdFragment]
+  declarations: [ExampleBoxComponent, NgbdApiDocsBadge, NgbdApiDocs, NgbdApiDocsClass, NgbdApiDocsConfig, NgbdFragment],
+  exports: [ExampleBoxComponent, NgbdApiDocsBadge, NgbdApiDocs, NgbdApiDocsClass, NgbdApiDocsConfig, NgbdFragment]
 })
 export class NgbdComponentsSharedModule {}
