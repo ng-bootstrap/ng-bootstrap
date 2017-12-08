0e325ca672abb61cd2f91762d5e68196a9ccb16a
Max Okorokov <max@okorokov.com>
2017-12-08 19:02:06 +0100

    fix(datepicker): support `NgbDateAdapter` in `NgbInputDatepicker`
    Fixes #2002
    
    Closes #2003

--- src/datepicker/ngb-date-adapter.spec.ts
+++ src/datepicker/ngb-date-adapter.spec.ts
@@ -7,30 +7,36 @@ describe('ngb-date model adapter', () => {
 
   describe('fromModel', () => {
 
-    it('should convert null undefined as null', () => {
+    it('should convert invalid and incomplete values to null', () => {
       expect(adapter.fromModel(null)).toBeNull();
       expect(adapter.fromModel(undefined)).toBeNull();
+      expect(adapter.fromModel(<any>'')).toBeNull();
+      expect(adapter.fromModel(<any>'s')).toBeNull();
+      expect(adapter.fromModel(<any>2)).toBeNull();
+      expect(adapter.fromModel(<any>{})).toBeNull();
+      expect(adapter.fromModel(<any>new Date())).toBeNull();
+      expect(adapter.fromModel(<any>{year: 2017, month: 10})).toBeNull();
     });
 
     it('should convert valid date',
        () => { expect(adapter.fromModel({year: 2016, month: 5, day: 1})).toEqual({year: 2016, month: 5, day: 1}); });
-
-    it('should do its best parsing incomplete dates',
-       () => { expect(adapter.fromModel({year: 2011, month: 5, day: null})).toEqual({year: 2011, month: 5, day: 1}); });
   });
 
   describe('toModel', () => {
 
-    it('should convert null and undefined as null', () => {
-      expect(adapter.toModel(null)).toBe(null);
-      expect(adapter.toModel(undefined)).toBe(null);
+    it('should convert invalid and incomplete values to null', () => {
+      expect(adapter.toModel(null)).toBeNull();
+      expect(adapter.toModel(undefined)).toBeNull();
+      expect(adapter.toModel(<any>'')).toBeNull();
+      expect(adapter.toModel(<any>'s')).toBeNull();
+      expect(adapter.toModel(<any>2)).toBeNull();
+      expect(adapter.toModel(<any>{})).toBeNull();
+      expect(adapter.toModel(<any>new Date())).toBeNull();
+      expect(adapter.toModel(<any>{year: 2017, month: 10})).toBeNull();
     });
 
     it('should convert a valid date',
        () => { expect(adapter.toModel({year: 2016, month: 10, day: 15})).toEqual({year: 2016, month: 10, day: 15}); });
-
-    it('should try its best with invalid dates',
-       () => { expect(adapter.toModel({year: 2016, month: 10, day: null})).toEqual({year: 2016, month: 10, day: 1}); });
   });
 
 });
