20af7a39f703598ad1fffa26119aa7412cf81f48
jnizet <jb@ninja-squad.com>
2018-03-09 22:39:49 +0100

    demo: changes after review
     - add `text-decoration: none` css rule to avoid visual glitch
     - make sure only valid random image URLs are generated

--- demo/src/app/components/carousel/demos/config/carousel-config.ts
+++ demo/src/app/components/carousel/demos/config/carousel-config.ts
@@ -1,18 +1,33 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
+import {map} from 'rxjs/operators';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'ngbd-carousel-config',
   templateUrl: './carousel-config.html',
-  providers: [NgbCarouselConfig] // add NgbCarouselConfig to the component providers
+  providers: [NgbCarouselConfig]  // add NgbCarouselConfig to the component providers
 })
-export class NgbdCarouselConfig {
-  images = [1, 2, 3].map(() => `https://picsum.photos/900/500?image=${Math.floor(Math.random() * 1000)}`);
+export class NgbdCarouselConfig implements OnInit {
+  images: Array<string>;
 
-  constructor(config: NgbCarouselConfig) {
+  constructor(config: NgbCarouselConfig, private _http: HttpClient) {
     // customize default values of carousels used by this component tree
     config.interval = 10000;
     config.wrap = false;
     config.keyboard = false;
+  }
+
+  ngOnInit() {
+    this._http.get('https://picsum.photos/list')
+        .pipe(map((images: Array<{id: number}>) => this._randomImageUrls(images)))
+        .subscribe(images => this.images = images);
+  }
+
+  private _randomImageUrls(images: Array<{id: number}>): Array<string> {
+    return [1, 2, 3].map(() => {
+      const randomId = images[Math.floor(Math.random() * images.length)].id;
+      return `https://picsum.photos/900/500?image=${randomId}`;
+    });
   }
 }
