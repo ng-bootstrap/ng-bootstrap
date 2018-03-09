09d322347e1de0b2cc5ea1f1bb112b15e62b5e3f
jnizet <jb@ninja-squad.com>
2018-03-09 22:39:49 +0100

    demo: changes after review
     - add `text-decoration: none` css rule to avoid visual glitch
     - make sure only valid random image URLs are generated

--- demo/src/app/components/carousel/demos/basic/carousel-basic.ts
+++ demo/src/app/components/carousel/demos/basic/carousel-basic.ts
@@ -1,9 +1,23 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {map} from 'rxjs/operators';
 
-@Component({
-  selector: 'ngbd-carousel-basic',
-  templateUrl: './carousel-basic.html'
-})
-export class NgbdCarouselBasic {
-  images = [1, 2, 3].map(() => `https://picsum.photos/900/500?image=${Math.floor(Math.random() * 1000)}`);
+@Component({selector: 'ngbd-carousel-basic', templateUrl: './carousel-basic.html'})
+export class NgbdCarouselBasic implements OnInit {
+  images: Array<string>;
+
+  constructor(private _http: HttpClient) {}
+
+  ngOnInit() {
+    this._http.get('https://picsum.photos/list')
+        .pipe(map((images: Array<{id: number}>) => this._randomImageUrls(images)))
+        .subscribe(images => this.images = images);
+  }
+
+  private _randomImageUrls(images: Array<{id: number}>): Array<string> {
+    return [1, 2, 3].map(() => {
+      const randomId = images[Math.floor(Math.random() * images.length)].id;
+      return `https://picsum.photos/900/500?image=${randomId}`;
+    });
+  }
 }
