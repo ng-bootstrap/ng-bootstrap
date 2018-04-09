e041ef9344147d11a49ad4bdbf8d58fa2050c0ec
Max Okorokov <max@okorokov.com>
2018-04-09 17:41:12 +0200

    demo: adapt demo to 'preserveWhiteSpaces' turned off

--- demo/src/app/components/popover/demos/container/popover-container.ts
+++ demo/src/app/components/popover/demos/container/popover-container.ts
@@ -3,7 +3,7 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'ngbd-popover-container',
   templateUrl: './popover-container.html',
-  styles: ['.card { padding: 50px 0; text-align: center; overflow:hidden }']
+  styles: ['.card { overflow: hidden }']
 })
 export class NgbdPopoverContainer {
 }
