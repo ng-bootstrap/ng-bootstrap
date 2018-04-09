a107292592fa5ec246df86c8731e6cd55655f866
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2018-04-09 10:42:24 +0200

    chore: correct RxJS paths mapping in Karma configuration

--- karma.conf.js
+++ karma.conf.js
@@ -19,6 +19,7 @@ module.exports = function(config) {
       'node_modules/zone.js/dist/jasmine-patch.js',
       'node_modules/zone.js/dist/async-test.js',
       'node_modules/zone.js/dist/fake-async-test.js',
+      {pattern: 'node_modules/tslib/**.js', included: false, watched: false},
       {pattern: 'node_modules/rxjs/**/*.js', included: false, watched: false},
       {pattern: 'node_modules/rxjs/**/*.js.map', included: false, watched: false},
       {pattern: 'karma-test-shim.js', included: true, watched: true},
