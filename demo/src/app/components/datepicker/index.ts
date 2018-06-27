2905b18032306e2c7173283fa97bf5225d217f99
Max Okorokov <max@okorokov.com>
2018-06-27 17:34:17 +0200

    demo: datepicker overview page
    Closes #2481

--- demo/src/app/components/datepicker/index.ts
+++ demo/src/app/components/datepicker/index.ts
@@ -4,11 +4,12 @@ import {NgModule} from '@angular/core';
 import {NgbdSharedModule} from '../../shared';
 import {NgbdComponentsSharedModule} from '../shared';
 import {NgbdDatepicker} from './datepicker.component';
+import {NgbdDatepickerOverviewComponent, NgbdDatepickerOverviewDemoComponent} from './overview';
 import {DEMO_DIRECTIVES} from './demos';
 
 @NgModule({
   imports: [NgbdSharedModule, NgbdComponentsSharedModule],
   exports: [NgbdDatepicker],
-  declarations: [NgbdDatepicker, ...DEMO_DIRECTIVES],
+  declarations: [NgbdDatepicker, ...DEMO_DIRECTIVES, NgbdDatepickerOverviewComponent, NgbdDatepickerOverviewDemoComponent],
 })
 export class NgbdDatepickerModule {}
