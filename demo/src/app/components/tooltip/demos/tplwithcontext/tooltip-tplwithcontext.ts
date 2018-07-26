bdab7957546e0282a887421081ef013eb3a6d644
ymeine <yannick.meine@gmail.com>
2018-07-26 17:30:47 +0200

    fix(demo): change demo of using template context with popover & tooltip
    Swapping context not being actually possible and requiring synchronous closing and opening of the popover/tooltip, remove it and replace the demo by a simple set of buttons with dedicated popovers/tooltips, each customized through the template context.

--- demo/src/app/components/tooltip/demos/tplwithcontext/tooltip-tplwithcontext.ts
+++ demo/src/app/components/tooltip/demos/tplwithcontext/tooltip-tplwithcontext.ts
@@ -1,22 +1,17 @@
-import {NgbTooltip} from '@ng-bootstrap/ng-bootstrap';
-import {Component, ViewChild} from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'ngbd-tooltip-tplwithcontext',
   templateUrl: './tooltip-tplwithcontext.html'
 })
 export class NgbdTooltipTplwithcontext {
-  greeting = {};
   name = 'World';
 
-  @ViewChild('t') public tooltip: NgbTooltip;
-
-  public changeGreeting(greeting: any): void {
-    const isOpen = this.tooltip.isOpen();
-    this.tooltip.close();
-    if (greeting !== this.greeting || !isOpen) {
-      this.greeting = greeting;
-      this.tooltip.open(greeting);
+  toggleWithGreeting(tooltip, greeting: string) {
+    if (tooltip.isOpen()) {
+      tooltip.close();
+    } else {
+      tooltip.open({greeting});
     }
   }
 }
