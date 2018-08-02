b4e29a199c07f75e9ec601c21aca48d72cfdf2dc
jnizet <jb@ninja-squad.com>
2018-08-02 21:48:12 +0200

    refactor(typeahead): property binding, else instead of second gIf

--- src/typeahead/highlight.ts
+++ src/typeahead/highlight.ts
@@ -9,7 +9,7 @@ import {regExpEscape, toString} from '../util/util';
   selector: 'ngb-highlight',
   changeDetection: ChangeDetectionStrategy.OnPush,
   template: `<ng-template ngFor [ngForOf]="parts" let-part let-isOdd="odd">` +
-      `<span *ngIf="isOdd" class="{{highlightClass}}">{{part}}</span><ng-template [ngIf]="!isOdd">{{part}}</ng-template>` +
+      `<span *ngIf="isOdd; else even" [class]="highlightClass">{{part}}</span><ng-template #even>{{part}}</ng-template>` +
       `</ng-template>`,  // template needs to be formatted in a certain way so we don't add empty text nodes
   styles: [`
     .ngb-highlight {
