31c1276716a6c46e315a24c3f9f7fec8d7377fda
ymeine <yannick.meine@gmail.com>
2018-08-06 17:46:07 +0200

    test(popover): test default configuration value for autoClose

--- src/popover/popover-config.spec.ts
+++ src/popover/popover-config.spec.ts
@@ -4,6 +4,7 @@ describe('ngb-popover-config', () => {
   it('should have sensible default values', () => {
     const config = new NgbPopoverConfig();
 
+    expect(config.autoClose).toBe(true);
     expect(config.placement).toBe('top');
     expect(config.triggers).toBe('click');
     expect(config.container).toBeUndefined();
