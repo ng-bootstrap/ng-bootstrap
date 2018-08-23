71e234317aeddae3b56fa7945d29cc71d47eeace
Ronn Black <rblack@btsoft.org>
2018-08-23 15:24:16 -0700

    feature(date-picker): added aria-labels to year and month select elements

--- src/datepicker/datepicker-navigation-select.ts
+++ src/datepicker/datepicker-navigation-select.ts
@@ -22,12 +22,14 @@ import {NgbDatepickerI18n} from './datepicker-i18n';
       [disabled]="disabled"
       class="custom-select"
       [value]="date?.month"
+      i18n-aria-label="@@ngb.datepicker.select-month" aria-label="Select month"
       (change)="changeMonth($event.target.value)">
         <option *ngFor="let m of months" [attr.aria-label]="i18n.getMonthFullName(m)" [value]="m">{{ i18n.getMonthShortName(m) }}</option>
     </select><select
       [disabled]="disabled"
       class="custom-select"
       [value]="date?.year"
+      i18n-aria-label="@@ngb.datepicker.select-year" aria-label="Select year"
       (change)="changeYear($event.target.value)">
         <option *ngFor="let y of years" [value]="y">{{ i18n.getYearNumerals(y) }}</option>
     </select>
