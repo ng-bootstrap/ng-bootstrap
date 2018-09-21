8ed7ce58e5f51f6e1ba96dfe1bd5a4f516a4ca18
Max Okorokov <max@okorokov.com>
2018-09-21 11:54:48 +0200

    fix(datepicker): convert years 0-99 correctly with native adapters (#2732)
    Fixes #2731

--- src/datepicker/adapters/ngb-date-native-utc-adapter.spec.ts
+++ src/datepicker/adapters/ngb-date-native-utc-adapter.spec.ts
@@ -15,6 +15,7 @@ describe('ngb-date-native-utc model adapter', () => {
       expect(adapter.fromModel(<any>2)).toBeNull();
       expect(adapter.fromModel(<any>{})).toBeNull();
       expect(adapter.fromModel(<any>{year: 2017, month: 10})).toBeNull();
+      expect(adapter.fromModel(new Date('boom'))).toBeNull();
     });
 
     it('should convert valid date',
@@ -35,6 +36,22 @@ describe('ngb-date-native-utc model adapter', () => {
 
     it('should convert a valid date',
        () => { expect(adapter.toModel({year: 2016, month: 10, day: 15})).toEqual(new Date(Date.UTC(2016, 9, 15))); });
+
+    it('should convert years between 0 and 99 correctly', () => {
+
+      function jsDate(jsYear: number, jsMonth: number, jsDay: number): Date {
+        const date = new Date(Date.UTC(jsYear, jsMonth, jsDay));
+        if (jsYear >= 0 && jsYear <= 99) {
+          date.setUTCFullYear(jsYear);
+        }
+        return date;
+      }
+
+      expect(adapter.toModel({year: 0, month: 1, day: 1})).toEqual(jsDate(0, 0, 1));
+      expect(adapter.toModel({year: 1, month: 1, day: 1})).toEqual(jsDate(1, 0, 1));
+      expect(adapter.toModel({year: 99, month: 1, day: 1})).toEqual(jsDate(99, 0, 1));
+      expect(adapter.toModel({year: 1900, month: 1, day: 1})).toEqual(jsDate(1900, 0, 1));
+    });
   });
 
 });
