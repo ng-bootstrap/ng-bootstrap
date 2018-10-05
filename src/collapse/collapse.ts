3677fabb42268bf288a306d68651933258929cb9
fbasso <fabrice.basso@amadeus.com>
2018-10-05 14:16:29 +0200

    feat(collapse): add animations

--- src/collapse/collapse.ts
+++ src/collapse/collapse.ts
@@ -1,4 +1,7 @@
-import {Directive, Input} from '@angular/core';
+import {Directive, Input, ElementRef, Output, EventEmitter} from '@angular/core';
+import {ngbRunTransition} from '../util/transition/ngbTransition';
+import {ngbCollapsingTransition} from '../util/transition/ngbCollapseTransition';
+import {NgbConfig} from '../ngb-config';
 
 /**
  * A directive to provide a simple way of hiding and showing elements on the page.
@@ -10,7 +13,34 @@ import {Directive, Input} from '@angular/core';
 })
 export class NgbCollapse {
   /**
+   * If `true`, collapse will be animated.
+   *
+   * Animation is triggered only when clicked on triggering element
+   * or via the `.toggle()` function
+   */
+  @Input() animation = false;
+
+  /**
    * If `true`, will collapse the element or show it otherwise.
    */
   @Input('ngbCollapse') collapsed = false;
+
+  @Output() ngbCollapseChange = new EventEmitter<boolean>();
+
+  constructor(private _element: ElementRef, ngbConfig: NgbConfig) { this.animation = ngbConfig.animation; }
+
+  /**
+   * Triggers collapsing programmatically.
+   *
+   * If there is a collapsing transition running already, it will be reversed.
+   * If the animations are turned off this happens synchronously.
+   */
+  toggle(open: boolean = this.collapsed) {
+    this.collapsed = !open;
+    ngbRunTransition(this._element.nativeElement, ngbCollapsingTransition, {
+      animation: this.animation,
+      runningTransition: 'stop',
+      context: {direction: open ? 'show' : 'hide'}
+    }).subscribe(() => this.ngbCollapseChange.next(this.collapsed));
+  }
 }
