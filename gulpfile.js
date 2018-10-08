73949efdd5e3f808ad5314963a42aaeec6997f15
Max Okorokov <max@okorokov.com>
2018-10-08 12:08:49 +0200

    chore: use TS for static file generation instead of Gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,5 @@
 const gulp = require('gulp');
 const ghPages = require('gulp-gh-pages');
-const gulpFile = require('gulp-file');
 const clangFormat = require('clang-format');
 const gulpFormat = require('gulp-clang-format');
 
@@ -18,11 +17,7 @@ gulp.task('changelog', function() {
 // Formatting
 
 gulp.task('check-format', function() {
-  return gulp
-      .src([
-        'gulpfile.js', 'karma-test-shim.js', 'misc/api-doc.js', 'misc/api-doc.spec.js', 'misc/demo-gen.js',
-        'src/**/*.ts'
-      ])
+  return gulp.src(['gulpfile.js', 'misc/*.ts', 'src/**/*.ts'])
       .pipe(gulpFormat.checkFormat('file', clangFormat))
       .on('warning', function(e) {
         console.log("See https://github.com/ng-bootstrap/ng-bootstrap/blob/master/DEVELOPER.md#clang-format");
@@ -31,35 +26,6 @@ gulp.task('check-format', function() {
 });
 
 // Demo
-
-gulp.task('generate-docs', function() {
-  const getApiDocs = require('./misc/get-doc');
-  const docs = `const API_DOCS = ${JSON.stringify(getApiDocs(), null, 2)};\n\nexport default API_DOCS;`;
-
-  return gulpFile('api-docs.ts', docs, {src: true}).pipe(gulp.dest('demo/src'));
-});
-
-gulp.task('generate-stackblitzes', function() {
-  const getStackblitz = require('./misc/stackblitz-gen');
-  const demoGenUtils = require('./misc/demo-gen-utils');
-  let stackblitzes = [];
-
-  demoGenUtils.getDemoComponentNames().forEach(function(componentName) {
-    stackblitzes = stackblitzes.concat(demoGenUtils.getDemoNames(componentName).reduce(function(soFar, demoName) {
-      soFar.push(
-          {name: `${componentName}/demos/${demoName}/stackblitz.html`, source: getStackblitz(componentName, demoName)});
-      return soFar;
-    }, []));
-  });
-
-  return gulpFile(stackblitzes, {src: true}).pipe(gulp.dest('demo/src/public/app/components'));
-});
-
-gulp.task('demo-generate-static', ['generate-docs', 'generate-stackblitzes']);
-
-gulp.task('copy-license', function() {
-  return gulp.src(['LICENSE', 'README.md']).pipe(gulp.dest('dist/ng-bootstrap'));
-});
 
 gulp.task('demo-push', function() {
   return gulp.src('demo/dist/**/*')
