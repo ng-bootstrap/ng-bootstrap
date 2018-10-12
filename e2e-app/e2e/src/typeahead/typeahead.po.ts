7e8f3e10a661fed118dc2a0226c168bdf43f0365
Basso <fabrice.basso@amadeus.com>
2018-10-12 17:22:35 +0200

    test(typeahead): add e2e tests for focus
    Closes #2783

---
+++ e2e-app/e2e/src/typeahead/typeahead.po.ts
@@ -0,0 +1,45 @@
+import {$, Key, browser} from 'protractor';
+import {sendKey} from '../tools';
+import {expectFocused} from '../tools';
+
+export class TypeaheadPage {
+
+  getInputBefore() {
+    return $('#first');
+  }
+
+  getTypeaheadInput() {
+    return $('#typeahead');
+  }
+
+  getDropdown() {
+    return $('ngb-typeahead-window');
+  }
+
+  getDropdownItems() {
+    return this.getDropdown().$$('button');
+  }
+
+  async clearText() {
+    const typeahead = this.getTypeaheadInput();
+    await typeahead.click();
+    await typeahead.sendKeys(Key.ESCAPE);
+    await typeahead.clear();
+  }
+
+  async getTypeaheadValue() {
+    return this.getTypeaheadInput().getAttribute('value');
+  }
+
+  async reset() {
+    await this.clearText();
+    const body = $('body');
+    await body.click();
+    await body.sendKeys(Key.ESCAPE);
+
+    await expectFocused(body, `Nothing should be focused initially`);
+    expect(await this.getDropdown().isPresent()).toBeFalsy(`Typeahead should be closed initially`);
+    expect(await this.getInputBefore().getAttribute('value')).toBe('', `Input before typeahead should be cleared initially`);
+    expect(await this.getTypeaheadValue()).toBe('', `Typeahead input should be cleared initially`);
+  }
+}
---
+++ e2e-app/src/app/typeahead/focus/typeahead-focus.component.ts
@@ -0,0 +1,36 @@
+import { Component, ViewChild } from '@angular/core';
+import {NgbTypeahead} from '@ng-bootstrap/ng-bootstrap';
+import {Observable, Subject, merge} from 'rxjs';
+import {debounceTime, distinctUntilChanged, filter, map} from 'rxjs/operators';
+
+const states = ['Alabama', 'Alaska', 'American Samoa', 'Arizona', 'Arkansas', 'California', 'Colorado',
+  'Connecticut', 'Delaware', 'District Of Columbia', 'Federated States Of Micronesia', 'Florida', 'Georgia',
+  'Guam', 'Hawaii', 'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine',
+  'Marshall Islands', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana',
+  'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+  'Northern Mariana Islands', 'Ohio', 'Oklahoma', 'Oregon', 'Palau', 'Pennsylvania', 'Puerto Rico', 'Rhode Island',
+  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont', 'Virgin Islands', 'Virginia',
+  'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'];
+
+@Component({
+  templateUrl: './typeahead-focus.component.html'
+})
+export class TypeaheadFocusComponent {
+
+  model: any;
+
+  @ViewChild('instance') instance: NgbTypeahead;
+  focus$ = new Subject<string>();
+  click$ = new Subject<string>();
+
+  search = (text$: Observable<string>) => {
+    const debouncedText$ = text$.pipe(distinctUntilChanged());
+    const clicksWithClosedPopup$ = this.click$.pipe(filter(() => !this.instance.isPopupOpen()));
+    const inputFocus$ = this.focus$;
+
+    return merge(debouncedText$, inputFocus$, clicksWithClosedPopup$).pipe(
+      map(term => (term === '' ? states
+        : states.filter(v => v.toLowerCase().indexOf(term.toLowerCase()) > -1)).slice(0, 10))
+    );
+  }
+}
