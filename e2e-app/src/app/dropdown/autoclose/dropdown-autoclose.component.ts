43ce3aee70944ee60f4c872ac2cddb9f768617c1
Max Okorokov <max@okorokov.com>
2018-10-12 16:48:30 +0200

    test(dropdown): add e2e tests for autoClose
    Closes #2787

---
+++ e2e-app/src/app/dropdown/autoclose/dropdown-autoclose.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  templateUrl: './dropdown-autoclose.component.html'
+})
+export class DropdownAutoCloseComponent {
+  autoClose: boolean | 'inside' | 'outside' = true;
+}
