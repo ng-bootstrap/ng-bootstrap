cf0d4b434bec794fe39311d09bd3794719e55a78
Max Okorokov <max@okorokov.com>
2018-10-17 16:37:10 +0200

    demo(datepicker): display Gregorian dates in Hebrew demo
    Closes #2806

--- demo/src/app/components/datepicker/demos/hebrew/datepicker-hebrew.ts
+++ demo/src/app/components/datepicker/demos/hebrew/datepicker-hebrew.ts
@@ -1,7 +1,7 @@
 import {Component} from '@angular/core';
 import {
   NgbCalendar,
-  NgbCalendarHebrew,
+  NgbCalendarHebrew, NgbDate,
   NgbDatepickerI18n,
   NgbDatepickerI18nHebrew,
   NgbDateStruct
@@ -10,6 +10,30 @@ import {
 @Component({
   selector: 'ngbd-datepicker-hebrew',
   templateUrl: './datepicker-hebrew.html',
+  styles: [`
+    .hebrew-day {
+      text-align: right;
+      padding: 0.25rem 0.65rem 0.25rem 0.25rem;
+      border-radius: 0.25rem;
+      display: inline-block;
+      height: 2.75rem;
+      width: 2.75rem;
+    }
+    .hebrew-day:hover, .hebrew-day.focused {
+      background-color: #e6e6e6;
+    }
+    .hebrew-day.selected {
+      background-color: #007bff;
+      color: white;
+    }
+    .outside {
+      color: lightgray;
+    }
+    .gregorian-num {
+      font-size: 0.5rem;
+      direction: ltr;
+    }
+  `],
   providers: [
     {provide: NgbCalendar, useClass: NgbCalendarHebrew},
     {provide: NgbDatepickerI18n, useClass: NgbDatepickerI18nHebrew}
@@ -19,7 +43,15 @@ export class NgbdDatepickerHebrew {
 
   model: NgbDateStruct;
 
-  constructor(private calendar: NgbCalendar) {}
+  constructor(private calendar: NgbCalendar, public i18n: NgbDatepickerI18n) {
+    this.dayTemplateData = this.dayTemplateData.bind(this);
+  }
+
+  dayTemplateData(date: NgbDate) {
+    return {
+      gregorian: (this.calendar as NgbCalendarHebrew).toGregorian(date)
+    };
+  }
 
   selectToday() {
     this.model = this.calendar.getToday();
