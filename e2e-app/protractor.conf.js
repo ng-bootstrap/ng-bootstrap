8423268391aa08c8029437a245d6edab9303ae84
divdavem <david-emmanuel.divernois@amadeus.com>
2018-12-18 15:40:39 +0100

    chore: e2e test coverage

--- e2e-app/protractor.conf.js
+++ e2e-app/protractor.conf.js
@@ -41,5 +41,32 @@ exports.config = {
     if (!process.env.TRAVIS) {
       jasmine.getEnv().addReporter(new SpecReporter({spec: {displayStacktrace: true}}));
     }
+  },
+  async onComplete() {
+    try {
+      console.log('Retrieving coverage...');
+      const coverage = await browser.executeScript('return JSON.stringify(window.__coverage__);');
+      if (coverage) {
+        console.log(`Coverage retrieved (${coverage.length} bytes)`);
+        const fs = require('fs');
+        const path = require('path');
+        fs.mkdirSync(path.join(__dirname, '..', '.nyc_output'));
+        fs.writeFileSync(path.join(__dirname, '..', '.nyc_output', 'out.json'), coverage);
+        const NYC = require('nyc');
+        const nycInstance = new NYC({
+          cwd: path.join(__dirname, '..'),
+          reportDir: 'coverage-e2e',
+          reporter: ['lcov', 'json', 'text-summary']
+        });
+        nycInstance.report();
+        nycInstance.cleanup();
+        console.log("Coverage saved successfully!");
+      } else {
+        console.log("No coverage data!");
+      }
+    } catch (error) {
+      console.log("Error in onComplete:", error);
+      process.exit(1);
+    }
   }
 };
