79f51ab269c2420f1068ff71da0b715f8f3a3772
Max Okorokov <max@okorokov.com>
2019-01-10 11:39:33 +0100

    test(datepicker): add e2e tests for 'autoClose' with 'outsideDays'

--- e2e-app/src/app/datepicker/autoclose/datepicker-autoclose.component.ts
+++ e2e-app/src/app/datepicker/autoclose/datepicker-autoclose.component.ts
@@ -3,4 +3,6 @@ import {Component} from '@angular/core';
 @Component({templateUrl: './datepicker-autoclose.component.html'})
 export class DatepickerAutoCloseComponent {
   autoClose: boolean | 'inside' | 'outside' = true;
+  model = null;
+  displayMonths = 1;
 }
