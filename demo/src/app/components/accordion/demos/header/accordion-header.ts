5d1dcd301c17e6647949fb8aeb5342f924a56071
Max Okorokov <max@okorokov.com>
2019-02-14 16:05:58 +0100

    demo(accordion): custom header demo

---
+++ demo/src/app/components/accordion/demos/header/accordion-header.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngbd-accordion-header',
+  templateUrl: './accordion-header.html'
+})
+export class NgbdAccordionHeader {
+  disabled = false;
+}
