82ef021624c4a53d50fb95f2746c7124a6dc43d1
Max Okorokov <max@okorokov.com>
2019-02-14 16:05:31 +0100

    feat(accordion): allow any content in the accordion card header
    Fixes #717

--- src/accordion/accordion.module.ts
+++ src/accordion/accordion.module.ts
@@ -1,12 +1,22 @@
 import {NgModule, ModuleWithProviders} from '@angular/core';
 import {CommonModule} from '@angular/common';
 
-import {NgbAccordion, NgbPanel, NgbPanelTitle, NgbPanelContent} from './accordion';
+import {NgbAccordion, NgbPanel, NgbPanelTitle, NgbPanelContent, NgbPanelHeader, NgbPanelToggle} from './accordion';
 
-export {NgbAccordion, NgbPanel, NgbPanelTitle, NgbPanelContent, NgbPanelChangeEvent} from './accordion';
+export {
+  NgbAccordion,
+  NgbPanel,
+  NgbPanelTitle,
+  NgbPanelContent,
+  NgbPanelChangeEvent,
+  NgbPanelHeader,
+  NgbPanelHeaderContext,
+  NgbPanelToggle
+} from './accordion';
 export {NgbAccordionConfig} from './accordion-config';
 
-const NGB_ACCORDION_DIRECTIVES = [NgbAccordion, NgbPanel, NgbPanelTitle, NgbPanelContent];
+const NGB_ACCORDION_DIRECTIVES =
+    [NgbAccordion, NgbPanel, NgbPanelTitle, NgbPanelContent, NgbPanelHeader, NgbPanelToggle];
 
 @NgModule({declarations: NGB_ACCORDION_DIRECTIVES, exports: NGB_ACCORDION_DIRECTIVES, imports: [CommonModule]})
 export class NgbAccordionModule {
