ca4545b589b6092322c63ab206fe9c31a91ba78b
Max Okorokov <max@okorokov.com>
2019-03-14 16:09:46 +0100

    chore: extract module name and component selector for stackblitz (#3068)

---
+++ misc/parse-demo-test-cases/no-component-selector/test.module.ts
@@ -0,0 +1,10 @@
+import {NgModule} from '@angular/core';
+
+import {TestComponent} from './test.component';
+
+@NgModule({
+  declarations: [TestComponent],
+  bootstrap: [TestComponent]
+})
+export class TestModule {
+}
