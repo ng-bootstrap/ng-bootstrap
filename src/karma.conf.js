0bc75eb7ef6bf57522968cad4d54aec417fadb10
Max Okorokov <max@okorokov.com>
2019-06-03 10:49:11 +0200

    chore: align config files with CLI 8.0

--- src/karma.conf.js
+++ src/karma.conf.js
@@ -15,6 +15,9 @@ module.exports = function (config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
     ],
+    client: {
+      clearContext: false // leave Jasmine Spec Runner output visible in browser
+    },
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '..', 'coverage'),
       reports: ['html', 'json', 'lcovonly'],
@@ -37,6 +40,7 @@ module.exports = function (config) {
     autoWatch: true,
     browsers,
     singleRun: false,
+    restartOnFileChange: true,
     browserNoActivityTimeout: 20000
   });
 };
--- ssr-app/src/polyfills.ts
+++ ssr-app/src/polyfills.ts
@@ -18,35 +18,6 @@
  * BROWSER POLYFILLS
  */
 
-/** IE9, IE10 and IE11 requires all of the following polyfills. **/
-// import 'core-js/es6/symbol';
-// import 'core-js/es6/object';
-// import 'core-js/es6/function';
-// import 'core-js/es6/parse-int';
-// import 'core-js/es6/parse-float';
-// import 'core-js/es6/number';
-// import 'core-js/es6/math';
-// import 'core-js/es6/string';
-// import 'core-js/es6/date';
-// import 'core-js/es6/array';
-// import 'core-js/es6/regexp';
-// import 'core-js/es6/map';
-// import 'core-js/es6/weak-map';
-// import 'core-js/es6/set';
-
-/**
- * If the application will be indexed by Google Search, the following is required.
- * Googlebot uses a renderer based on Chrome 41.
- * https://developers.google.com/search/docs/guides/rendering
- **/
-// import 'core-js/es6/array';
-
-/** IE10 and IE11 requires the following for NgClass support on SVG elements */
-// import 'classlist.js';  // Run `npm install --save classlist.js`.
-
-/** IE10 and IE11 requires the following for the Reflect API. */
-// import 'core-js/es6/reflect';
-
 /**
  * Web Animations `@angular/platform-browser/animations`
  * Only required if AnimationBuilder is used within the application and using IE/Edge or Safari.
