81077d392fee2b219a43c2d0b95646b612b58c7b
Benoit Charbonnier <benoit.charbonnier@amadeus.com>
2019-09-05 17:05:15 +0200

    refactor(demos): improve icons usage with svg symbols

--- demo/src/app/shared/icons/icons.component.ts
+++ demo/src/app/shared/icons/icons.component.ts
@@ -1,8 +1,17 @@
 import {Component, Input} from '@angular/core';
 
 @Component({
-  selector: 'svg[ngbdIcon]',
+  selector: 'svg[ngbdIcons]',
   templateUrl: './icons.component.html',
+  host: {'xmlns': 'http://www.w3.org/2000/svg', '[class.d-none]': 'true', '[style.fill]': '"currentColor"'}
+})
+export class NgbdIcons {
+}
+
+
+@Component({
+  selector: 'svg[ngbdIcon]',
+  templateUrl: './icon.component.html',
   host: {
     'xmlns': 'http://www.w3.org/2000/svg',
     'viewBox': '0 0 24 24',
@@ -10,7 +19,7 @@ import {Component, Input} from '@angular/core';
     '[attr.height]': 'height',
   }
 })
-export class NgbdIcons {
+export class NgbdIcon {
   @Input() ngbdIcon: string;
   @Input() width = '24';
   @Input() height = '24';
