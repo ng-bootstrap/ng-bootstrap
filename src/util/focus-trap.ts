00d42d6982c2fd9118b1ac79a4b3061f0c6c2c3e
Max Okorokov <max@okorokov.com>
2019-10-25 10:37:01 +0200

    fix(focustrap): run focus trap event handlers outside Angular

--- src/util/focus-trap.ts
+++ src/util/focus-trap.ts
@@ -1,7 +1,9 @@
+import {NgZone} from '@angular/core';
+
 import {fromEvent, Observable} from 'rxjs';
 import {filter, map, takeUntil, withLatestFrom} from 'rxjs/operators';
 
-import {Key} from '../util/key';
+import {Key} from './key';
 
 
 const FOCUSABLE_ELEMENTS_SELECTOR = [
@@ -24,43 +26,47 @@ export function getFocusableBoundaryElements(element: HTMLElement): HTMLElement[
  *
  * Works only for clicks inside the element and navigation with 'Tab', ignoring clicks outside of the element
  *
+ * @param zone Angular zone
  * @param element The element around which focus will be trapped inside
  * @param stopFocusTrap$ The observable stream. When completed the focus trap will clean up listeners
  * and free internal resources
  * @param refocusOnClick Put the focus back to the last focused element whenever a click occurs on element (default to
  * false)
  */
-export const ngbFocusTrap = (element: HTMLElement, stopFocusTrap$: Observable<any>, refocusOnClick = false) => {
-  // last focused element
-  const lastFocusedElement$ =
-      fromEvent<FocusEvent>(element, 'focusin').pipe(takeUntil(stopFocusTrap$), map(e => e.target));
+export const ngbFocusTrap =
+    (zone: NgZone, element: HTMLElement, stopFocusTrap$: Observable<any>, refocusOnClick = false) => {
+      zone.runOutsideAngular(() => {
+        // last focused element
+        const lastFocusedElement$ =
+            fromEvent<FocusEvent>(element, 'focusin').pipe(takeUntil(stopFocusTrap$), map(e => e.target));
 
-  // 'tab' / 'shift+tab' stream
-  fromEvent<KeyboardEvent>(element, 'keydown')
-      .pipe(
-          takeUntil(stopFocusTrap$),
-          // tslint:disable:deprecation
-          filter(e => e.which === Key.Tab),
-          // tslint:enable:deprecation
-          withLatestFrom(lastFocusedElement$))
-      .subscribe(([tabEvent, focusedElement]) => {
-        const[first, last] = getFocusableBoundaryElements(element);
+        // 'tab' / 'shift+tab' stream
+        fromEvent<KeyboardEvent>(element, 'keydown')
+            .pipe(
+                takeUntil(stopFocusTrap$),
+                // tslint:disable:deprecation
+                filter(e => e.which === Key.Tab),
+                // tslint:enable:deprecation
+                withLatestFrom(lastFocusedElement$))
+            .subscribe(([tabEvent, focusedElement]) => {
+              const[first, last] = getFocusableBoundaryElements(element);
 
-        if ((focusedElement === first || focusedElement === element) && tabEvent.shiftKey) {
-          last.focus();
-          tabEvent.preventDefault();
-        }
+              if ((focusedElement === first || focusedElement === element) && tabEvent.shiftKey) {
+                last.focus();
+                tabEvent.preventDefault();
+              }
 
-        if (focusedElement === last && !tabEvent.shiftKey) {
-          first.focus();
-          tabEvent.preventDefault();
+              if (focusedElement === last && !tabEvent.shiftKey) {
+                first.focus();
+                tabEvent.preventDefault();
+              }
+            });
+
+        // inside click
+        if (refocusOnClick) {
+          fromEvent(element, 'click')
+              .pipe(takeUntil(stopFocusTrap$), withLatestFrom(lastFocusedElement$), map(arr => arr[1] as HTMLElement))
+              .subscribe(lastFocusedElement => lastFocusedElement.focus());
         }
       });
-
-  // inside click
-  if (refocusOnClick) {
-    fromEvent(element, 'click')
-        .pipe(takeUntil(stopFocusTrap$), withLatestFrom(lastFocusedElement$), map(arr => arr[1] as HTMLElement))
-        .subscribe(lastFocusedElement => lastFocusedElement.focus());
-  }
-};
+    };
