fbd6e8d720922e35fe4e6769ba36933c468e9ec6
Max Okorokov <max@okorokov.com>
2019-11-15 16:33:34 +0100

    refactor(datepicker): inherit 'NgbInputDatepickerConfig' from 'NgbDatepickerConfig'

--- demo/src/app/components/datepicker/demos/config/datepicker-config.ts
+++ demo/src/app/components/datepicker/demos/config/datepicker-config.ts
@@ -1,6 +1,5 @@
 import {Component} from '@angular/core';
 import {
-  NgbDatepickerConfig,
   NgbCalendar,
   NgbDate,
   NgbDateStruct,
@@ -10,28 +9,27 @@ import {
 @Component({
   selector: 'ngbd-datepicker-config',
   templateUrl: './datepicker-config.html',
-  providers: [NgbDatepickerConfig] // add NgbDatepickerConfig to the component providers
+  providers: [NgbInputDatepickerConfig] // add config to the component providers
 })
 export class NgbdDatepickerConfig {
 
   model: NgbDateStruct;
 
-  constructor(
-      datepickerConfig: NgbDatepickerConfig, inputDatepickerConfig: NgbInputDatepickerConfig, calendar: NgbCalendar) {
+  constructor(config: NgbInputDatepickerConfig, calendar: NgbCalendar) {
     // customize default values of datepickers used by this component tree
-    datepickerConfig.minDate = {year: 1900, month: 1, day: 1};
-    datepickerConfig.maxDate = {year: 2099, month: 12, day: 31};
+    config.minDate = {year: 1900, month: 1, day: 1};
+    config.maxDate = {year: 2099, month: 12, day: 31};
 
     // days that don't belong to current month are not visible
-    datepickerConfig.outsideDays = 'hidden';
+    config.outsideDays = 'hidden';
 
     // weekends are disabled
-    datepickerConfig.markDisabled = (date: NgbDate) => calendar.getWeekday(date) >= 6;
+    config.markDisabled = (date: NgbDate) => calendar.getWeekday(date) >= 6;
 
     // setting datepicker popup to close only on click outside
-    inputDatepickerConfig.autoClose = 'outside';
+    config.autoClose = 'outside';
 
     // setting datepicker popup to open above the input
-    inputDatepickerConfig.placement = ['top-left', 'top-right'];
+    config.placement = ['top-left', 'top-right'];
   }
 }
