9a11107926228de1d8f51301720eacbc40372c19
Benoit Charbonnier <benoit.charbonnier@amadeus.com>
2020-02-07 11:43:52 +0100

    demos: npm download count (#3559)

--- demo/src/app/app.component.ts
+++ demo/src/app/app.component.ts
@@ -1,32 +1,34 @@
-import { ViewportScroller } from '@angular/common';
-import { Component, NgZone, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { filter } from 'rxjs/operators';
+import {ViewportScroller} from '@angular/common';
+import {HttpClient} from '@angular/common/http';
+import {Component, NgZone, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {filter, pluck} from 'rxjs/operators';
 
-import { componentsList } from './shared';
-import { Analytics } from './shared/analytics/analytics';
+import {environment} from '../environments/environment';
+import {componentsList} from './shared';
+import {Analytics} from './shared/analytics/analytics';
+import {of} from 'rxjs';
 
-@Component({
-  selector: 'ngbd-app',
-  templateUrl: './app.component.html'
-})
+
+@Component({selector: 'ngbd-app', templateUrl: './app.component.html'})
 export class AppComponent implements OnInit {
+  downloadCount = '';
   navbarCollapsed = true;
 
   components = componentsList;
 
   constructor(
-    private _analytics: Analytics,
-    route: ActivatedRoute,
-    vps: ViewportScroller,
-    zone: NgZone
-  ) {
-    route.fragment
-      .pipe(filter(fragment => !!fragment))
-      .subscribe(fragment => zone.runOutsideAngular(() => requestAnimationFrame(() => vps.scrollToAnchor(fragment))));
+      private _analytics: Analytics, route: ActivatedRoute, vps: ViewportScroller, zone: NgZone,
+      httpClient: HttpClient) {
+    route.fragment.pipe(filter(fragment => !!fragment))
+        .subscribe(fragment => zone.runOutsideAngular(() => requestAnimationFrame(() => vps.scrollToAnchor(fragment))));
+
+    if (environment.production) {
+      httpClient.get('https://api.npmjs.org/downloads/point/last-month/@ng-bootstrap/ng-bootstrap')
+          .pipe(pluck('downloads'))
+          .subscribe(count => this.downloadCount = count.toLocaleString(), () => of(''));
+    }
   }
 
-  ngOnInit(): void {
-    this._analytics.trackPageViews();
-  }
+  ngOnInit(): void { this._analytics.trackPageViews(); }
 }
