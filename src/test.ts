7322f458b9feca36dd6c8dad702be9cb5a86f051
Max Okorokov <max@okorokov.com>
2020-02-12 10:11:29 +0100

    build: add '@angular/localize' to polyfills

--- src/test.ts
+++ src/test.ts
@@ -2,6 +2,7 @@
 
 import 'zone.js/dist/zone';
 import 'zone.js/dist/zone-testing';
+import '@angular/localize/init';
 
 import {getTestBed} from '@angular/core/testing';
 import {BrowserDynamicTestingModule, platformBrowserDynamicTesting} from '@angular/platform-browser-dynamic/testing';
