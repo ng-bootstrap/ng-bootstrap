7b8569cd41c79750ef7c73fdc3860b3e4b6b7877
fbasso <fabrice.basso@amadeus.com>
2020-06-03 15:42:18 +0200

    demo: changes on examples

--- demo/src/app/components/toast/demos/custom-header/toast-custom-header.ts
+++ demo/src/app/components/toast/demos/custom-header/toast-custom-header.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
-@Component({ selector: 'ngbd-toast-customheader', templateUrl: './toast-custom-header.html' })
-export class NgbdToastCustomHeader {}
+@Component({selector: 'ngbd-toast-customheader', templateUrl: './toast-custom-header.html'})
+export class NgbdToastCustomHeader {
+  show = true;
+}
