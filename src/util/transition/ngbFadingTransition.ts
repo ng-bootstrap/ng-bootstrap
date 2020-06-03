bcc0a54962053025d20229d8a675f34f7e7c9fc0
fbasso <fabrice.basso@amadeus.com>
2020-06-03 15:43:27 +0200

    feat(toast): add animations
    BREAKING CHANGE:
    
    Toast events API has changed to stick with specs provided by Bootstrap
    
    Before:
    
    ```
    <ngb-toast (hide)="..."></ngb-toast>
    ```
    
    After:
    
    ```
    // template
    ngb-toast (hidden)="..."></ngb-toast>
    ```
    
    The `hidden` event is emitted after the 'fade out' animation is

--- src/util/transition/ngbFadingTransition.ts
+++ src/util/transition/ngbFadingTransition.ts
@@ -3,3 +3,19 @@ import {NgbTransitionStartFn} from './ngbTransition';
 export const ngbAlertFadingTransition: NgbTransitionStartFn = ({classList}: HTMLElement) => {
   classList.remove('show');
 };
+
+export const ngbToastFadeInTransition: NgbTransitionStartFn = ({classList}: HTMLElement) => {
+  classList.remove('hide');
+  classList.add('showing');
+
+  return () => {
+    classList.remove('showing');
+    classList.add('show');
+  };
+};
+
+export const ngbToastFadeOutTransition: NgbTransitionStartFn = ({classList}: HTMLElement) => {
+  classList.remove('show');
+
+  return () => { classList.add('hide'); };
+};
