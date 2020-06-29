87941fbc103dd80dfe66d3089723abeaa551773d
Marco Santarelli <marco@backbase.com>
2020-06-29 14:56:47 +0200

    fix: addressing warning related to protractor config displayStacktrace options are now string type, replaced true with 'pretty'

--- e2e-app/protractor.conf.js
+++ e2e-app/protractor.conf.js
@@ -39,7 +39,7 @@ exports.config = {
     });
 
     if (!process.env.TRAVIS) {
-      jasmine.getEnv().addReporter(new SpecReporter({spec: {displayStacktrace: true}}));
+      jasmine.getEnv().addReporter(new SpecReporter({spec: {displayStacktrace: 'pretty'}}));
     }
   },
   async onComplete() {
