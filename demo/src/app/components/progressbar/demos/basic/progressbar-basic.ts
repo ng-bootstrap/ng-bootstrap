ea9921a9335f1a60f0c479df57e6830d4dfc9b13
zVolodymyr <volodymyr.baydalka@gmail.com>
2020-07-29 21:49:31 +0300

    feat(progressbar): simplify template

--- demo/src/app/components/progressbar/demos/basic/progressbar-basic.ts
+++ demo/src/app/components/progressbar/demos/basic/progressbar-basic.ts
@@ -2,12 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'ngbd-progressbar-basic',
-  templateUrl: './progressbar-basic.html',
-  styles: [`
-    ngb-progressbar {
-      margin-top: 5rem;
-    }
-  `]
+  templateUrl: './progressbar-basic.html'
 })
 export class NgbdProgressbarBasic {
 }
