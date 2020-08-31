4fdf6469aa2ac291b5aebb39bdf012453e1531ef
Benoit Charbonnier <benoit.charbonnier@amadeus.com>
2020-08-31 09:43:37 +0200

    demo: improved UX for sidebar (#3829)

---
+++ demo/src/app/pages/i18n/i18n.component.ts
@@ -0,0 +1,9 @@
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+
+@Component({
+  templateUrl: './i18n.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class I18nPage {
+
+}
