3bc321931a9f53a88498f941c49e83db30d34084
Max Okorokov <max@okorokov.com>
2020-10-15 10:59:31 +0200

    docs: update animations documentation page for 8.0.0

--- demo/src/app/pages/animations/animations.component.ts
+++ demo/src/app/pages/animations/animations.component.ts
@@ -6,11 +6,6 @@ import {versions} from '../../../environments/versions';
 export class AnimationsPage {
   bsVersion = versions.bootstrap;
 
-  install = Snippet({
-    lang: 'bash',
-    code: `npm install @ng-bootstrap/ng-bootstrap@next`
-  });
-
   config = Snippet({
     lang: 'typescript',
     code: `
@@ -47,4 +42,102 @@ export class AnimationsPage {
   });
 
   alert = Snippet({lang: 'html', code: `<ngb-alert *ngIf="isOpen" (closed)="isOpen = false">I'm open</ngb-alert>`});
+
+  recipeAccordion = Snippet({
+    lang: 'html',
+    code: `
+    <ngb-accordion (shown)="onPanelShown($event)" (hidden)="onPanelHidden($event)">
+      <ngb-panel id="one" (shown)="onPanelShown('one')" (hidden)="onPanelHidden('one')">
+        ...
+      </ngb-panel>
+    </ngb-accordion>
+    `
+  });
+
+  recipeAlertTpl = Snippet({
+    lang: 'html',
+    code: `
+    <ngb-alert #a *ngIf="visible" (closed)="visible = false">...</ngb-alert>
+    <button (click)="a.close()">Close</button>
+    `
+  });
+
+  recipeAlertCmp = Snippet({
+    lang: 'typescript',
+    code: `
+    alert.close().subscribe(() => {});
+    `
+  });
+
+  recipeCarousel = Snippet({
+    lang: 'html',
+    code: `
+    <ngb-carousel (slide)="onSlideEvent($event)" (slid)="onSlideEvent($event)">
+      <ng-template ngbSlide id="one" (slid)="onSignleSlideEvent($event)">
+      ...
+      </ng-template>
+    </ngb-carousel>
+    `
+  });
+
+  recipeCollapse = Snippet({
+    lang: 'html',
+    code: `
+    <div #c="ngbCollapse" [(ngbCollapse)]="isCollapsed"
+        (shown)="onShown()" (hidden)="onHidden()">
+      ...
+    </div>
+    <button (click)="c.toggle()">Toggle</button>
+    `
+  });
+
+  recipeModal = Snippet({
+    lang: 'typescript',
+    code: `
+    const modalRef = this.modalService.open(...);
+
+    modalRef.closed.subscribe(result => {});
+    modalRef.dismissed.subscribe(reason => {});
+    modalRef.shown.subscribe(() => {});
+    modalRef.hidden.subscribe(() => {});
+    `
+  });
+
+  recipeNav = Snippet({
+    lang: 'html',
+    code: `
+    <ul ngbNav class="nav-tabs" (shown)="onShown($event)" (hidden)="onHidden($event)">
+      <li ngbNavItem="one" (shown)="onShown('one')" (hidden)="onHidden('one')">
+        <a ngbNavLink>...</a>
+        <ng-template ngbNavContent>...</ng-template>
+      </li>
+    </ul>
+    `
+  });
+
+  recipePopoverTooltip = Snippet({
+    lang: 'html',
+    code: `
+    <button ngbPopover="popover!" (shown)="onShown()" (hidden)="onHidden()"></button>
+    <button ngbTooltip="tooltip!" (shown)="onShown()" (hidden)="onHidden()"></button>
+    `
+  });
+
+  recipeToastTpl = Snippet({
+    lang: 'html',
+    code: `
+    <ngb-toast *ngIf="visible" header="Hello" [autohide]="false"
+        (shown)="onShown()" (hidden)="onHidden(); visible = false">
+    ...
+    </ngb-toast>
+    `
+  });
+
+  recipeToastCmp = Snippet({
+    lang: 'typescript',
+    code: `
+    toast.show().subscribe(() => {});
+    toast.hide().subscribe(() => {});
+    `
+  });
 }
