8edbcc997da63ca5d1d6f0213446dcbfa4e29332
Max Okorokov <max@okorokov.com>
2021-01-14 10:39:28 +0100

    test: remove IE10 from testing
    Remove SauceLabs tests and IE10 special cases in unit tests

--- src/karma.sauce.conf.js
+++ src/karma.sauce.conf.js
@@ -25,12 +25,6 @@ const BROWSERS = {
     platform: 'Windows 10',
     version: '18.17763'
   },
-  'IE10': {
-    base: 'SauceLabs',
-    browserName: 'internet explorer',
-    platform: 'Windows 8',
-    version: '10'
-  },
   'IE11': {
     base: 'SauceLabs',
     browserName: 'internet explorer',
