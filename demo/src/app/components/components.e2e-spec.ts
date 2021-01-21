0e78d1b9e18c1db0ee0f0987caab0df55884c9b5
Max Okorokov <max@okorokov.com>
2021-01-21 17:44:39 +0100

    demo: migrate demo e2e tests to Playwright

--- demo/src/app/components/components.e2e-spec.ts
+++ demo/src/app/components/components.e2e-spec.ts
@@ -1,59 +1,66 @@
-import {browser} from 'protractor';
-import {
-  getLinkElement,
-  getComponentTabsLinks,
-  getCodeToggleElement,
-  getCodeElements,
-  scrollIntoView
-} from '../tools.po';
+import {BASE_URL, playwright} from '../../../playwright.conf';
+import {ConsoleMessage} from 'playwright';
 
+const SELECTOR_TAB_LINKS = 'header.title a.nav-link';
+const SELECTOR_SIDE_NAV_COMPONENT_LINKS = 'ngbd-side-nav >> a[href^="#/components/"]';
+const SELECTOR_CODE_BUTTONS = 'button.toggle-code';
 
+const COMPONENTS = [
+  'accordion', 'alert', 'buttons', 'carousel', 'collapse', 'datepicker', 'dropdown', 'modal', 'nav', 'pagination',
+  'popover', 'progressbar', 'rating', 'table', 'tabset', 'timepicker', 'toast', 'tooltip', 'typeahead'
+];
 
 describe(`Components`, () => {
 
-  beforeAll(async() => { await getLinkElement(`components`).click(); });
+  let messages: ConsoleMessage[] = [];
 
-  const components = [
-    'accordion', 'alert', 'buttons', 'carousel', 'collapse', 'datepicker', 'dropdown', 'modal', 'nav', 'pagination',
-    'popover', 'progressbar', 'rating', 'table', 'tabset', 'timepicker', 'toast', 'tooltip', 'typeahead'
-  ];
-  components.forEach((component) => {
+  beforeAll(async () => {
+    // just opening the 'Components' page to dump initial console messages
+    await playwright.newPage();
+    playwright.page.on('console', msg => messages.push(msg));
+
+    await playwright.page.goto(`${BASE_URL}/components`);
+    await playwright.page.waitForSelector(SELECTOR_SIDE_NAV_COMPONENT_LINKS);
+  });
+
+  it('should cover all components we have', async() => {
+    for (const link of await playwright.page.$$(SELECTOR_SIDE_NAV_COMPONENT_LINKS)) {
+      const componentName = await link.innerText();
+      expect(COMPONENTS).toContain(componentName.toLowerCase(), `'${componentName}' is not covered by demo e2e tests`);
+    }
+  });
+
+  COMPONENTS.forEach(component => {
     describe(`${component} page`, () => {
 
+      const SELECTOR_EXAMPLE_LINK = `a[href="#/components/${component}/examples"]`;
+      const SELECTOR_SIDE_NAV_COMPONENT_LINK = `ngbd-side-nav >> a[href^="#/components/${component}"]`;
 
       beforeAll(async() => {
-        const selector = `components/${component}`;
-        await scrollIntoView(`a[href="#/${selector}"]`);
-        await getLinkElement(selector).click();
+        messages = [];
+        await playwright.page.click(SELECTOR_SIDE_NAV_COMPONENT_LINK);
+        await playwright.page.waitForSelector(SELECTOR_TAB_LINKS);
       });
 
       afterAll(async() => {
-        const browserLog = await browser.manage().logs().get('browser');
-        if (browserLog.length > 0) {
-          console.error(browserLog);
-          fail(`Unexpected console messages found`);
+        if (messages.length > 0) {
+          console.log(messages);
+          fail('Unexpected console messages found');
         }
       });
 
       it(`should display the tabs`, async() => {
-        const links = getComponentTabsLinks(component);
-        const nbTabs = await links.count();
-        for (let i = 0; i < nbTabs; i++) {
-          await links.get(i).click();
+        for (const link of await playwright.page.$$(SELECTOR_TAB_LINKS)) {
+          await link.click();
         }
       });
 
       it(`should display code samples`, async() => {
-        await getLinkElement(`components/${component}/examples`).click();
-
-        const initialCodeElements = await getCodeElements().count();
-        await getCodeToggleElement().click();
-        expect(await getCodeElements().count()).toBe(initialCodeElements + 1);
-        await getCodeToggleElement().click();
-        expect(await getCodeElements().count()).toBe(initialCodeElements);
+        await playwright.page.click(SELECTOR_EXAMPLE_LINK);
+        for (const link of await playwright.page.$$(SELECTOR_CODE_BUTTONS)) {
+          await link.click();
+        }
       });
-
     });
-
   });
 });
