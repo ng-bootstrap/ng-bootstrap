175c7ab5164eeda3868186dce5c9065ae6ee07b5
Max Okorokov <max@okorokov.com>
2021-01-21 09:18:55 +0100

    fix(carousel): respect `[animation]="false"` (#3964)
    Fixes #3961

--- src/carousel/carousel-transition.ts
+++ src/carousel/carousel-transition.ts
@@ -11,17 +11,17 @@ export enum NgbSlideEventDirection {
 
 export interface NgbCarouselCtx { direction: 'left' | 'right'; }
 
-const isAnimated = ({classList}) => {
+const isBeingAnimated = ({classList}: HTMLElement) => {
   return classList.contains('carousel-item-left') || classList.contains('carousel-item-right');
 };
 
-const removeDirectionClasses = ({classList}) => {
+const removeDirectionClasses = (classList: DOMTokenList) => {
   classList.remove('carousel-item-left');
   classList.remove('carousel-item-right');
 };
 
-const removeClasses = ({classList}) => {
-  removeDirectionClasses({classList});
+const removeClasses = (classList: DOMTokenList) => {
+  removeDirectionClasses(classList);
   classList.remove('carousel-item-prev');
   classList.remove('carousel-item-next');
 };
@@ -29,9 +29,17 @@ const removeClasses = ({classList}) => {
 export const ngbCarouselTransitionIn: NgbTransitionStartFn<NgbCarouselCtx> =
     (element: HTMLElement, animation: boolean, {direction}: NgbCarouselCtx) => {
       const {classList} = element;
-      if (isAnimated(element)) {
+
+      if (!animation) {
+        removeDirectionClasses(classList);
+        removeClasses(classList);
+        classList.add('active');
+        return;
+      }
+
+      if (isBeingAnimated(element)) {
         // Revert the transition
-        removeDirectionClasses(element);
+        removeDirectionClasses(classList);
       } else {
         // For the 'in' transition, a 'pre-class' is applied to the element to ensure its visibility
         classList.add('carousel-item-' + (direction === NgbSlideEventDirection.LEFT ? 'next' : 'prev'));
@@ -40,7 +48,7 @@ export const ngbCarouselTransitionIn: NgbTransitionStartFn<NgbCarouselCtx> =
       }
 
       return () => {
-        removeClasses(element);
+        removeClasses(classList);
         classList.add('active');
       };
     };
@@ -48,16 +56,24 @@ export const ngbCarouselTransitionIn: NgbTransitionStartFn<NgbCarouselCtx> =
 export const ngbCarouselTransitionOut: NgbTransitionStartFn<NgbCarouselCtx> =
     (element: HTMLElement, animation: boolean, {direction}: NgbCarouselCtx) => {
       const {classList} = element;
+
+      if (!animation) {
+        removeDirectionClasses(classList);
+        removeClasses(classList);
+        classList.remove('active');
+        return;
+      }
+
       //  direction is left or right, depending on the way the slide goes out.
-      if (isAnimated(element)) {
+      if (isBeingAnimated(element)) {
         // Revert the transition
-        removeDirectionClasses(element);
+        removeDirectionClasses(classList);
       } else {
         classList.add('carousel-item-' + direction);
       }
 
       return () => {
-        removeClasses(element);
+        removeClasses(classList);
         classList.remove('active');
       };
     };
