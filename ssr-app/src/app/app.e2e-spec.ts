c71d2b26e64f2738ccba0cee3a1856cd18467373
Max Okorokov <max@okorokov.com>
2021-01-22 14:46:08 +0100

    test: migrate ssr-app e2e tests to Playwright

--- ssr-app/src/app/app.e2e-spec.ts
+++ ssr-app/src/app/app.e2e-spec.ts
@@ -1,13 +1,28 @@
-import { browser } from 'protractor';
-import { ApplicationPage } from './app.po';
+import {BASE_URL, playwright} from '../../playwright.conf';
+import {ConsoleMessage} from 'playwright';
+
+const SELECTOR_HEADER = 'h1';
 
 describe('SSR application', () => {
 
-  const page = new ApplicationPage();
+  const messages: ConsoleMessage[] = [];
 
-  beforeAll(async () => await browser.get('/'));
+  beforeAll(async () => {
+    await playwright.newPage();
+    playwright.page.on('console', msg => messages.push(msg));
+
+    await playwright.page.goto(BASE_URL);
+    await playwright.page.waitForSelector(SELECTOR_HEADER);
+  });
+
+  afterAll(async() => {
+    if (messages.length > 0) {
+      console.log(messages);
+      fail('Unexpected console messages found');
+    }
+  });
 
   it('should open server side rendered page without failures', async () => {
-    expect(await page.getHeading().getText()).toBe('ng-bootstrap SSR test application');
+    expect(await playwright.page.innerText(SELECTOR_HEADER)).toBe('ng-bootstrap SSR test application');
   });
 });
