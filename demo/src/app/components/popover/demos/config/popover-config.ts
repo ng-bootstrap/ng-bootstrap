48f32d6e169c85ac95e2ca9c8ac451ff93743411
fbasso <fabrice.basso@amadeus.com>
2021-01-26 11:31:49 +0100

    feat(popover): rename left right classes for positioning

--- demo/src/app/components/popover/demos/config/popover-config.ts
+++ demo/src/app/components/popover/demos/config/popover-config.ts
@@ -4,12 +4,12 @@ import {NgbPopoverConfig} from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'ngbd-popover-config',
   templateUrl: './popover-config.html',
-  providers: [NgbPopoverConfig] // add NgbPopoverConfig to the component providers
+  providers: [NgbPopoverConfig]  // add NgbPopoverConfig to the component providers
 })
 export class NgbdPopoverConfig {
   constructor(config: NgbPopoverConfig) {
     // customize default values of popovers used by this component tree
-    config.placement = 'right';
+    config.placement = 'end';
     config.triggers = 'hover';
   }
 }
