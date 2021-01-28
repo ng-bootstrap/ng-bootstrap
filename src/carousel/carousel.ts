e919f1999d1942ddc63c333d1dc111662548de80
ExFlo <florent.exbrayat@amadeus.com>
2021-01-28 12:54:47 +0100

    fix: renamed .sr-only to .visually-hidden (#3982)

--- src/carousel/carousel.ts
+++ src/carousel/carousel.ts
@@ -80,7 +80,7 @@ export class NgbSlide {
     '[attr.aria-activedescendant]': `'slide-' + activeId`
   },
   template: `
-    <ol class="carousel-indicators" [class.sr-only]="!showNavigationIndicators" role="tablist">
+    <ol class="carousel-indicators" [class.visually-hidden]="!showNavigationIndicators" role="tablist">
       <li *ngFor="let slide of slides" [class.active]="slide.id === activeId"
           role="tab" [attr.aria-labelledby]="'slide-' + slide.id" [attr.aria-controls]="'slide-' + slide.id"
           [attr.aria-selected]="slide.id === activeId"
@@ -88,7 +88,7 @@ export class NgbSlide {
     </ol>
     <div class="carousel-inner">
       <div *ngFor="let slide of slides; index as i; count as c" class="carousel-item" [id]="'slide-' + slide.id" role="tabpanel">
-        <span class="sr-only" i18n="Currently selected slide number read by screen reader@@ngb.carousel.slide-number">
+        <span class="visually-hidden" i18n="Currently selected slide number read by screen reader@@ngb.carousel.slide-number">
           Slide {{i + 1}} of {{c}}
         </span>
         <ng-template [ngTemplateOutlet]="slide.tplRef"></ng-template>
@@ -96,11 +96,11 @@ export class NgbSlide {
     </div>
     <a class="carousel-control-prev" role="button" (click)="arrowLeft()" *ngIf="showNavigationArrows">
       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-      <span class="sr-only" i18n="@@ngb.carousel.previous">Previous</span>
+      <span class="visually-hidden" i18n="@@ngb.carousel.previous">Previous</span>
     </a>
     <a class="carousel-control-next" role="button" (click)="arrowRight()" *ngIf="showNavigationArrows">
       <span class="carousel-control-next-icon" aria-hidden="true"></span>
-      <span class="sr-only" i18n="@@ngb.carousel.next">Next</span>
+      <span class="visually-hidden" i18n="@@ngb.carousel.next">Next</span>
     </a>
   `
 })
