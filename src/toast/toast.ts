5c03fcd5d3eaf8f4ae30e8cb3fa5ce43994cb63b
Nordine Bittich <nordine.bittich@ext.europa.ec.eu>
2021-01-28 13:05:45 +0100

    fix(toast): new css class for btn-close

--- src/toast/toast.ts
+++ src/toast/toast.ts
@@ -52,13 +52,12 @@ export class NgbToastHeader {
   },
   template: `
     <ng-template #headerTpl>
-      <strong class="mr-auto">{{header}}</strong>
+      <strong class="me-auto">{{header}}</strong>
     </ng-template>
     <ng-template [ngIf]="contentHeaderTpl || header">
       <div class="toast-header">
         <ng-template [ngTemplateOutlet]="contentHeaderTpl || headerTpl"></ng-template>
-        <button type="button" class="close" aria-label="Close" i18n-aria-label="@@ngb.toast.close-aria" (click)="hide()">
-          <span aria-hidden="true">&times;</span>
+        <button type="button" class="btn-close" aria-label="Close" i18n-aria-label="@@ngb.toast.close-aria" (click)="hide()">
         </button>
       </div>
     </ng-template>
