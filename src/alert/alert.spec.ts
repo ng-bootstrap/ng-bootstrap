0bab5e129a9eb5d6b735459bf605025ee66decad
Nordine Bittich <3816305+nbittich@users.noreply.github.com>
2021-01-28 12:53:21 +0100

    fix(alert): new css class for close button (#3976)
    Co-authored-by: Nordine Bittich <nordine.bittich@ext.europa.ec.eu>

--- src/alert/alert.spec.ts
+++ src/alert/alert.spec.ts
@@ -23,10 +23,6 @@ function getCloseButton(element: HTMLElement): HTMLButtonElement {
   return <HTMLButtonElement>element.querySelector('button');
 }
 
-function getCloseButtonIcon(element: HTMLElement): HTMLSpanElement {
-  return <HTMLSpanElement>element.querySelector('button > span');
-}
-
 describe('ngb-alert', () => {
 
   beforeEach(() => { TestBed.configureTestingModule({declarations: [TestComponent], imports: [NgbAlertModule]}); });
@@ -83,14 +79,11 @@ describe('ngb-alert', () => {
     const fixture = createTestComponent('<ngb-alert [dismissible]="true">Watch out!</ngb-alert>');
     const alertEl = getAlertElement(fixture.nativeElement);
     const buttonEl = getCloseButton(alertEl);
-    const buttonIconEl = getCloseButtonIcon(alertEl);
 
     expect(alertEl).toHaveCssClass('alert-dismissible');
     expect(buttonEl).toBeTruthy();
-    expect(buttonEl.getAttribute('class')).toContain('close');
+    expect(buttonEl.getAttribute('class')).toContain('btn-close');
     expect(buttonEl.getAttribute('aria-label')).toBe('Close');
-    expect(buttonIconEl.getAttribute('aria-hidden')).toBe('true');
-    expect(buttonIconEl.textContent).toBe('×');
   });
 
   it('should not render the close button if it is not dismissible', () => {
