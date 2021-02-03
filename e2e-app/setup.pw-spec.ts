d43bb0d860645dfdf227022f629dd04261dc9d3f
Max Okorokov <max@okorokov.com>
2021-02-03 15:29:15 +0100

    test: extract coverage only from chromium tests

--- e2e-app/setup.pw-spec.ts
+++ e2e-app/setup.pw-spec.ts
@@ -12,29 +12,27 @@ beforeAll(async() => {
   }
 });
 
-afterAll(async() => {
-  try {
-    console.log('Retrieving coverage...');
-    const coverage: string = await page().evaluate('JSON.stringify(window.__coverage__);');
-    if (coverage) {
-      console.log(`Coverage retrieved (${coverage.length} bytes)`);
-      fs.mkdirSync(path.join(__dirname, '..', '.nyc_output'));
-      fs.writeFileSync(path.join(__dirname, '..', '.nyc_output', 'out.json'), coverage);
-      const NYC = require('nyc');
-      const nycInstance = new NYC({
-        cwd: path.join(__dirname, '..'),
-        reportDir: `coverage-e2e-${browserName}`,
-        reporter: ['lcov', 'json', 'text-summary']
-      });
-      nycInstance.report();
-      nycInstance.cleanup();
-      console.log('Coverage saved successfully!');
-    } else {
-      console.log('No coverage data!');
+if (browserName === 'chromium') {
+  afterAll(async() => {
+    try {
+      console.log('Retrieving coverage...');
+      const coverage: string = await page().evaluate('JSON.stringify(window.__coverage__);');
+      if (coverage) {
+        console.log(`Coverage retrieved (${coverage.length} bytes)`);
+        fs.mkdirSync(path.join(__dirname, '..', '.nyc_output'));
+        fs.writeFileSync(path.join(__dirname, '..', '.nyc_output', 'out.json'), coverage);
+        const NYC = require('nyc');
+        const nycInstance = new NYC(
+            {cwd: path.join(__dirname, '..'), reportDir: `coverage-e2e`, reporter: ['lcov', 'json', 'text-summary']});
+        nycInstance.report();
+        nycInstance.cleanup();
+        console.log('Coverage saved successfully!');
+      } else {
+        console.log('No coverage data!');
+      }
+    } catch (error) {
+      console.log('Error in onComplete:', error);
+      process.exit(1);
     }
-  } catch (error) {
-    console.log('Error in onComplete:', error);
-    process.exit(1);
-  }
-
-});
+  });
+}
