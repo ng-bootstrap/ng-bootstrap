ccbe885ad2ca76a6b08b6fb6668756b8350567ef
Max Okorokov <max@okorokov.com>
2021-03-22 11:49:30 +0100

    test: add a minimal CLI test app to the project
    Can be used for issue reproduction/fixes as it references ng-bootstrap sources directly

---
+++ test-app/src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <div class="container">
+      <ngb-alert type="success" [dismissible]="false">
+        This is a minimal CLI app for issue reproduction purposes
+      </ngb-alert>
+    </div>
+  `,
+})
+export class AppComponent {
+}
