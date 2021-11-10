fad207d6cadb5b908b2b1395e0991a77a2f6d20d
Max Okorokov <max@okorokov.com>
2021-11-10 13:35:56 +0100

    build: remove IE from build/testing pipeline

--- demo/src/polyfills.ts
+++ demo/src/polyfills.ts
@@ -14,17 +14,6 @@
  * Learn more in https://angular.io/docs/ts/latest/guide/browser-support.html
  */
 
-/***************************************************************************************************
- * BROWSER POLYFILLS
- */
-import 'core-js/fn/object/values';
-
-/**
- * Required to support Web Animations `@angular/platform-browser/animations`.
- * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
- **/
-// import 'web-animations-js';  // Run `npm install --save web-animations-js`.
-
 
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
