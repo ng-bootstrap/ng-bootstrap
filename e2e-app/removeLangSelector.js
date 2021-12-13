4d46bf3d63a43fa855d9f354ddfcf1616aaba219
David-Emmanuel DIVERNOIS <david-emmanuel.divernois@amadeus.com>
2021-12-13 14:47:56 +0100

    build: re-enable console warning reporting in e2e tests
    Reverts commit a8b2575ceadca25f8b6c3c1b07f50e4cabf49e87 and adds a webpack loader
    to remove from bootstrap during the e2e-app build the :lang(en) selector that causes
    the warning.

---
+++ e2e-app/removeLangSelector.js
@@ -0,0 +1,5 @@
+// This loader is used to remove from bootstrap during the e2e-app build
+// the :lang(en) selector that causes a warning.
+module.exports = function (file) {
+  return file.replace(/:lang\(en\)/g, "");
+};
