0a1275c696c14788b430657bc015f7d9efce4ce9
divdavem <david-emmanuel.divernois@amadeus.com>
2022-02-23 11:49:46 +0100

    build(schematics): clean from rollup config instead of using rimraf (#4262)

--- schematics/rollup.config.js
+++ schematics/rollup.config.js
@@ -2,13 +2,18 @@ const pkg = require("../package.json");
 const typescript = require("@rollup/plugin-typescript");
 const replace = require("@rollup/plugin-replace");
 const path = require("path");
+const {promises: fs} = require("fs");
 
 const version = (str) => JSON.stringify(str.startsWith("^") ? str : "^" + str);
+
+const distDir = path.join(__dirname, "dist");
+
+const clean = () => ({async buildStart() { await fs.rm(distDir, {force: true, recursive: true}); }});
 
 module.exports = [
   {
     output: {
-      dir: path.join(__dirname, "dist"),
+      dir: distDir,
       format: "cjs",
       exports: "named",
     },
@@ -23,6 +28,7 @@ module.exports = [
         dependency.startsWith(__dirname)
       ),
     plugins: [
+      clean(),
       replace({
         preventAssignment: true,
         "VERSIONS.BOOTSTRAP": version(pkg.devDependencies.bootstrap),
