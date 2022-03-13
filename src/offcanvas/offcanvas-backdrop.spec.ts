3d7026ffcb6dd71e047634b795ac1b2143320a0c
jnizet <jb@ninja-squad.com>
2022-03-13 08:40:41 +0100

    test(offcanvas): add tests for offcanvas backdrop

---
+++ src/offcanvas/offcanvas-backdrop.spec.ts
@@ -0,0 +1,47 @@
+import {TestBed} from '@angular/core/testing';
+
+import {NgbOffcanvasBackdrop} from './offcanvas-backdrop';
+import {OffcanvasDismissReasons} from './offcanvas-dismiss-reasons';
+
+describe('ngb-offcanvas-backdrop', () => {
+
+  beforeEach(() => { TestBed.configureTestingModule({declarations: [NgbOffcanvasBackdrop]}); });
+
+  it('should render backdrop with required CSS classes', () => {
+    const fixture = TestBed.createComponent(NgbOffcanvasBackdrop);
+
+    fixture.detectChanges();
+    expect(fixture.nativeElement).toHaveCssClass('offcanvas-backdrop');
+    expect(fixture.nativeElement).toHaveCssClass('show');
+    expect(fixture.nativeElement).not.toHaveCssClass('fade');
+  });
+
+  it('should render correct CSS classes for animations', () => {
+    const fixture = TestBed.createComponent(NgbOffcanvasBackdrop);
+    fixture.componentInstance.animation = true;
+
+    fixture.detectChanges();
+    expect(fixture.nativeElement).toHaveCssClass('show');
+    expect(fixture.nativeElement).toHaveCssClass('fade');
+  });
+
+  it('should render custom CSS class', () => {
+    const fixture = TestBed.createComponent(NgbOffcanvasBackdrop);
+    fixture.componentInstance.backdropClass = 'custom-backdrop';
+
+    fixture.detectChanges();
+    expect(fixture.nativeElement).toHaveCssClass('custom-backdrop');
+  });
+
+  it('should emit dismiss event on click', () => {
+    const fixture = TestBed.createComponent(NgbOffcanvasBackdrop);
+    fixture.detectChanges();
+
+    let emittedEvent: any;
+    fixture.componentInstance.dismissEvent.subscribe(event => emittedEvent = event);
+    fixture.nativeElement.dispatchEvent(new Event('click'));
+    fixture.detectChanges();
+
+    expect(emittedEvent).toBe(OffcanvasDismissReasons.BACKDROP_CLICK);
+  });
+});
