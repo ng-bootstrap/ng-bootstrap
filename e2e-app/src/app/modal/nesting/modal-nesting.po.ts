801dfb377f09fff301ce45428ff3bd3abb82d0c1
Max Okorokov <max@okorokov.com>
2022-04-04 15:35:12 +0200

    test: add z-index tests for the modal nested components

--- e2e-app/src/app/modal/nesting/modal-nesting.po.ts
+++ e2e-app/src/app/modal/nesting/modal-nesting.po.ts
@@ -3,16 +3,27 @@ import {focusElement, sendKey} from '../../tools.po';
 import {waitForModalCount} from '../modal.po';
 
 export const SELECTOR_DATEPICKER = 'ngb-datepicker';
+export const SELECTOR_CLOSE_BUTTON = '.btn-close';
 export const SELECTOR_DATEPICKER_BUTTON = '#datepicker-button';
 export const SELECTOR_DROPDOWN_BUTTON = '#dropdown';
 export const SELECTOR_DROPDOWN = '[ngbDropdown].show';
-export const SELECTOR_TYPEAHEAD_INPUT = '#typeahead';
+export const SELECTOR_DROPDOWN_ITEM = 'button[ngbDropdownItem]';
+export const SELECTOR_TYPEAHEAD_INPUT = 'input#typeahead';
 export const SELECTOR_TYPEAHEAD_DROPDOWN = 'ngb-typeahead-window.show';
+export const SELECTOR_TYPEAHEAD_DROPDOWN_ITEM = 'ngb-typeahead-window > button';
+export const SELECTOR_TOOLTIP_BUTTON = 'button#tooltip';
+export const SELECTOR_TOOLTIP = 'ngb-tooltip-window';
+export const SELECTOR_POPOVER_BUTTON = 'button#popover';
+export const SELECTOR_POPOVER = 'ngb-popover-window';
 
 
 export const openModal = async() => {
   await getPage().click(`#open-modal`);
   await waitForModalCount(1);
+};
+
+export const setContainer = async(type: string) => {
+  await getPage().click(`#container-${type}`);
 };
 
 export const pressButton = async(buttonSelector) => {
