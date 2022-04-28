cae915a6fc0a47afc2befa700ee76ba0cff37787
JB Nizet <jb@ninja-squad.com>
2022-04-28 10:27:41 +0200

    demo(tooltip): fix custom css for tooltip arrow (#4311)
    fix #4310

--- demo/src/app/components/tooltip/demos/customclass/tooltip-customclass.ts
+++ demo/src/app/components/tooltip/demos/customclass/tooltip-customclass.ts
@@ -9,8 +9,17 @@ import {Component, ViewEncapsulation} from '@angular/core';
       background-color: darkgreen;
       font-size: 125%;
     }
-    .my-custom-class .arrow::before {
+    .my-custom-class.bs-tooltip-end .tooltip-arrow::before {
+      border-right-color: darkgreen;
+    }
+    .my-custom-class.bs-tooltip-start .tooltip-arrow::before {
+      border-left-color: darkgreen;
+    }
+    .my-custom-class.bs-tooltip-top .tooltip-arrow::before {
       border-top-color: darkgreen;
+    }
+    .my-custom-class.bs-tooltip-bottom .tooltip-arrow::before {
+      border-bottom-color: darkgreen;
     }
   `]
 })
