893f10049e4c3ed23b2ba71c8fcea8d62f41b07d
Max Okorokov <max@okorokov.com>
2022-07-28 15:20:44 +0200

    demo: align demo site with Bootstrap 5.2

--- demo/src/app/components/tooltip/demos/basic/tooltip-basic.ts
+++ demo/src/app/components/tooltip/demos/basic/tooltip-basic.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'ngbd-tooltip-basic',
-  templateUrl: './tooltip-basic.html'
+  templateUrl: './tooltip-basic.html',
+  host: { 'class': 'd-block' }
 })
 export class NgbdTooltipBasic {
 }
