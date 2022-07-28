6e2a590972a8c22c8bd2097f98cfcf58ac4c6e81
Max Okorokov <max@okorokov.com>
2022-07-28 15:20:44 +0200

    demo: align demo site with Bootstrap 5.2

--- demo/src/app/components/popover/demos/basic/popover-basic.ts
+++ demo/src/app/components/popover/demos/basic/popover-basic.ts
@@ -2,7 +2,8 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'ngbd-popover-basic',
-  templateUrl: './popover-basic.html'
+  templateUrl: './popover-basic.html',
+  host: { 'class': 'd-block' }
 })
 export class NgbdPopoverBasic {
 }
