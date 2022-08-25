b23cf101c5b1687cd3e73b88fe2afd734e6998e4
Max Okorokov <max@okorokov.com>
2022-08-25 14:48:39 +0200

    demo: simplify `positionTarget` demo

--- demo/src/app/components/popover/demos/custom-target/popover-target.ts
+++ demo/src/app/components/popover/demos/custom-target/popover-target.ts
@@ -1,21 +1,8 @@
-import {Component, HostListener, ViewChild} from '@angular/core';
-import {NgbPopover} from '@ng-bootstrap/ng-bootstrap';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'ngbd-popover-target',
   templateUrl: './popover-target.html'
 })
 export class NgbdPopoverTarget {
-  @ViewChild(NgbPopover, {static: false}) popover: NgbPopover;
-
-  @HostListener('mouseover', ['$event.target'])
-  onMouseOver(el: HTMLAnchorElement) {
-    if (el.dataset.description) {
-      if (this.popover.isOpen() && this.popover.target !== el) {
-        this.popover.close();
-      }
-      this.popover.target = el;
-      this.popover.open({text: el.dataset.description, href: el.href});
-    }
-  }
 }
