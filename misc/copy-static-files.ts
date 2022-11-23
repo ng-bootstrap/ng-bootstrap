1b2fdf02d36924a3a5f1a5c336d0cfe0ffabe04e
Max Okorokov <max@okorokov.com>
2022-11-23 11:09:52 +0100

    build: temporarily disable schematics build

--- misc/copy-static-files.ts
+++ misc/copy-static-files.ts
@@ -1,15 +1,15 @@
-import { copyFileSync, copySync, readJsonSync, writeJSONSync } from 'fs-extra';
+import { copyFileSync, readJsonSync, writeJSONSync } from 'fs-extra';
 
 const DEST = 'dist/ng-bootstrap';
-const SCHEMATICS = `${DEST}/schematics`;
+// const SCHEMATICS = `${DEST}/schematics`;
 
 // 1. Copy static assets to 'dist/ng-bootstrap'
 ['LICENSE', 'README.md'].forEach((file) => copyFileSync(file, `${DEST}/${file}`));
 
 // 2. Copy built schematics to 'dist/ng-bootstrap'
-copySync('schematics/dist', SCHEMATICS);
-copyFileSync('schematics/collection.json', `${SCHEMATICS}/collection.json`);
-copyFileSync('schematics/ng-add/schema.json', `${SCHEMATICS}/ng-add/schema.json`);
+// copySync('schematics/dist', SCHEMATICS);
+// copyFileSync('schematics/collection.json', `${SCHEMATICS}/collection.json`);
+// copyFileSync('schematics/ng-add/schema.json', `${SCHEMATICS}/ng-add/schema.json`);
 
 // 3. Patch version in the 'dist/package.json' with the one from 'package.json'
 const { version } = readJsonSync(`package.json`, { encoding: 'utf-8' });
