998b01385eb22928e60ed48c376cad3847def7c8
Max Okorokov <max@okorokov.com>
2022-11-28 13:50:39 +0100

    docs: 14.0.0-rc announcement

--- demo/src/app/default/default.component.ts
+++ demo/src/app/default/default.component.ts
@@ -3,11 +3,12 @@ import { environment } from '../../environments/environment';
 import { Snippet } from '../services/snippet';
 import { NgbdCodeComponent } from '../shared/code/code.component';
 import { RouterLink } from '@angular/router';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
 	selector: 'ngbd-default',
 	standalone: true,
-	imports: [NgbdCodeComponent, RouterLink],
+	imports: [NgbdCodeComponent, RouterLink, NgbAlertModule],
 	templateUrl: './default.component.html',
 })
 export class DefaultComponent {
