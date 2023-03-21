bd039ab369c7232294093dd5bf3373ac487aeb13
Németh Tamás <17056056+tamas-nemeth@users.noreply.github.com>
2023-03-21 10:23:24 +0000

    feat(timepicker): export NgbTimeStructAdapter

--- src/index.ts
+++ src/index.ts
@@ -143,6 +143,7 @@ export {
 	NgbTimepickerConfig,
 	NgbTimepickerModule,
 	NgbTimeStruct,
+	NgbTimeStructAdapter,
 } from './timepicker/timepicker.module';
 export { NgbToast, NgbToastConfig, NgbToastHeader, NgbToastModule } from './toast/toast.module';
 export { NgbTooltip, NgbTooltipConfig, NgbTooltipModule } from './tooltip/tooltip.module';
