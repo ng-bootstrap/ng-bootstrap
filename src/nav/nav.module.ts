7db38b827728d461f789cf7fc5d047aaf61fcf69
Max Okorokov <max@okorokov.com>
2023-03-30 15:27:15 +0200

    feat(nav): allow using `NgbNavLink` on a button element

--- src/nav/nav.module.ts
+++ src/nav/nav.module.ts
@@ -1,13 +1,31 @@
 import { NgModule } from '@angular/core';
 
-import { NgbNav, NgbNavContent, NgbNavItem, NgbNavLink } from './nav';
+import { NgbNav, NgbNavContent, NgbNavItem, NgbNavLink, NgbNavLinkButton, NgbNavLinkBase } from './nav';
 import { NgbNavOutlet, NgbNavPane } from './nav-outlet';
 
-export { NgbNav, NgbNavContent, NgbNavContentContext, NgbNavItem, NgbNavLink, NgbNavChangeEvent } from './nav';
+export {
+	NgbNav,
+	NgbNavContent,
+	NgbNavContentContext,
+	NgbNavItem,
+	NgbNavLink,
+	NgbNavLinkButton,
+	NgbNavLinkBase,
+	NgbNavChangeEvent,
+} from './nav';
 export { NgbNavOutlet, NgbNavPane } from './nav-outlet';
 export { NgbNavConfig } from './nav-config';
 
-const NGB_NAV_DIRECTIVES = [NgbNavContent, NgbNav, NgbNavItem, NgbNavLink, NgbNavOutlet, NgbNavPane];
+const NGB_NAV_DIRECTIVES = [
+	NgbNavContent,
+	NgbNav,
+	NgbNavItem,
+	NgbNavLink,
+	NgbNavLinkButton,
+	NgbNavLinkBase,
+	NgbNavOutlet,
+	NgbNavPane,
+];
 
 @NgModule({
 	imports: NGB_NAV_DIRECTIVES,
