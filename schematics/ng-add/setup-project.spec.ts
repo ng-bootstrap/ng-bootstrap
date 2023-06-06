5ca57a29aada2d25a3b248fd30c49c774eba4eb1
Max Okorokov <max@okorokov.com>
2023-06-06 13:09:36 +0200

    fix(schematics): support apps created with `--standalone` flag
    Fixes #4511

--- schematics/ng-add/setup-project.spec.ts
+++ schematics/ng-add/setup-project.spec.ts
@@ -1,75 +1,80 @@
-import { SchematicTestRunner, UnitTestTree } from '@angular-devkit/schematics/testing';
+import { SchematicTestRunner } from '@angular-devkit/schematics/testing';
 import { readWorkspace } from '@schematics/angular/utility';
-import { workspaces } from '@angular-devkit/core';
 import { Schema } from './schema';
 import * as messages from './messages';
 import { createTestApp } from '../utils/testing';
 
-['app', 'second-app'].forEach((projectName) => {
-	describe(`ng-add-project-setup, 'project=${projectName}'`, () => {
-		let runner: SchematicTestRunner;
-		let log: string[] = [];
-
-		async function createAppWithOptions(
-			appOptions = {},
-		): Promise<{ tree: UnitTestTree; project: workspaces.ProjectDefinition }> {
-			// 'app' is the default application, so we're not passing '--project' option
+for (const projectName of ['app', 'second-app']) {
+	describe(`ng-add-project-setup, app=${projectName}`, () => {
+		async function createApp(appOptions = {}) {
+			let log: string[] = [];
 			const options: Schema = { project: projectName };
+
+			let runner = new SchematicTestRunner('schematics', require.resolve('../collection.json'));
+			runner.logger.subscribe(({ message }) => log.push(message));
+
 			let tree = await createTestApp(runner, appOptions);
 			tree = await runner.runSchematic('ng-add-setup-project', options, tree);
 			const workspace = await readWorkspace(tree);
 			const project = workspace.projects.get(projectName)!;
-			return { tree, project };
+			return { tree, project, runner, log };
 		}
 
-		beforeEach(() => {
-			log = [];
-			runner = new SchematicTestRunner('schematics', require.resolve('../collection.json'));
-			runner.logger.subscribe(({ message }) => log.push(message));
+		it(`should setup standalone project correctly by default`, async () => {
+			let { tree, project } = await createApp({ standalone: true });
+
+			// localize polyfill was executed
+			expect(tree.read(`projects/${projectName}/tsconfig.app.json`)!.toString()).toContain('@angular/localize');
+
+			// default styles were added
+			expect(project.targets.get('build')!.options!.styles).toContain(
+				'node_modules/bootstrap/dist/css/bootstrap.min.css',
+			);
 		});
 
-		it(`should add '@angular/localize' polyfill`, async () => {
-			let tree = await createTestApp(runner);
-			const tsconfigAppFilePath = `projects/${projectName}/tsconfig.app.json`;
+		it(`should setup module-based project correctly by default`, async () => {
+			let { tree, project } = await createApp({ standalone: false });
 
-			expect(tree.read(tsconfigAppFilePath)!.toString()).not.toContain('@angular/localize');
+			// localize polyfill was executed
+			expect(tree.read(`projects/${projectName}/tsconfig.app.json`)!.toString()).toContain('@angular/localize');
 
-			tree = await runner.runSchematic('ng-add-setup-project', projectName ? { project: projectName } : {}, tree);
-			expect(tree.read(tsconfigAppFilePath)!.toString()).toContain('@angular/localize');
+			// default styles were added
+			expect(project.targets.get('build')!.options!.styles).toContain(
+				'node_modules/bootstrap/dist/css/bootstrap.min.css',
+			);
+
+			// NgbModule was imported
+			let appModuleFile = tree.read(`projects/${projectName}/src/app/app.module.ts`)!.toString();
+			expect(appModuleFile).toContain(`import { NgbModule } from '@ng-bootstrap/ng-bootstrap';`);
+			expect(appModuleFile).toMatch(/imports:\s*\[[^\]]+NgbModule[^\]]+]/m);
 		});
 
-		it(`should add 'bootstrap.min.css' to 'angular.json' by default`, async () => {
-			const { project } = await createAppWithOptions();
-			const targetOptions = project.targets.get('build')!.options!;
+		describe('with different style options', () => {
+			for (let standalone of [false, true]) {
+				it(`should patch 'style.sass' when using SASS styles (standalone=${standalone})`, async () => {
+					const { tree } = await createApp({ standalone, style: 'sass' });
 
-			expect(targetOptions.styles).toContain('node_modules/bootstrap/dist/css/bootstrap.min.css');
-		});
+					const stylesFile = tree.read(`projects/${projectName}/src/styles.sass`)!.toString();
+					expect(stylesFile).toContain(`@import 'bootstrap/scss/bootstrap'`);
+					expect(stylesFile).not.toContain(`@import 'bootstrap/scss/bootstrap;'`);
+				});
 
-		it(`should patch 'style.sass' when using SASS styles`, async () => {
-			const { tree } = await createAppWithOptions({ style: 'sass' });
+				it(`should patch 'style.scss' when using SCSS styles`, async () => {
+					const { tree } = await createApp({ standalone, style: 'scss' });
 
-			const expectedStylesPath = `projects/${projectName}/src/styles.sass`;
-			const stylesFile = tree.read(expectedStylesPath)!.toString();
+					expect(tree.read(`projects/${projectName}/src/styles.scss`)!.toString()).toContain(
+						`@import 'bootstrap/scss/bootstrap';`,
+					);
+				});
 
-			expect(stylesFile).toContain(`@import 'bootstrap/scss/bootstrap'`);
-			expect(stylesFile).not.toContain(`@import 'bootstrap/scss/bootstrap;'`);
-		});
-
-		it(`should patch 'style.scss' when using SCSS styles`, async () => {
-			const { tree } = await createAppWithOptions({ style: 'scss' });
-
-			const expectedStylesPath = `projects/${projectName}/src/styles.scss`;
-			const stylesFile = tree.read(expectedStylesPath)!.toString();
-
-			expect(stylesFile).toContain(`@import 'bootstrap/scss/bootstrap';`);
-		});
-
-		it(`should add 'bootstrap.min.css' to 'angular.json' if style system is unsupported`, async () => {
-			const { project } = await createAppWithOptions({ style: 'less' });
-			const targetOptions = project.targets.get('build')!.options!;
-
-			expect(targetOptions.styles).toContain('node_modules/bootstrap/dist/css/bootstrap.min.css');
-			expect(log).toEqual([messages.unsupportedStyles(`projects/${projectName}/src/styles.less`)]);
+				it(`should add 'bootstrap.min.css' to 'angular.json' if style system is unsupported`, async () => {
+					const { project, log } = await createApp({ standalone, style: 'less' });
+					expect(project.targets.get('build')!.options!.styles).toContain(
+						'node_modules/bootstrap/dist/css/bootstrap.min.css',
+					);
+					expect(log).toContain(messages.unsupportedStyles(`projects/${projectName}/src/styles.less`));
+				});
+			}
 		});
 	});
-});
+}
