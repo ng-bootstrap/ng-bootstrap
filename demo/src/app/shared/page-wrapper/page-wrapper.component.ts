e2a60e06dd6076363b8f84c550221aa2b5e31dae
Max Okorokov <max@okorokov.com>
2023-06-26 14:14:43 +0200

    demo: use scrollspy to highlight sections

--- demo/src/app/shared/page-wrapper/page-wrapper.component.ts
+++ demo/src/app/shared/page-wrapper/page-wrapper.component.ts
@@ -1,7 +1,12 @@
-import { Component, ContentChildren, Input, NgZone, QueryList } from '@angular/core';
+import { AfterViewInit, Component, ContentChildren, inject, Input, NgZone, QueryList } from '@angular/core';
 import { NgbdPageHeaderComponent } from './page-header.component';
 import { TableOfContents } from '../component-wrapper/component-wrapper.component';
-import { NgbCollapseModule, NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import {
+	NgbCollapseModule,
+	NgbDropdownModule,
+	NgbScrollSpyModule,
+	NgbScrollSpyService,
+} from '@ng-bootstrap/ng-bootstrap';
 import { NgFor, NgIf } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { SideNavComponent } from '../side-nav/side-nav.component';
@@ -9,10 +14,10 @@ import { SideNavComponent } from '../side-nav/side-nav.component';
 @Component({
 	selector: 'ngbd-page-wrapper',
 	standalone: true,
-	imports: [NgbCollapseModule, NgbDropdownModule, NgIf, NgFor, RouterLink, SideNavComponent],
+	imports: [NgbCollapseModule, NgbDropdownModule, NgbScrollSpyModule, NgIf, NgFor, RouterLink, SideNavComponent],
 	templateUrl: './page-wrapper.component.html',
 })
-export class NgbdPageWrapper {
+export class NgbdPageWrapper implements AfterViewInit {
 	@Input() pageTitle: string;
 
 	@ContentChildren(NgbdPageHeaderComponent) private _tableOfContents: QueryList<NgbdPageHeaderComponent>;
@@ -20,11 +25,19 @@ export class NgbdPageWrapper {
 	sidebarCollapsed = true;
 	isLargeScreenOrLess: boolean;
 
+	private _scrollSpy = inject(NgbScrollSpyService);
+
 	constructor(ngZone: NgZone) {
 		const largeScreenQL = matchMedia('(max-width: 1199.98px)');
 		this.isLargeScreenOrLess = largeScreenQL.matches;
 		// eslint-disable-next-line deprecation/deprecation
 		largeScreenQL.addListener((event) => ngZone.run(() => (this.isLargeScreenOrLess = event.matches)));
+	}
+
+	ngAfterViewInit() {
+		this._scrollSpy.start({
+			fragments: this.tableOfContents.map(({ fragment }) => fragment),
+		});
 	}
 
 	get tableOfContents(): TableOfContents {
