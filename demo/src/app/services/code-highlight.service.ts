521863769064f01c969681d0c5f34f8e4ed3e5dd
Max Okorokov <max@okorokov.com>
2023-10-26 16:19:35 +0200

    demo: add light/dark mode for prism

--- demo/src/app/services/code-highlight.service.ts
+++ demo/src/app/services/code-highlight.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 
 import * as prism from 'prismjs';
 import 'prismjs/components/prism-typescript';
-import 'prismjs/components/prism-bash';
 
 // Prism tries to highlight the whole document on DOMContentLoad.
 // Unfortunately with webpack the only way of disabling it
