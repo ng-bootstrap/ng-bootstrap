de31bf2fceecda5d9a9b0265e7c2ac3ba9e9b231
Max Okorokov <max@okorokov.com>
2023-11-06 15:20:28 +0100

    docs: update component-based accordion removal notice

--- demo/src/app/components/accordion/accordion-deprecation.component.ts
+++ demo/src/app/components/accordion/accordion-deprecation.component.ts
@@ -10,9 +10,9 @@ import { NgbAlert } from '@ng-bootstrap/ng-bootstrap';
 		<ngb-alert type="warning" [dismissible]="false">
 			<p>
 				This page describes accordion directives that were introduced in <code>14.1.0</code>. Previous implementation of
-				the accordion was component-based and is deprecated. It will be removed completely in <code>16.0.0</code>.
+				the accordion was component-based and was removed in <code>16.0.0</code>.
 			</p>
-			You can find related documentation and examples for the old version
+			You can find documentation and examples for the old version
 			<a href="/releases/13.x/#/components/accordion">here</a>.
 		</ngb-alert>
 	`,
