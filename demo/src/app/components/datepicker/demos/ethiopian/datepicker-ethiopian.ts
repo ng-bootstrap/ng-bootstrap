4d5d5f009c9c90f50fc59be2366872bb9b8396a7
Max Okorokov <max@okorokov.com>
2023-11-16 15:57:59 +0100

    demo(datepicker): add ethiopian calendar demo

---
+++ demo/src/app/components/datepicker/demos/ethiopian/datepicker-ethiopian.ts
@@ -0,0 +1,35 @@
+import { Component, inject, ViewEncapsulation } from '@angular/core';
+import {
+	NgbCalendar,
+	NgbCalendarEthiopian,
+	NgbDatepickerI18n,
+	NgbDatepickerI18nAmharic,
+	NgbDatepickerModule,
+	NgbDateStruct,
+} from '@ng-bootstrap/ng-bootstrap';
+import { JsonPipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+	selector: 'ngbd-datepicker-ethiopian',
+	standalone: true,
+	imports: [NgbDatepickerModule, FormsModule, JsonPipe],
+	templateUrl: './datepicker-ethiopian.html',
+	encapsulation: ViewEncapsulation.None,
+	styles: `
+	  ngbd-datepicker-ethiopian .ngb-dp-weekday {
+			font-size: x-small;
+			overflow: hidden;
+		}
+	`,
+	providers: [
+		{ provide: NgbCalendar, useClass: NgbCalendarEthiopian },
+		{ provide: NgbDatepickerI18n, useClass: NgbDatepickerI18nAmharic },
+	],
+})
+export class NgbdDatepickerEthiopian {
+	today = inject(NgbCalendar).getToday();
+
+	model: NgbDateStruct;
+	date: { year: number; month: number };
+}
