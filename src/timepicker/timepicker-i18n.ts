fd81fa0c6e6611357b2b207b6fe266850e4e153f
Bastien <bmoulia@gmail.com>
2024-05-28 21:29:11 +0200

    fix(timepicker): get period from angular formatDate

--- src/timepicker/timepicker-i18n.ts
+++ src/timepicker/timepicker-i18n.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { formatDate } from '@angular/common';
+import { Injectable, LOCALE_ID, inject } from '@angular/core';
 
 /**
  * Type of the service supplying day periods (for example, 'AM' and 'PM') to NgbTimepicker component.
@@ -23,11 +24,18 @@ export abstract class NgbTimepickerI18n {
 
 @Injectable()
 export class NgbTimepickerI18nDefault extends NgbTimepickerI18n {
+	private _locale = inject(LOCALE_ID);
+
+	private _periods = [
+		formatDate(new Date(3600000), 'a', this._locale, 'UTC'),
+		formatDate(new Date(3600000 * 13), 'a', this._locale, 'UTC'),
+	];
+
 	getMorningPeriod(): string {
-		return 'AM';
+		return this._periods[0];
 	}
 
 	getAfternoonPeriod(): string {
-		return 'PM';
+		return this._periods[1];
 	}
 }
