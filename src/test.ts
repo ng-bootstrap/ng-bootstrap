a92860293c7246b917068b559a8a3b63c7542313
jnizet <jb@ninja-squad.com>
2024-05-31 14:32:58 +0200

    test(datepicker): ignore warnings that are unavoidable
    Track-related warnings now cause test failures by default.
    So, if such a failure happens, we can either fix the code to avoid the failure when the code is incorrect (and the error is thus justified), or ignore the warning when the code is correct and the warning is unavoidable.
    
    In the end, the test report is not be spammed with warnings anymore, and when there is a warning, the error allows knowing which test caused it.

--- src/test.ts
+++ src/test.ts
@@ -8,9 +8,24 @@ import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
 import './test/jasmine.config';
+import { areTrackWarningsIgnored, honorTrackWarnings } from './test/common';
 
 // First, initialize the Angular testing environment.
 getTestBed().initTestEnvironment(BrowserDynamicTestingModule, platformBrowserDynamicTesting(), {
 	errorOnUnknownElements: true,
 	errorOnUnknownProperties: true,
+});
+
+beforeEach(() => {
+	honorTrackWarnings();
+	const originalWarn = console.warn;
+	spyOn(console, 'warn').and.callFake((message: string, ...args: any[]) => {
+		if (message.includes('NG0956') || message.includes('NG0955')) {
+			if (!areTrackWarningsIgnored()) {
+				fail('track warning found: ' + message);
+			}
+		} else {
+			originalWarn(message, ...args);
+		}
+	});
 });
--- src/test/common.ts
+++ src/test/common.ts
@@ -16,7 +16,7 @@ export function createGenericTestComponent<T>(
 
 export function isBrowserVisible(suiteName: string) {
 	if (document.hidden) {
-		console.warn(`${suiteName} tests were skipped because browser tab running these tests is hidden or inactive`);
+		// console.warn(`${suiteName} tests were skipped because browser tab running these tests is hidden or inactive`);
 		return false;
 	}
 	return true;
@@ -25,4 +25,27 @@ export function isBrowserVisible(suiteName: string) {
 export function triggerEvent(element: DebugElement | HTMLElement, eventName: string) {
 	const evt = new Event(eventName, { bubbles: true, cancelable: false });
 	(element instanceof DebugElement ? element.nativeElement : element).dispatchEvent(evt);
+}
+
+let trackWarningsIgnored = false;
+
+/**
+ * This function may be called by a test in order for a track-related warning
+ * to be ignored and not cause a test failure, because the warning is unavoidable
+ * and the code is correct.
+ */
+export function ignoreTrackWarnings() {
+	trackWarningsIgnored = true;
+}
+
+/**
+ * This function is called by the global beforeEach in test.ts in order to
+ * honor the track warnings, in case the previous test has ignored them.
+ */
+export function honorTrackWarnings() {
+	trackWarningsIgnored = false;
+}
+
+export function areTrackWarningsIgnored() {
+	return trackWarningsIgnored;
 }
