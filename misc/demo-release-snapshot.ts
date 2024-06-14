57a0a3201cc16a83a7965951da04b0002271b4b2
Max Okorokov <max@okorokov.com>
2024-06-14 11:02:27 +0200

    feat: use esbuild for the demo site (#4726)

--- misc/demo-release-snapshot.ts
+++ misc/demo-release-snapshot.ts
@@ -11,16 +11,16 @@ spawnSync('yarn', ['demo:build'], { cwd: join(__dirname, '..'), stdio: 'inherit'
 
 // 1. Cleaning the built demo
 console.log(`Removing '/releases' and 'versions.json' from the built demo`);
-fs.rmSync('demo/dist/releases', { recursive: true, force: true });
-fs.rmSync('demo/dist/versions.json', { force: true });
+fs.rmSync('demo/dist/browser/releases', { recursive: true, force: true });
+fs.rmSync('demo/dist/browser/versions.json', { force: true });
 
-// 2. Patching 'demo/dist/index.html' to remove it from indexing
-console.log(`Patching 'demo/dist/index.html' with <meta content="noindex">`);
-let indexHtml = fs.readFileSync('demo/dist/index.html').toString();
+// 2. Patching 'demo/dist/browser/index.html' to remove it from indexing
+console.log(`Patching 'demo/dist/browser/index.html' with <meta content="noindex">`);
+let indexHtml = fs.readFileSync('demo/dist/browser/index.html').toString();
 indexHtml = indexHtml.replace('<head>', '<head>\n    <meta name="robots" content="noindex">');
-fs.writeFileSync('demo/dist/index.html', indexHtml);
+fs.writeFileSync('demo/dist/browser/index.html', indexHtml);
 
 // 3. Moving demo to the public directory
 const destination = `demo/src/public/releases/${majorVersion}.x`; // ex. 12.x
 console.log(`Moving built demo to '${destination}'`);
-fs.copySync('demo/dist', destination, { recursive: true, overwrite: true });
+fs.copySync('demo/dist/browser', destination, { recursive: true, overwrite: true });
