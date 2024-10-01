0ec111528041d06a9b7422dc7612c4c1079ff8dd
Parsa Arvaneh <Parsa.Arvaneh@gmail.com>
2024-10-01 11:29:28 +0330

    test(accordion): add missing tests

--- src/accordion/accordion-config.spec.ts
+++ src/accordion/accordion-config.spec.ts
@@ -3,10 +3,24 @@ import { NgbAccordionConfig } from './accordion-config';
 import { TestBed } from '@angular/core/testing';
 
 describe('ngb-accordion-config', () => {
-	it('should have sensible default values', () => {
-		const config = TestBed.inject(NgbAccordionConfig);
+	let config: NgbAccordionConfig;
 
+	beforeEach(() => {
+		config = TestBed.inject(NgbAccordionConfig);
+	});
+
+	it('should have sensible default values', () => {
 		expect(config.closeOthers).toBe(false);
 		expect(config.destroyOnHide).toBe(true);
+	});
+
+	it('should set animation with correct value', () => {
+		expect(config['_animation']).toBeUndefined();
+
+		config.animation = true;
+		expect(config['_animation']).toBe(true);
+
+		config.animation = false;
+		expect(config['_animation']).toBe(false);
 	});
 });
