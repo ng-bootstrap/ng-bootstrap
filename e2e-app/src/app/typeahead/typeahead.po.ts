2506e4d4ff3eaa59851d7a1e48f246bdf7835d19
Ruslan Zabarov <ruszabarov@gmail.com>
2025-05-16 23:42:53 -0400

    fix(typeahead): add scroll functionality to typeahead component for accessibility
    Fixes #4789

--- e2e-app/src/app/typeahead/typeahead.po.ts
+++ e2e-app/src/app/typeahead/typeahead.po.ts
@@ -3,6 +3,7 @@ import { getPage } from '../../../baseTest';
 export const SELECTOR_TYPEAHEAD = 'input#typeahead';
 export const SELECTOR_TYPEAHEAD_WINDOW = 'ngb-typeahead-window';
 export const SELECTOR_TYPEAHEAD_ITEMS = 'ngb-typeahead-window > button';
+export const SELECTOR_TYPEAHEAD_ACTIVE_ITEM = 'ngb-typeahead-window > button.active';
 
 export const getTypeaheadValue = async () =>
 	await getPage().$eval(SELECTOR_TYPEAHEAD, (el: HTMLInputElement) => el.value);
