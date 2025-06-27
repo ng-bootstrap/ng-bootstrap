5d523bfd3f04e46b672328d5eaa205398a6fde5f
ezqel <ezekiel3499@gmail.com>
2025-06-27 15:19:36 +0300

    demo: add clipboard button functionality and relevant styling
    Add a "Copy to clipboard" button on the code component to enable one-click copying of  snippets across the demo site.
    
    Fixes #4826

--- demo/src/app/shared/code.component.ts
+++ demo/src/app/shared/code.component.ts
@@ -5,27 +5,55 @@ import {
 	ElementRef,
 	inject,
 	input,
+	signal,
 	viewChild,
 } from '@angular/core';
 
 import { ISnippet } from '../services/snippet';
 import { CodeHighlightService } from '../services/code-highlight.service';
+import { NgbTooltip } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
 	selector: 'ngbd-code',
 	changeDetection: ChangeDetectionStrategy.OnPush,
+	imports: [NgbTooltip],
 	template: `
-		<pre class="language-{{ snippet().lang }}"><code #code class="language-{{ snippet().lang }}"></code></pre>
+		<div class="position-relative">
+			<button
+				(click)="copyToClipboard()"
+				[ngbTooltip]="showAsCopied() ? 'Copied!' : 'Copy to clipboard'"
+				class="btn btn-clipboard position-absolute top-0 end-0 mt-2 me-3 z-2"
+				aria-label="Copy code to clipboard"
+			>
+				<i class="bi bi-{{ showAsCopied() ? 'clipboard-check' : 'clipboard' }}"></i>
+			</button>
+			<pre class="language-{{ snippet().lang }}"><code #code class="language-{{ snippet().lang }}"></code></pre>
+		</div>
 	`,
 })
 export class CodeComponent {
+	private _copyBtnTooltip = viewChild.required(NgbTooltip);
 	private _codeEl = viewChild.required('code', { read: ElementRef });
 	snippet = input.required<ISnippet>();
+
+	showAsCopied = signal(false);
 
 	constructor() {
 		const highlightService = inject(CodeHighlightService);
 		afterNextRender(() => {
 			this._codeEl().nativeElement.innerHTML = highlightService.highlight(this.snippet().code, this.snippet().lang);
+		});
+	}
+
+	copyToClipboard() {
+		navigator.clipboard.writeText(this.snippet().code).then(() => {
+			this.showAsCopied.set(true);
+
+			// 'Restart' tooltip to show 'Copied!' message
+			this._copyBtnTooltip().close();
+			setTimeout(() => this._copyBtnTooltip().open(), 0);
+
+			setTimeout(() => this.showAsCopied.set(false), 2000);
 		});
 	}
 }
