3945da9f203eacb01e70eb8f3352442e2acd2f33
JB Nizet <jb@ninja-squad.com>
2025-12-15 10:23:21 +0100

    test(collapse): make the collapse test zoneless (#4883)
    and refactor it to benefit from vitest's locators and assertions

--- src/vitest.config.ts
+++ src/vitest.config.ts
@@ -3,5 +3,10 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	test: {
 		isolate: true,
+		testTimeout: 2000,
+		hookTimeout: 2000,
+		browser: {
+			screenshotFailures: false,
+		},
 	},
 });
