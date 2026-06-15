bb270581b78e4bdbca707cc83edd4a07c06db7e4
pqderoub_amadeus <quentin.deroubaix@amadeus.com>
2026-06-15 11:15:03 +0200

    fix(schematics): cannot run ng-add

--- misc/copy-static-files.ts
+++ misc/copy-static-files.ts
@@ -9,6 +9,7 @@ const SCHEMATICS = `${DEST}/schematics`;
 // 2. Copy built schematics to 'dist/ng-bootstrap'
 copySync('schematics/dist', SCHEMATICS);
 copyFileSync('schematics/collection.json', `${SCHEMATICS}/collection.json`);
+copyFileSync('schematics/package.json', `${SCHEMATICS}/package.json`);
 copyFileSync('schematics/ng-add/schema.json', `${SCHEMATICS}/ng-add/schema.json`);
 
 // 3. Patch version in the 'dist/package.json' with the one from 'package.json'
@@ -16,3 +17,6 @@ const { version } = readJsonSync(`package.json`, { encoding: 'utf-8' });
 const packageJson = readJsonSync(`${DEST}/package.json`, { encoding: 'utf-8' });
 packageJson.version = version;
 writeJSONSync(`${DEST}/package.json`, packageJson, { spaces: 2, encoding: 'utf-8' });
+
+// 4. Patch .npmignore file to include schematics package.json
+copyFileSync('.npmignore', `${DEST}/.npmignore`);
